4ed5674cd0c5caf621b403ac6962a76c9b0ab097
Kabin Karki <91150148+kabinspace@users.noreply.github.com>
2022-02-03 04:22:36 +0545

    Create colorizer.lua

---
+++ lua/configs/colorizer.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.config()
+  local present, colorizer = pcall(require, "colorizer")
+  if not present then
+    return
+  end
+
+  colorizer.setup(
+    { "*" },                    -- Highlight all files, but customize some others
+    {
+      RGB = true,               -- #RGB hex codes
+      RRGGBB = true,            -- #RRGGBB hex codes
+      names = false,            -- "Name" codes like Blue
+      RRGGBBAA = false,         -- #RRGGBBAA hex codes
+      rgb_fn = false,           -- CSS rgb() and rgba() functions
+      hsl_fn = false,           -- CSS hsl() and hsla() functions
+      css = false,              -- Enable all css features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = false,           -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      mode = "background",      -- Set the display mode
+    }
+  )
+end
+
+return M
