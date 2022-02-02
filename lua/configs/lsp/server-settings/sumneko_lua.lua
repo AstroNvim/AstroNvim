8c036040a19485849f012356513c8679ebc703f0
Kabin Karki <91150148+kabinspace@users.noreply.github.com>
2022-02-03 04:34:18 +0545

    Create sumneko_lua.lua

---
+++ lua/configs/lsp/server-settings/sumneko_lua.lua
@@ -0,0 +1,15 @@
+return {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+    },
+  },
+}
