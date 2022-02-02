ea7ce0d330ca6cebb72c31b64d78a7629ff980d3
Kabin Karki <91150148+kabinspace@users.noreply.github.com>
2022-02-03 04:22:58 +0545

    Create comment.lua

---
+++ lua/configs/comment.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+function M.config()
+  local status_ok, comment = pcall(require, "Comment")
+  if not status_ok then
+    return
+  end
+
+  comment.setup {
+    pre_hook = function(ctx)
+      local U = require "Comment.utils"
+
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require("ts_context_commentstring.utils").get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
+      end
+
+      return require("ts_context_commentstring.internal").calculate_commentstring {
+        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+        location = location,
+      }
+    end,
+  }
+end
+
+return M
