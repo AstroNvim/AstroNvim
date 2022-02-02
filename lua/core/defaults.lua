c771fdf86ab8f2377763290b9891bd4913271a1b
Kabin Karki <91150148+kabinspace@users.noreply.github.com>
2022-02-03 04:38:10 +0545

    Create defaults.lua

---
+++ lua/core/defaults.lua
@@ -0,0 +1,26 @@
+local config = {
+
+  colorscheme = "onedark",
+
+  plugins = {},
+
+  virtual_text = true,
+
+  enabled = {
+    comment = true,
+    lualine = true,
+    lspsaga = true,
+    gitsigns = true,
+    colorizer = true,
+    nvim_tree = true,
+    which_key = true,
+    neoscroll = true,
+    ts_rainbow = true,
+    ts_autotag = true,
+    toggle_term = true,
+    symbols_outline = true,
+    indent_blankline = true,
+  },
+}
+
+return config
