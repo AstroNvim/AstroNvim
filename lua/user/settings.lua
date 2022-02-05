d68dc39648253b4ea2bedcd9f42576912b463c10
Niels Kobschätzki <niels@kobschaetzki.net>
2022-02-05 09:04:24 +0100

    Remove -- in plugins-part, so that Packer can recognize them

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -9,14 +9,14 @@ local config = {
 
   -- Add plugins
   plugins = {
-    -- { "andweeb/presence.nvim" },
-    -- {
-      -- "ray-x/lsp_signature.nvim",
-      -- event = "BufRead",
-      -- config = function()
-        -- require("lsp_signature").setup()
-      -- end,
-    -- },
+    { "andweeb/presence.nvim" },
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "BufRead",
+      config = function()
+        require("lsp_signature").setup()
+      end,
+    },
   },
 
   -- On/off virtual diagnostics text
