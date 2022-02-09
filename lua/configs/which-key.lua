b66da6ed971f41a26fa6657256df1eb98269bc21
Kabin Karki <91150148+kabinspace@users.noreply.github.com>
2022-02-09 10:18:54 +0545

    Which key menu for lazygit

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -127,6 +127,7 @@ function M.config()
       u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
       t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
       p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+      l = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "LazyGit" },
       f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
       h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
       v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
