1ea38e912ac50f1b8640ea172dcc5a8bf67bde88
Micah Halter <micah@balena.io>
2022-02-18 11:30:46 -0500

    Disable sumneko formatting
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -81,6 +81,9 @@ M.on_attach = function(client, bufnr)
   if client.name == "html" then
     client.resolved_capabilities.document_formatting = false
   end
+  if client.name == "sumneko_lua" then
+    client.resolved_capabilities.document_formatting = false
+  end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
