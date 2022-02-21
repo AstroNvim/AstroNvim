acd714ff77cee0768094e75966a43c63f2a460be
Arismendy Evangelista <ArismendyEZ115@hotmail.com>
2022-02-21 17:34:01 -0400

    Checking the OS to correctly show how many pluing are loaded

--- lua/configs/dashboard.lua
+++ lua/configs/dashboard.lua
@@ -6,6 +6,10 @@ function M.config()
 
   local plugins_count = fn.len(fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 
+  if fn.has("Win32") == 1 then
+    plugins_count = vim.fn.len(vim.fn.globpath("~/AppData/Local/nvim-data/site/pack/packer/start", "*", 0, 1))
+  end
+
   g.dashboard_disable_statusline = 1
   g.dashboard_default_executive = "telescope"
   g.dashboard_custom_header = {
