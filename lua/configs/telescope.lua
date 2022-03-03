2bf9965ec8c3740507c414fd9634eea950e1826f
Micah Halter <micah@balena.io>
2022-03-03 10:01:52 -0500

    Change telescope layout to seem more natural
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -15,6 +15,22 @@ function M.config()
       prompt_prefix = " ",
       selection_caret = "❯ ",
       path_display = { "truncate" },
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+          results_width = 0.8,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
 
       mappings = {
         i = {
