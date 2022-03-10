c6eefeb4b697e6597409276fad82d2e22224ef2e
Micah Halter <micah@balena.io>
2022-03-10 11:53:27 -0500

    Moved terminal mappings for easy disabling
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -29,18 +29,6 @@ function M.config()
     },
   }))
 
-  function _G.set_terminal_keymaps()
-    local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-  end
-
-  vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
   local Terminal = require("toggleterm.terminal").Terminal
   local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
 
