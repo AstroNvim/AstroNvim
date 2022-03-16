24e390978445b56b6f2dfd8dfa349c919335b091
Micah Halter <micah@balena.io>
2022-03-16 07:41:35 -0400

    Moved better-escape config to match other plugin configuration location
    Signed-off-by: Micah Halter <micah@balena.io>

---
+++ lua/configs/better_escape.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.config()
+  local present, better_escape = pcall(require, "better_escape")
+  if not present then
+    return
+  end
+
+  better_escape.setup(require("core.utils").user_plugin_opts("better_escape", {
+    mapping = { "ii", "jj", "jk", "kj" },
+    timeout = vim.o.timeoutlen,
+    keys = "<ESC>",
+  }))
+end
+
+return M
