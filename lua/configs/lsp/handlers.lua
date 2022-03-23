e9b086f737f75b1ed0765202a09315c941b4d9ad
Micah Halter <micah@balena.io>
2022-03-23 16:48:16 -0400

    Move LSP mappings to general mappings file
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -56,18 +56,6 @@ local function lsp_highlight_document(client)
   end
 end
 
-local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
-end
-
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
@@ -84,7 +72,7 @@ M.on_attach = function(client, bufnr)
     on_attach_override(client, bufnr)
   end
 
-  lsp_keymaps(bufnr)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
   lsp_highlight_document(client)
 end
 
