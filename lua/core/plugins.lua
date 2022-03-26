a894214df49b96f774a1d1ecd6531213721b86a0
Micah Halter <micah@balena.io>
2022-03-25 20:21:35 -0400

    fix telescope lazy loading for dressing and other plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -260,6 +260,7 @@ local astro_plugins = {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
+    module = "telescope",
     config = function()
       require("configs.telescope").config()
     end,
