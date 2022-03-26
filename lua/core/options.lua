799e9b56308356e583eefc53c451a3cbd6ec9f3a
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-26 09:19:24 +0800

    Global statusline

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -45,5 +45,16 @@ set.pumheight = 10 -- Height of the pop up menu
 set.history = 100 -- Number of commands to remember in a history table
 set.timeoutlen = 300 -- Length of time to wait for a mapped sequence
 set.updatetime = 300 -- Length of time to wait before triggering the plugin
+set.laststatus = 3
+set.fillchars = {
+  eob = " ",
+  vert = "║",
+  horiz = "═",
+  horizup = "╩",
+  horizdown = "╦",
+  vertleft = "╣",
+  vertright = "╠",
+  verthoriz = "╬",
+}
 
 return M
