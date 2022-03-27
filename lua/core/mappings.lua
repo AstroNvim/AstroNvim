6199d3b65b7460c3c135014861de312c37a58cce
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-27 10:26:41 +0800

    Update mappings.lua to the latest api

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,184 +2,234 @@ local M = {}
 
 local config = require("core.utils").user_settings()
 
-local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 -- Remap space as leader key
-map("", "<Space>", "<Nop>", opts)
+map("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-map("n", "<C-h>", "<cmd>lua require'smart-splits'.move_cursor_left()<cr>", opts)
-map("n", "<C-j>", "<cmd>lua require'smart-splits'.move_cursor_down()<cr>", opts)
-map("n", "<C-k>", "<cmd>lua require'smart-splits'.move_cursor_up()<cr>", opts)
-map("n", "<C-l>", "<cmd>lua require'smart-splits'.move_cursor_right()<cr>", opts)
+map("n", "<C-h>", function()
+  return require("smart-splits").move_cursor_left()
+end)
+map("n", "<C-j>", function()
+  return require("smart-splits").move_cursor_down()
+end)
+map("n", "<C-j>", function()
+  return require("smart-splits").move_cursor_up()
+end)
+map("n", "<C-l>", function()
+  return require("smart-splits").move_cursor_right()
+end)
 
 -- Resize with arrows
-map("n", "<C-Up>", "<cmd>lua require'smart-splits'.resize_up(2)<cr>", opts)
-map("n", "<C-Down>", "<cmd>lua require'smart-splits'.resize_down(2)<cr>", opts)
-map("n", "<C-Left>", "<cmd>lua require'smart-splits'.resize_left(2)<cr>", opts)
-map("n", "<C-Right>", "<cmd>lua require'smart-splits'.resize_right(2)<cr>", opts)
+map("n", "<C-Up>", function()
+  return require("smart-splits").resize_up(2)
+end)
+map("n", "<C-Down>", function()
+  return require("smart-splits").resize_down(2)
+end)
+map("n", "<C-Left>", function()
+  return require("smart-splits").resize_left(2)
+end)
+map("n", "<C-Right>", function()
+  return require("smart-splits").resize_right(2)
+end)
 
 -- Navigate buffers
 if config.enabled.bufferline then
-  map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", opts)
-  map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", opts)
-  map("n", "}", "<cmd>BufferLineMoveNext<cr>", opts)
-  map("n", "{", "<cmd>BufferLineMovePrev<cr>", opts)
+  map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>")
+  map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
+  map("n", "}", "<cmd>BufferLineMoveNext<cr>")
+  map("n", "{", "<cmd>BufferLineMovePrev<cr>")
 else
-  map("n", "<S-l>", "<cmd>bnext<CR>", opts)
-  map("n", "<S-h>", "<cmd>bprevious<CR>", opts)
+  map("n", "<S-l>", "<cmd>bnext<CR>")
+  map("n", "<S-h>", "<cmd>bprevious<CR>")
 end
 
 -- Move text up and down
-map("n", "<A-j>", "<Esc><cmd>m .+1<CR>==gi", opts)
-map("n", "<A-k>", "<Esc><cmd>m .-2<CR>==gi", opts)
+map("n", "<A-j>", "<Esc><cmd>m .+1<CR>==gi")
+map("n", "<A-k>", "<Esc><cmd>m .-2<CR>==gi")
 
 -- Standard Operations
-map("n", "<leader>w", "<cmd>w<CR>", opts)
-map("n", "<leader>q", "<cmd>q<CR>", opts)
-map("n", "<leader>c", "<cmd>Bdelete!<CR>", opts)
-map("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+map("n", "<leader>w", "<cmd>w<CR>")
+map("n", "<leader>q", "<cmd>q<CR>")
+map("n", "<leader>c", "<cmd>Bdelete!<CR>")
+map("n", "<leader>h", "<cmd>nohlsearch<CR>")
 
 -- Packer
-map("n", "<leader>pc", "<cmd>PackerCompile<cr>", opts)
-map("n", "<leader>pi", "<cmd>PackerInstall<cr>", opts)
-map("n", "<leader>ps", "<cmd>PackerSync<cr>", opts)
-map("n", "<leader>pS", "<cmd>PackerStatus<cr>", opts)
-map("n", "<leader>pu", "<cmd>PackerUpdate<cr>", opts)
+map("n", "<leader>pc", "<cmd>PackerCompile<cr>")
+map("n", "<leader>pi", "<cmd>PackerInstall<cr>")
+map("n", "<leader>ps", "<cmd>PackerSync<cr>")
+map("n", "<leader>pS", "<cmd>PackerStatus<cr>")
+map("n", "<leader>pu", "<cmd>PackerUpdate<cr>")
 
 -- LSP
-map("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting_sync()<cr>", opts)
-map("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-map("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+map("n", "<leader>lf", vim.lsp.buf.formatting_sync)
+map("n", "<leader>li", "<cmd>LspInfo<cr>")
+map("n", "<leader>lI", "<cmd>LspInstallInfo<cr>")
 
 -- NvimTree
 if config.enabled.nvim_tree then
-  map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
-  map("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", opts)
+  map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+  map("n", "<leader>o", "<cmd>NvimTreeFocus<CR>")
 end
 
 -- Dashboard
 if config.enabled.dashboard then
-  map("n", "<leader>d", "<cmd>Dashboard<CR>", opts)
-  map("n", "<leader>fn", "<cmd>DashboardNewFile<CR>", opts)
-  map("n", "<leader>db", "<cmd>Dashboard<CR>", opts)
-  map("n", "<leader>bm", "<cmd>DashboardJumpMarks<CR>", opts)
-  map("n", "<leader>sl", "<cmd>SessionLoad<CR>", opts)
-  map("n", "<leader>ss", "<cmd>SessionSave<CR>", opts)
+  map("n", "<leader>d", "<cmd>Dashboard<CR>")
+  map("n", "<leader>fn", "<cmd>DashboardNewFile<CR>")
+  map("n", "<leader>db", "<cmd>Dashboard<CR>")
+  map("n", "<leader>bm", "<cmd>DashboardJumpMarks<CR>")
+  map("n", "<leader>sl", "<cmd>SessionLoad<CR>")
+  map("n", "<leader>ss", "<cmd>SessionSave<CR>")
 end
 
 -- GitSigns
 if config.enabled.gitsigns then
-  map("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", opts)
-  map("n", "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", opts)
-  map("n", "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", opts)
-  map("n", "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", opts)
-  map("n", "<leader>gh", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", opts)
-  map("n", "<leader>gr", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", opts)
-  map("n", "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", opts)
-  map("n", "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", opts)
-  map("n", "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", opts)
+  map("n", "<leader>gj", function()
+    return require("gitsigns").next_hunk()
+  end)
+  map("n", "<leader>gk", function()
+    return require("gitsigns").prev_hunk()
+  end)
+  map("n", "<leader>gl", function()
+    return require("gitsigns").blame_line()
+  end)
+  map("n", "<leader>gp", function()
+    return require("gitsigns").preview_hunk()
+  end)
+  map("n", "<leader>gh", function()
+    return require("gitsigns").reset_hunk()
+  end)
+  map("n", "<leader>gr", function()
+    return require("gitsigns").reset_buffer()
+  end)
+  map("n", "<leader>gs", function()
+    return require("gitsigns").stage_hunk()
+  end)
+  map("n", "<ledaer>gu", function()
+    return require("gitsigns").undo_stage_hunk()
+  end)
+  map("n", "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>")
 end
 
 -- Telescope
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", opts)
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", opts)
-map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", opts)
-map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", opts)
-map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", opts)
-map("n", "<leader>sb", "<cmd>Telescope git_branches<CR>", opts)
-map("n", "<leader>sh", "<cmd>Telescope help_tags<CR>", opts)
-map("n", "<leader>sm", "<cmd>Telescope man_pages<CR>", opts)
-map("n", "<leader>sn", "<cmd>Telescope notify<CR>", opts)
-map("n", "<leader>sr", "<cmd>Telescope registers<CR>", opts)
-map("n", "<leader>sk", "<cmd>Telescope keymaps<CR>", opts)
-map("n", "<leader>sc", "<cmd>Telescope commands<CR>", opts)
-map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", opts)
-map("n", "<leader>lR", "<cmd>Telescope lsp_references<CR>", opts)
-map("n", "<leader>lD", "<cmd>Telescope diagnostics<CR>", opts)
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>")
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>")
+map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>")
+map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>")
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>")
+map("n", "<leader>sb", "<cmd>Telescope git_branches<CR>")
+map("n", "<leader>sh", "<cmd>Telescope help_tags<CR>")
+map("n", "<leader>sm", "<cmd>Telescope man_pages<CR>")
+map("n", "<leader>sn", "<cmd>Telescope notify<CR>")
+map("n", "<leader>sr", "<cmd>Telescope registers<CR>")
+map("n", "<leader>sk", "<cmd>Telescope keymaps<CR>")
+map("n", "<leader>sc", "<cmd>Telescope commands<CR>")
+map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>")
+map("n", "<leader>lR", "<cmd>Telescope lsp_references<CR>")
+map("n", "<leader>lD", "<cmd>Telescope diagnostics<CR>")
 
 -- LSP
-map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-map("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-map("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev({ border = 'rounded' })<CR>", opts)
-map("n", "]d", "<cmd>lua vim.diagnostic.goto_next({ border = 'rounded' })<CR>", opts)
-map("n", "gj", "<cmd>lua vim.diagnostic.goto_next({ border = 'rounded' })<cr>", opts)
-map("n", "gk", "<cmd>lua vim.diagnostic.goto_prev({ border = 'rounded' })<cr>", opts)
-map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-map("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-map("n", "<leader>ld", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+map("n", "gD", vim.lsp.buf.declaration)
+map("n", "gd", vim.lsp.buf.definition, { desc = "Show the definition of current function" })
+map("n", "gI", vim.lsp.buf.implementation)
+map("n", "gr", vim.lsp.buf.references)
+map("n", "go", vim.diagnostic.open_float)
+map("n", "gl", vim.diagnostic.open_float)
+map("n", "[d", function()
+  vim.diagnostic.goto_prev { border = "rounded" }
+end)
+map("n", "gk", function()
+  vim.diagnostic.goto_prev { border = "rounded" }
+end)
+map("n", "]d", function()
+  vim.diagnostic.goto_next { border = "rounded" }
+end)
+map("n", "gj", function()
+  vim.diagnostic.goto_next { border = "rounded" }
+end)
+map("n", "K", vim.lsp.buf.hover)
+map("n", "<leader>rn", vim.lsp.buf.rename)
+map("n", "<leader>la", vim.lsp.buf.code_action)
+map("n", "<leader>lr", vim.lsp.buf.rename)
+map("n", "<leader>ld", vim.diagnostic.open_float)
 
 -- Comment
 if config.enabled.comment then
-  map("n", "<leader>/", "<cmd>lua require('Comment.api').toggle_current_linewise()<cr>", opts)
-  map("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>", opts)
+  map("n", "<leader>/", function()
+    return require("Comment.api").toggle_current_linewise()
+  end)
+  map("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
 end
 
 -- ForceWrite
-map("n", "<C-s>", "<cmd>w!<CR>", opts)
+map("n", "<C-s>", "<cmd>w!<CR>")
 
 -- ForceQuit
-map("n", "<C-q>", "<cmd>q!<CR>", opts)
+map("n", "<C-q>", "<cmd>q!<CR>")
 
 -- Terminal
 if config.enabled.toggle_term then
-  map("n", "<C-\\>", "<cmd>ToggleTerm<CR>", opts)
-  map("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-  map("n", "<leader>tn", "<cmd>lua _NODE_TOGGLE()<CR>", opts)
-  map("n", "<leader>tu", "<cmd>lua _NCDU_TOGGLE()<CR>", opts)
-  map("n", "<leader>tt", "<cmd>lua _HTOP_TOGGLE()<CR>", opts)
-  map("n", "<leader>tp", "<cmd>lua _PYTHON_TOGGLE()<CR>", opts)
-  map("n", "<leader>tl", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-  map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", opts)
-  map("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", opts)
-  map("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", opts)
+  map("n", "<C-\\>", "<cmd>ToggleTerm<CR>")
+  map("n", "<leader>gg", function()
+    return _lazygit_toggle()
+  end)
+  map("n", "<leader>tn", function()
+    return _NODE_TOGGLE()
+  end)
+  map("n", "leader>tu", function()
+    return _NCDU_TOGGLE()
+  end)
+  map("n", "<leader>tt", function()
+    return _HTOP_TOGGLE()
+  end)
+  map("n", "<ledaer>tp", function()
+    return _PYTHON_TOGGLE()
+  end)
+  map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>")
+  map("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>")
+  map("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>")
 end
 
 -- SymbolsOutline
 if config.enabled.symbols_outline then
-  map("n", "<leader>lS", "<cmd>SymbolsOutline<CR>", opts)
+  map("n", "<leader>lS", "<cmd>SymbolsOutline<CR>")
 end
 
 -- Visual --
 -- Stay in indent mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- Move text up and down
-map("v", "<A-j>", "<cmd>m .+1<CR>==", opts)
-map("v", "<A-k>", "<cmd>m .-2<CR>==", opts)
-map("v", "p", '"_dP', opts)
+map("v", "<A-j>", "<cmd>m .+1<CR>==")
+map("v", "<A-k>", "<cmd>m .-2<CR>==")
+map("v", "p", '"_dP')
 
 -- Visual Block --
 -- Move text up and down
-map("x", "J", "<cmd>move '>+1<CR>gv-gv", opts)
-map("x", "K", "<cmd>move '<-2<CR>gv-gv", opts)
-map("x", "<A-j>", "<cmd>move '>+1<CR>gv-gv", opts)
-map("x", "<A-k>", "<cmd>move '<-2<CR>gv-gv", opts)
+map("x", "J", "<cmd>move '>+1<CR>gv-gv")
+map("x", "K", "<cmd>move '<-2<CR>gv-gv")
+map("x", "<A-j>", "<cmd>move '>+1<CR>gv-gv")
+map("x", "<A-k>", "<cmd>move '<-2<CR>gv-gv")
 
 -- disable Ex mode:
-map("n", "Q", "<Nop>", opts)
+map("n", "Q", "<Nop>")
 
 function _G.set_terminal_keymaps()
-  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]])
+  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]])
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]])
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]])
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]])
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]])
 end
 
 vim.cmd [[
