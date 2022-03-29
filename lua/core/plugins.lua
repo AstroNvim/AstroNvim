9354be278bbadbf8ae39de2d5a18ae98e080a492
kabinspace <kabinkarki555@gmail.com>
2022-03-30 03:53:36 +0545

    Remove sources and configs

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -179,36 +179,24 @@ local astro_plugins = {
   {
     "saadparwaiz1/cmp_luasnip",
     after = "nvim-cmp",
-    config = function()
-      require("core.utils").add_cmp_source "luasnip"
-    end,
   },
 
   -- Buffer completion source
   {
     "hrsh7th/cmp-buffer",
     after = "nvim-cmp",
-    config = function()
-      require("core.utils").add_cmp_source "buffer"
-    end,
   },
 
   -- Path completion source
   {
     "hrsh7th/cmp-path",
     after = "nvim-cmp",
-    config = function()
-      require("core.utils").add_cmp_source "path"
-    end,
   },
 
   -- LSP completion source
   {
     "hrsh7th/cmp-nvim-lsp",
     after = "nvim-cmp",
-    config = function()
-      require("core.utils").add_cmp_source "nvim_lsp"
-    end,
   },
 
   -- LSP manager
