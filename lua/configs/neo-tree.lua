c0ba6df5ffd54750883fed9cc8cce909ba121d78
Micah Halter <micah@balena.io>
2022-03-29 09:56:48 -0400

    Remove padding
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -20,7 +20,7 @@ function M.config()
     default_component_configs = {
       indent = {
         indent_size = 2,
-        padding = 1,
+        padding = 0,
         with_markers = true,
         indent_marker = "│",
         last_indent_marker = "└",
