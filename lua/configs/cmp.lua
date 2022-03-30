e08b53824cdef322e01c5e6fbd3a1e7d2fd75d1c
kabinspace <kabinkarki555@gmail.com>
2022-03-30 23:12:34 +0545

    Add tab selection for cmp

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -11,6 +11,11 @@ function M.config()
     return
   end
 
+  local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  end
+
   local kind_icons = {
     Text = "",
     Method = "",
@@ -91,12 +96,16 @@ function M.config()
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       },
-      ["<CR>"] = cmp.mapping.confirm(),
+      ["<CR>"] = cmp.mapping.confirm { select = true },
       ["<Tab>"] = cmp.mapping(function(fallback)
-        if luasnip.expandable() then
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expandable() then
           luasnip.expand()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
+        elseif check_backspace() then
+          fallback()
         else
           fallback()
         end
@@ -105,7 +114,9 @@ function M.config()
         "s",
       }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if luasnip.jumpable(-1) then
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
           luasnip.jump(-1)
         else
           fallback()
