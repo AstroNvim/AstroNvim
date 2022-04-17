42c726d75f8a4a4c2a216ae6ecda7fe96eb33384
kabinspace <kabinkarki555@gmail.com>
2022-04-17 22:30:16 +0545

    Augroup argument error fix

--- lua/user_example/init.lua
+++ lua/user_example/init.lua
@@ -177,7 +177,7 @@ local config = {
     map("n", "<C-s>", ":w!<CR>")
 
     -- Set autocommands
-    vim.api.nvim_create_augroup "packer_conf"
+    vim.api.nvim_create_augroup("packer_conf", {})
     vim.api.nvim_create_autocmd("BufWritePost", {
       desc = "Sync packer after modifying plugins.lua",
       group = "packer_conf",
