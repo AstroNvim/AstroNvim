89fe1e983c0eef35d3188bd62289df37b1433e52
Jose Angel Mendez Santiago <jamsmendez02@gmail.com>
2022-04-21 16:02:57 -0500

    Detail lualine and status

--- lua/core/status.lua
+++ lua/core/status.lua
@@ -56,7 +56,7 @@ end
 function M.treesitter_status()
   local b = vim.api.nvim_get_current_buf()
   if next(vim.treesitter.highlighter.active[b]) then
-    return " 綠TS"
+    return " 綠 TS"
   end
   return ""
 end
