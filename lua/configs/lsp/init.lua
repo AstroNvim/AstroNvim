eb5ee79816d6f30598bedbf29adaab39332141a3
Micah Halter <micah@balena.io>
2022-04-24 21:52:44 -0400

    Add ability for easy lsp setup without lsp installer
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -1,5 +1,10 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if status_ok then
   require "configs.lsp.lsp-installer"
-  require("configs.lsp.handlers").setup()
+  local handlers = require "configs.lsp.handlers"
+  handlers.setup()
+  local lsp_setup = require("core.utils").user_plugin_opts "lsp.setup"
+  if type(lsp_setup) == "function" then
+    lsp_setup(handlers.on_attach, handlers.capabilities)
+  end
 end
