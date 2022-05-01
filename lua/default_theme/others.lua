d8792d61cd975d9375d72f04ce84a3001b7649ad
kabinspace <kabinkarki555@gmail.com>
2022-05-01 14:13:10 +0545

    Add icons and highlights

--- lua/default_theme/others.lua
+++ lua/default_theme/others.lua
@@ -244,8 +244,9 @@ local others = {
 
   -- Aerial
   AerialLine = { fg = C.yellow, bg = C.none },
-  AerialGuide = { fg = C.blue_3 },
+  AerialGuide = { fg = C.grey_2 },
   AerialBooleanIcon = { link = "TSBoolean" },
+  AerialClassIcon = { link = "TSType" },
   AerialConstantIcon = { link = "TSConstant" },
   AerialConstructorIcon = { link = "TSConstructor" },
   AerialFieldIcon = { link = "TSField" },
@@ -257,7 +258,19 @@ local others = {
   AerialTypeParameterIcon = { link = "TSParameter" },
   AerialPropertyIcon = { link = "TSProperty" },
   AerialStringIcon = { link = "TSString" },
-  AerialVariableIcon = { link = "TSVariable" },
+  AerialVariableIcon = { link = "TSConstant" },
+  AerialEnumMemberIcon = { link = "TSField" },
+  AerialEnumIcon = { link = "TSType" },
+  AerialFileIcon = { link = "TSURI" },
+  AerialModuleIcon = { link = "TSNamespace" },
+  AerialPackageIcon = { link = "TSNamespace" },
+  AerialInterfaceIcon = { link = "TSType" },
+  AerialStructIcon = { link = "TSType" },
+  AerialEventIcon = { link = "TSType" },
+  AerialArrayIcon = { link = "TSConstant" },
+  AerialObjectIcon = { link = "TSType" },
+  AerialKeyIcon = { link = "TSType" },
+  AerialNullIcon = { link = "TSType" },
 }
 
 return others
