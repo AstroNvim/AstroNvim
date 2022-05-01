2937e1fcb617103273b573b6989ddc70135d1326
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-01 22:12:17 +0700

    disable persisted autosave

--- lua/configs/persisted.lua
+++ lua/configs/persisted.lua
@@ -6,7 +6,7 @@ function M.config()
     persisted.setup(require("core.utils").user_plugin_opts("plugins.notify", {
       dir = vim.fn.expand(vim.fn.stdpath "data" .. "/sessions/"), -- directory where session files are saved
       use_git_branch = true, -- create session files based on the branch of the git enabled repository
-      autosave = true, -- automatically save session files when exiting Neovim
+      autosave = false, -- automatically save session files when exiting Neovim
       autoload = false, -- automatically load the session for the cwd on Neovim startup
       allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
       ignored_dirs = nil, -- table of dirs that are ignored when auto-saving and auto-loading
