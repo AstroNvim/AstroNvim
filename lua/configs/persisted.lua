2e067f26052a9cc0870c3a1d0b912c0483f0561c
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-01 22:32:35 +0700

    correct the user opts in persisted

--- lua/configs/persisted.lua
+++ lua/configs/persisted.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
   local status_ok, persisted = pcall(require, "persisted")
   if status_ok then
-    persisted.setup(require("core.utils").user_plugin_opts("plugins.notify", {
+    persisted.setup(require("core.utils").user_plugin_opts("plugins.persisted", {
       use_git_branch = true, -- create session files based on the branch of the git enabled repository
       autosave = false, -- automatically save session files when exiting Neovim
     }))
