71beb1db0b82c416adc702c057d8ddfb1f682eae
Micah Halter <micah@balena.io>
2022-05-03 09:34:26 -0400

    fix(lsp): migrate resolved_capabilities to server_capabilities

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -38,7 +38,7 @@ function M.setup()
 end
 
 local function lsp_highlight_document(client)
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.documentHighlightProvider then
     vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
     vim.api.nvim_create_autocmd("CursorHold", {
       group = "lsp_document_highlight",
@@ -61,7 +61,7 @@ M.on_attach = function(client, bufnr)
     vim.lsp.buf.code_action()
   end, { desc = "LSP code action", buffer = bufnr })
   map("n", "<leader>lf", function()
-    vim.lsp.buf.formatting_sync()
+    vim.lsp.buf.format()
   end, { desc = "Format code", buffer = bufnr })
   map("n", "<leader>lh", function()
     vim.lsp.buf.signature_help()
@@ -98,7 +98,7 @@ M.on_attach = function(client, bufnr)
   end, { desc = "Format file with LSP" })
 
   if client.name == "tsserver" or client.name == "jsonls" or client.name == "html" or client.name == "sumneko_lua" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   local on_attach_override = user_plugin_opts("lsp.on_attach", nil, false)
