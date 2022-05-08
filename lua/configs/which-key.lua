13305af317e0453d4e5f68c4479a6a5a85458490
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-05-08 19:00:44 +0700

    Don't show which-key popup when Telescope window is opened

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -3,6 +3,14 @@ local M = {}
 function M.config()
   local status_ok, which_key = pcall(require, "which-key")
   if status_ok then
+    -- Don't show which-key popup when Telescope is opened
+    local show = which_key.show
+    which_key.show = function(keys, opts)
+      if vim.bo.filetype == "TelescopePrompt" then
+        return
+      end
+      show(keys, opts)
+    end
     which_key.setup(require("core.utils").user_plugin_opts("plugins.which-key", {
       plugins = {
         spelling = { enabled = true },
