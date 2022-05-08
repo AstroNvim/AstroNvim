689b7e911d23f24b6510d3a54c880ae8b837b806
Micah Halter <micah@balena.io>
2022-05-08 15:58:37 -0400

    Allow user override of which-key show function
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -3,13 +3,16 @@ local M = {}
 function M.config()
   local status_ok, which_key = pcall(require, "which-key")
   if status_ok then
-    -- Don't show which-key popup when Telescope is opened
     local show = which_key.show
-    which_key.show = function(keys, opts)
-      if vim.bo.filetype == "TelescopePrompt" then
-        return
+    local show_override = require("core.utils").user_plugin_opts("which-key.show", nil, false)
+    if type(show_override) == "function" then
+      which_key.show = show_override(show)
+    else
+      which_key.show = function(keys, opts)
+        if vim.bo.filetype ~= "TelescopePrompt" then
+          show(keys, opts)
+        end
       end
-      show(keys, opts)
     end
     which_key.setup(require("core.utils").user_plugin_opts("plugins.which-key", {
       plugins = {
