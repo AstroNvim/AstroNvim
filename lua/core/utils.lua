6efd75c32771af5a5816205741b30de4e9826654
Micah Halter <micah@balena.io>
2022-05-09 14:34:57 -0400

    Generalize cmp add source utility function
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -170,7 +170,7 @@ function M.add_cmp_source(source, priority)
   local cmp_avail, cmp = pcall(require, "cmp")
   if cmp_avail then
     local config = cmp.get_config()
-    table.insert(config.sources, { name = source, priority = priority })
+    table.insert(config.sources, type(source) == "table" and source or { name = source, priority = priority })
     cmp.setup(config)
   end
 end
