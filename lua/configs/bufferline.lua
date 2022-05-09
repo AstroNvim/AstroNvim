f72cf439ed0d5c133b6f97fbd0347745d2356a28
Micah Halter <micah@balena.io>
2022-05-09 14:09:23 -0400

    Fix tab highlights in bufferline
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -13,12 +13,9 @@ function M.config()
         buffer_close_icon = "",
         modified_icon = "",
         close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
         max_name_length = 14,
         max_prefix_length = 13,
         tab_size = 20,
-        view = "multiwindow",
         separator_style = "thin",
       },
     }))
