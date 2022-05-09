06e6229eafef112b3679790e2e2e9a1d1098580f
Micah Halter <micah@balena.io>
2022-05-09 13:22:39 -0400

    Use default better escape bindings
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/configs/better_escape.lua
+++ lua/configs/better_escape.lua
@@ -3,9 +3,7 @@ local M = {}
 function M.config()
   local present, better_escape = pcall(require, "better_escape")
   if present then
-    better_escape.setup(require("core.utils").user_plugin_opts("plugins.better_escape", {
-      mapping = { "ii", "jj", "jk", "kj" },
-    }))
+    better_escape.setup(require("core.utils").user_plugin_opts("plugins.better_escape", {}))
   end
 end
 
