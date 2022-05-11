4647da27709873dc4ef71f37dcc1310b6b8821f2
Micah Halter <micah@balena.io>
2022-05-11 09:56:22 -0400

    add better icons to lsp-installer to increase accessibility

--- lua/configs/nvim-lsp-installer.lua
+++ lua/configs/nvim-lsp-installer.lua
@@ -3,7 +3,15 @@ local M = {}
 function M.config()
   local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
   if status_ok then
-    lsp_installer.setup(require("core.utils").user_plugin_opts("plugins.nvim-lsp-installer", {}))
+    lsp_installer.setup(require("core.utils").user_plugin_opts("plugins.nvim-lsp-installer", {
+      ui = {
+        icons = {
+          server_installed = "✓",
+          server_uninstalled = "✗",
+          server_pending = "⟳",
+        },
+      },
+    }))
   end
 end
 
