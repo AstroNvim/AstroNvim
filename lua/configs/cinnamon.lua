ed9c2eaeb65ec1f5acf27e7e2fb9412718095d5e
Micah Halter <micah@balena.io>
2022-05-17 10:28:23 -0400

    chore(cinnamon): use plugins default configuration

--- lua/configs/cinnamon.lua
+++ lua/configs/cinnamon.lua
@@ -3,10 +3,7 @@ local M = {}
 function M.config()
   local status_ok, cinnamon = pcall(require, "cinnamon")
   if status_ok then
-    cinnamon.setup(astronvim.user_plugin_opts("plugins.cinnamon", {
-      extra_keymaps = true,
-      extended_keymaps = true,
-    }))
+    cinnamon.setup(astronvim.user_plugin_opts("plugins.cinnamon", {}))
   end
 end
 
