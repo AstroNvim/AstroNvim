6fed94af0186c320b4c6256f2eae3b85c8950cb6
Micah Halter <micah@balena.io>
2022-05-26 23:03:07 -0400

    fix(cmp): don't select automatically on enter

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -83,7 +83,7 @@ function M.config()
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
         },
-        ["<CR>"] = cmp.mapping.confirm { select = true },
+        ["<CR>"] = cmp.mapping.confirm { select = false },
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
