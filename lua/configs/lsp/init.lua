ab2ca4f83818b8ff2d010eb7893628d84c5ac155
Micah Halter <micah@balena.io>
2022-06-02 09:21:09 -0400

    chore(lsp): make astronvim lsp functions globally accessible

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -1,13 +1,40 @@
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if status_ok then
+  require "configs.lsp.handlers"
   local insert = table.insert
   local tbl_deep_extend = vim.tbl_deep_extend
   local tbl_contains = vim.tbl_contains
+  local sign_define = vim.fn.sign_define
   local user_plugin_opts = astronvim.user_plugin_opts
   local user_registration = user_plugin_opts("lsp.server_registration", nil, false)
-  local handlers = require "configs.lsp.handlers"
-  local on_attach = handlers.on_attach
-  handlers.setup()
+  local on_attach = astronvim.lsp.on_attach
+
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
+  for _, sign in ipairs(signs) do
+    sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
+  vim.diagnostic.config(user_plugin_opts("diagnostics", {
+    virtual_text = true,
+    signs = { active = signs },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }))
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
   local servers = user_plugin_opts "lsp.servers"
   local skip_setup = user_plugin_opts "lsp.skip_setup"
@@ -27,7 +54,7 @@ if status_ok then
           end
           on_attach(client, bufnr)
         end,
-        capabilities = tbl_deep_extend("force", handlers.capabilities, lspconfig[server].capabilities or {}),
+        capabilities = tbl_deep_extend("force", astronvim.lsp.capabilities, lspconfig[server].capabilities or {}),
       }
       local present, av_overrides = pcall(require, "configs.lsp.server-settings." .. server)
       if present then
