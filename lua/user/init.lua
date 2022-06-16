34078f03bdfb6ab91e60aa8cc80b57d4020b3703
Przemysław Pająk <office@fearlessspider.com>
2022-06-16 13:33:15 +0200

    Added user config to repo

---
+++ lua/user/init.lua
@@ -0,0 +1,39 @@
+return {
+  colorscheme = "dayfox",
+  plugins = {
+    init = {
+      {
+        "EdenEast/nightfox.nvim",
+        config = function()
+          require("nightfox").setup {
+            -- disable extra plugins that AstroNvim doesn't use (this is optional)
+            modules = { 
+              barbar = false,
+              dashboard = false,
+              fern = false,
+              fidget = false,
+              gitgutter = false,
+              glyph_palette = false,
+              illuminate = false,
+              lightspeed = false,
+              lsp_saga = false,
+              lsp_trouble = false,
+              modes = false,
+              neogit = false,
+              nvimtree = false,
+              pounce = false,
+              sneak = false,
+              symbols_outline = false,
+            },
+            groups = {
+              all = {
+                -- add highlight group for AstroNvim's built in URL highlighting
+                HighlightURL = { style = "underline" },
+              },
+            },
+          }
+        end,
+      },
+    },
+  },
+}
