13158040000edcd1be812262721112dd63ef060b
Micah Halter <micah@balena.io>
2022-06-21 11:13:01 -0400

    feat(core): add new section for configuring keymaps

--- lua/core/utils/init.lua
+++ lua/core/utils/init.lua
@@ -1,6 +1,7 @@
 _G.astronvim = {}
 local stdpath = vim.fn.stdpath
 local tbl_insert = table.insert
+local map = vim.keymap.set
 
 local supported_configs = {
   stdpath "config",
@@ -244,6 +245,23 @@ end
 
 function astronvim.is_available(plugin)
   return packer_plugins ~= nil and packer_plugins[plugin] ~= nil
+end
+
+function astronvim.set_mappings(map_table, base)
+  for mode, maps in pairs(map_table) do
+    for keymap, options in pairs(maps) do
+      if options then
+        local cmd = options
+        if type(options) == "table" then
+          cmd = options[1]
+          options[1] = nil
+        else
+          options = {}
+        end
+        map(mode, keymap, cmd, vim.tbl_deep_extend("force", options, base or {}))
+      end
+    end
+  end
 end
 
 function astronvim.delete_url_match()
