c72e503588a0086588b1e57ba86c4c1b673864a1
Micah Halter <micah@balena.io>
2022-07-05 09:58:07 -0400

    refactor(default_theme)!: deprecate diagnostic_style option in favor or just overriding highlights

--- lua/user_example/init.lua
+++ lua/user_example/init.lua
@@ -37,6 +37,13 @@ local config = {
       local C = require "default_theme.colors"
 
       highlights.Normal = { fg = C.fg, bg = C.bg }
+
+      -- New approach instead of diagnostic_style
+      highlights.DiagnosticError.italic = true
+      highlights.DiagnosticHint.italic = true
+      highlights.DiagnosticInfo.italic = true
+      highlights.DiagnosticWarn.italic = true
+
       return highlights
     end,
   },
@@ -77,8 +84,6 @@ local config = {
 
   -- Default theme configuration
   default_theme = {
-    -- set the highlight style for diagnostic messages
-    diagnostics_style = { italic = true },
     -- Modify the color palette for the default theme
     colors = {
       fg = "#abb2bf",
