24d71ca59fbaf7fa92623327c8e787535e62f435
Micah Halter <micah@balena.io>
2022-07-06 12:20:12 -0400

    chore(lsp): remove unnecessary lines in lsp mappings

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -11,91 +11,78 @@ astronvim.lsp.on_attach = function(client, bufnr)
             vim.lsp.buf.hover()
           end,
           desc = "Hover symbol details",
-          buffer = bufnr,
         },
         ["<leader>la"] = {
           function()
             vim.lsp.buf.code_action()
           end,
           desc = "LSP code action",
-          buffer = bufnr,
         },
         ["<leader>lf"] = {
           function()
             vim.lsp.buf.formatting_sync()
           end,
           desc = "Format code",
-          buffer = bufnr,
         },
         ["<leader>lh"] = {
           function()
             vim.lsp.buf.signature_help()
           end,
           desc = "Signature help",
-          buffer = bufnr,
         },
         ["<leader>lr"] = {
           function()
             vim.lsp.buf.rename()
           end,
           desc = "Rename current symbol",
-          buffer = bufnr,
         },
         ["gD"] = {
           function()
             vim.lsp.buf.declaration()
           end,
           desc = "Declaration of current symbol",
-          buffer = bufnr,
         },
         ["gI"] = {
           function()
             vim.lsp.buf.implementation()
           end,
           desc = "Implementation of current symbol",
-          buffer = bufnr,
         },
         ["gd"] = {
           function()
             vim.lsp.buf.definition()
           end,
           desc = "Show the definition of current symbol",
-          buffer = bufnr,
         },
         ["gr"] = {
           function()
             vim.lsp.buf.references()
           end,
           desc = "References of current symbol",
-          buffer = bufnr,
         },
         ["<leader>ld"] = {
           function()
             vim.diagnostic.open_float()
           end,
           desc = "Hover diagnostics",
-          buffer = bufnr,
         },
         ["[d"] = {
           function()
             vim.diagnostic.goto_prev()
           end,
           desc = "Previous diagnostic",
-          buffer = bufnr,
         },
         ["]d"] = {
           function()
             vim.diagnostic.goto_next()
           end,
           desc = "Next diagnostic",
-          buffer = bufnr,
         },
         ["gl"] = {
           function()
             vim.diagnostic.open_float()
           end,
           desc = "Hover diagnostics",
-          buffer = bufnr,
         },
       },
     }),
