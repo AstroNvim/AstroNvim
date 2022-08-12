aa768d177a3faf7444472791c9a8ef2a93ee9c89
Micah Halter <micah@balena.io>
2022-08-12 14:16:32 -0400

    fix(mappings): update Comment.nvim mappings to new API

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -46,9 +46,9 @@ end
 
 -- Comment
 if is_available "Comment.nvim" then
-  maps.n["<leader>/"] = { function() require("Comment.api").toggle_current_linewise() end, desc = "Comment line" }
+  maps.n["<leader>/"] = { function() require("Comment.api").toggle.linewise.current() end, desc = "Comment line" }
   maps.v["<leader>/"] = {
-    "<esc><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<cr>",
+    "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
     desc = "Toggle comment line",
   }
 end
