4adac6f164694e8170f5db11db1804bada06467c
Nguyen Phuc Nguyen <phucnguyen1999.nn@gmail.com>
2022-08-13 17:13:33 -0400

    fix(Comment): fixed pre_hook function

--- lua/configs/Comment.lua
+++ lua/configs/Comment.lua
@@ -4,14 +4,14 @@ local utils = require "Comment.utils"
 Comment.setup(astronvim.user_plugin_opts("plugins.Comment", {
   pre_hook = function(ctx)
     local location = nil
-    if ctx.ctype == utils.ctype.block then
+    if ctx.ctype == utils.ctype.blockwise then
       location = require("ts_context_commentstring.utils").get_cursor_location()
     elseif ctx.cmotion == utils.cmotion.v or ctx.cmotion == utils.cmotion.V then
       location = require("ts_context_commentstring.utils").get_visual_start_location()
     end
 
     return require("ts_context_commentstring.internal").calculate_commentstring {
-      key = ctx.ctype == utils.ctype.line and "__default" or "__multiline",
+      key = ctx.ctype == utils.ctype.linewise and "__default" or "__multiline",
       location = location,
     }
   end,
