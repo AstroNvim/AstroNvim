00640c1cc8617ed3913355c5dd6d6690f937b708
Micah Halter <micah@balena.io>
2022-08-29 12:36:53 -0400

    chore: move away from deprecated parts of Neovim

--- lua/core/utils/status.lua
+++ lua/core/utils/status.lua
@@ -321,7 +321,7 @@ function astronvim.status.provider.lsp_client_names(opts)
   opts = astronvim.default_tbl(opts, { expand_null_ls = true, truncate = 0.25 })
   return function()
     local buf_client_names = {}
-    for _, client in pairs(vim.lsp.buf_get_clients(0)) do
+    for _, client in pairs(vim.lsp.get_active_clients { bufnr = 0 }) do
       if client.name == "null-ls" and opts.expand_null_ls then
         local null_ls_sources = {}
         for _, type in ipairs { "FORMATTING", "DIAGNOSTICS" } do
