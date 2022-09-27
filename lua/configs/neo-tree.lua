32477e0bf8343f0da862d6cb44ad8c3a32216d4b
Micah Halter <micah@balena.io>
2022-09-27 12:16:08 -0400

    chore(neo-tree): simplify neo-tree default options

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -2,12 +2,10 @@ local status_ok, neotree = pcall(require, "neo-tree")
 if not status_ok then return end
 neotree.setup(astronvim.user_plugin_opts("plugins.neo-tree", {
   close_if_last_window = true,
-  popup_border_style = "rounded",
   enable_diagnostics = false,
   default_component_configs = {
     indent = {
       padding = 0,
-      with_expanders = false,
     },
     icon = {
       folder_closed = astronvim.get_icon "FolderClosed",
@@ -36,25 +34,9 @@ neotree.setup(astronvim.user_plugin_opts("plugins.neo-tree", {
     },
   },
   filesystem = {
-    filtered_items = {
-      visible = false,
-      hide_dotfiles = true,
-      hide_gitignored = false,
-      hide_by_name = {
-        ".DS_Store",
-        "thumbs.db",
-        "node_modules",
-        "__pycache__",
-      },
-    },
     follow_current_file = true,
     hijack_netrw_behavior = "open_current",
     use_libuv_file_watcher = true,
-  },
-  git_status = {
-    window = {
-      position = "float",
-    },
   },
   event_handlers = {
     { event = "neo_tree_buffer_enter", handler = function(_) vim.opt_local.signcolumn = "auto" end },
