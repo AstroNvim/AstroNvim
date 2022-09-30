69a78e873a7cdc5039afb02dec3401e67b004a4a
Micah Halter <micah@balena.io>
2022-09-30 14:53:07 -0400

    fix(lsp): schedule language server setup for after astronvim loads

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,4 +4,5 @@ vim.g.diagnostics_enabled = true
 vim.diagnostic.config(astronvim.lsp.diagnostics[vim.g.diagnostics_enabled and "on" or "off"])
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-vim.tbl_map(astronvim.lsp.setup, astronvim.user_plugin_opts "lsp.servers")
+-- setup language servers once AstroNvim loads
+vim.schedule(function() vim.tbl_map(astronvim.lsp.setup, astronvim.user_plugin_opts "lsp.servers") end)
