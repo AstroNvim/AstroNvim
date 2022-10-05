7f5e366c4774b5ff2fd1b9862682d22b915b59fc
Micah Halter <micah@balena.io>
2022-10-05 14:01:34 -0500

    chore: fix typo in comment

--- lua/configs/aerial.lua
+++ lua/configs/aerial.lua
@@ -13,10 +13,10 @@ aerial.setup(astronvim.user_plugin_opts("plugins.aerial", {
     whitespace = "  ",
   },
   on_attach = function(bufnr)
-    -- Jump forwards/backwards with '{' and '}'
+    -- Jump forwards/backwards with '[y' and ']y'
     vim.keymap.set("n", "[y", "<cmd>AerialPrev<cr>", { buffer = bufnr, desc = "Previous Aerial" })
     vim.keymap.set("n", "]y", "<cmd>AerialNext<cr>", { buffer = bufnr, desc = "Next Aerial" })
-    -- Jump up the tree with '[[' or ']]'
+    -- Jump up the tree with '[Y' or ']Y'
     vim.keymap.set("n", "[Y", "<cmd>AerialPrevUp<cr>", { buffer = bufnr, desc = "Previous and Up in Aerial" })
     vim.keymap.set("n", "]Y", "<cmd>AerialNextUp<cr>", { buffer = bufnr, desc = "Next and Up in Aerial" })
   end,
