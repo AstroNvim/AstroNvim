b1ba6b558e482354133415545508812e6f75304b
Micah Halter <micah@balena.io>
2022-10-10 10:59:25 -0400

    chore(user_example): set spell to false in user_example

--- lua/user_example/init.lua
+++ lua/user_example/init.lua
@@ -44,7 +44,7 @@ local config = {
       -- set to true or false etc.
       relativenumber = true, -- sets vim.opt.relativenumber
       number = true, -- sets vim.opt.number
-      spell = true, -- sets vim.opt.spell
+      spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
       wrap = false, -- sets vim.opt.wrap
     },
