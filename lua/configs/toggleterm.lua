e45c7ea16aa3af6a83f08dc7a49ae7e34d893891
Micah Halter <micah@balena.io>
2022-10-10 09:41:42 -0400

    fix(mappings): use F7 for toggling terminal

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -2,7 +2,7 @@ local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then return end
 toggleterm.setup(astronvim.user_plugin_opts("plugins.toggleterm", {
   size = 10,
-  open_mapping = [[<C-'>]],
+  open_mapping = [[<F7>]],
   shading_factor = 2,
   direction = "float",
   float_opts = {
