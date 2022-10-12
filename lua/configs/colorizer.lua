e889ff790490a32f9a1cd88709e4945aa4aab144
Micah Halter <micah@balena.io>
2022-10-12 13:37:01 -0400

    chore(colorizer): remove redefinition of default plugin options

--- lua/configs/colorizer.lua
+++ lua/configs/colorizer.lua
@@ -1,17 +1,3 @@
 local status_ok, colorizer = pcall(require, "colorizer")
 if not status_ok then return end
-local colorizer_opts = astronvim.user_plugin_opts("plugins.colorizer", {
-  filetypes = { "*" },
-  user_default_options = {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    names = false, -- "Name" codes like Blue
-    RRGGBBAA = false, -- #RRGGBBAA hex codes
-    rgb_fn = false, -- CSS rgb() and rgba() functions
-    hsl_fn = false, -- CSS hsl() and hsla() functions
-    css = false, -- Enable all css features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    mode = "background", -- Set the display mode
-  },
-})
-colorizer.setup(colorizer_opts)
+colorizer.setup(astronvim.user_plugin_opts("plugins.colorizer", { user_default_options = { names = false } }))
