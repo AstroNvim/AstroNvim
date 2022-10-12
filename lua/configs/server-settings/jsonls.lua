45cb8aff3557ba76aa93d62852fe99070bdefdd6
Micah Halter <micah@balena.io>
2022-10-12 10:36:25 -0400

    chore(lsp): remove hard coded settings for pyright and rust_analyzer as it leads to confusion

--- lua/configs/server-settings/jsonls.lua
+++ lua/configs/server-settings/jsonls.lua
@@ -4,11 +4,4 @@ return {
       schemas = require("schemastore").json.schemas(),
     },
   },
-  setup = {
-    commands = {
-      Format = {
-        function() vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 }) end,
-      },
-    },
-  },
 }
