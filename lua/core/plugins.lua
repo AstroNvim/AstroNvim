55e9db853de73a58fb73a2807c65964d820d4ab6
Micah Halter <micah@balena.io>
2022-10-13 13:51:51 -0400

    chore(plugins): move back to the main repo for nvim-window-picker

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -53,10 +53,8 @@ local astro_plugins = {
   -- Better buffer closing
   ["famiu/bufdelete.nvim"] = { cmd = { "Bdelete", "Bwipeout" } },
 
-  ["mehalter/nvim-window-picker"] = {
-    -- UNCOMMENT WHEN MOVE BACK TO ORIGINAL REPO
-    -- ["s1n7ax/nvim-window-picker"] = {
-    --  tag = "v1.*",
+  ["s1n7ax/nvim-window-picker"] = {
+    tag = "v1.*",
     module = "window-picker",
     config = function() require "configs.window-picker" end,
   },
