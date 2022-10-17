c96394b479be58ae929fc024a243b66e4b0cdb60
Micah Halter <micah@balena.io>
2022-10-17 14:16:53 -0400

    chore(default_theme): clean up organization

---
+++ lua/default_theme/plugins/cmp.lua
@@ -0,0 +1,9 @@
+return {
+  CmpItemAbbrDefault = { fg = C.fg },
+  CmpItemAbbrDeprecated = { fg = C.grey_2 },
+  CmpItemAbbrMatch = { fg = C.white },
+  CmpItemAbbrMatchFuzzy = { fg = C.white },
+  CmpItemKind = { fg = C.yellow },
+  CmpItemAbbr = { fg = C.fg },
+  CmpItemMenuDefault = { fg = C.fg },
+}
