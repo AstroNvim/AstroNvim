b7f0cec429d050173fce32d7d62ebda4d5cfa62b
Micah Halter <micah@balena.io>
2022-10-19 09:54:30 -0400

    chore(telescope): remove redefined default config values

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -2,18 +2,13 @@ local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then return end
 local actions = require "telescope.actions"
 
-astronvim.conditional_func(telescope.load_extension, pcall(require, "notify"), "notify")
-astronvim.conditional_func(telescope.load_extension, pcall(require, "aerial"), "aerial")
-
 telescope.setup(astronvim.user_plugin_opts("plugins.telescope", {
   defaults = {
 
     prompt_prefix = " ",
     selection_caret = "❯ ",
     path_display = { "truncate" },
-    selection_strategy = "reset",
     sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
         prompt_position = "top",
@@ -32,64 +27,13 @@ telescope.setup(astronvim.user_plugin_opts("plugins.telescope", {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
-
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
       },
-
-      n = {
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-      },
+      n = { ["q"] = actions.close },
     },
   },
-  pickers = {},
-  extensions = {},
 }))
+
+astronvim.conditional_func(telescope.load_extension, pcall(require, "notify"), "notify")
+astronvim.conditional_func(telescope.load_extension, pcall(require, "aerial"), "aerial")
