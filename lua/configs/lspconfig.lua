2c4fadef2a8d431b85022c78faf9bb544bc5d535
Micah Halter <micah@balena.io>
2022-10-25 09:51:25 -0400

    chore(lspconfig): don't schedule lsp setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,5 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then return end
-
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
--- setup language servers once AstroNvim loads
-vim.schedule(function() vim.tbl_map(astronvim.lsp.setup, astronvim.user_plugin_opts "lsp.servers") end)
+vim.tbl_map(astronvim.lsp.setup, astronvim.user_plugin_opts "lsp.servers")
