28bdb9800c1fdaa0f2063a2073e1af1c9c9faa6d
Micah Halter <micah@balena.io>
2022-10-27 10:56:26 -0400

    feat(mappings): Start adding debugging mappings

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -110,6 +110,16 @@ if is_available "neo-tree.nvim" then
   })
 end
 
+if is_available "nvim-dap-ui" then
+  vim.api.nvim_create_augroup("dapui", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    desc = "Make q close dap floating windows",
+    group = "dapui",
+    pattern = "dap-float",
+    callback = function() vim.keymap.set("n", "q", "<cmd>close!<cr>") end,
+  })
+end
+
 augroup("astronvim_highlights", { clear = true })
 cmd({ "VimEnter", "ColorScheme" }, {
   desc = "Load custom highlights from user configuration",
