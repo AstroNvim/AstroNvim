9a942cc7b7bf27a7bf7102e5c77a0d80bf503cfd
Micah Halter <micah@mehalter.com>
2022-11-02 13:22:48 -0400

    chore(which-key): simplify which-key disabling for filetypes

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -1,11 +1,5 @@
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then return end
-local show = which_key.show
-local show_override = astronvim.user_plugin_opts("which-key.show", nil, false)
-which_key.show = type(show_override) == "function" and show_override(show)
-  or function(keys, opts)
-    if vim.bo.filetype ~= "TelescopePrompt" then show(keys, opts) end
-  end
 which_key.setup(astronvim.user_plugin_opts("plugins.which-key", {
   plugins = {
     spelling = { enabled = true },
@@ -14,5 +8,8 @@ which_key.setup(astronvim.user_plugin_opts("plugins.which-key", {
   window = {
     border = "rounded",
     padding = { 2, 2, 2, 2 },
+  },
+  disable = {
+    filetypes = { "TelescopePrompt", "neo-tree" },
   },
 }))
