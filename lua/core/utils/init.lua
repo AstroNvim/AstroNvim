36dcd25d4443731e2e4d750e83d2fc9d054e5783
Micah Halter <micah@mehalter.com>
2022-12-13 11:37:48 -0500

    fix(utils): fix get_hlgroup for fully transparent highlight groups

--- lua/core/utils/init.lua
+++ lua/core/utils/init.lua
@@ -110,16 +110,11 @@ end
 function astronvim.get_hlgroup(name, fallback)
   if vim.fn.hlexists(name) == 1 then
     local hl = vim.api.nvim_get_hl_by_name(name, vim.o.termguicolors)
-    local old_true_val = hl[true]
-    hl[true] = nil
-    if not vim.tbl_isempty(hl) then
-      hl[true] = old_true_val
-      if not hl["foreground"] then hl["foreground"] = "NONE" end
-      if not hl["background"] then hl["background"] = "NONE" end
-      hl.fg, hl.bg, hl.sp = hl.foreground, hl.background, hl.special
-      hl.ctermfg, hl.ctermbg = hl.foreground, hl.background
-      return hl
-    end
+    if not hl["foreground"] then hl["foreground"] = "NONE" end
+    if not hl["background"] then hl["background"] = "NONE" end
+    hl.fg, hl.bg, hl.sp = hl.foreground, hl.background, hl.special
+    hl.ctermfg, hl.ctermbg = hl.foreground, hl.background
+    return hl
   end
   return fallback
 end
