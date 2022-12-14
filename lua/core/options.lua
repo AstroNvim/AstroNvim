d5df50c1147108dfdc76353955f23da297a16224
Micah Halter <micah@mehalter.com>
2022-12-14 09:41:09 -0500

    chore(options): enable swapfile by default

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -28,7 +28,6 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     smartcase = true, -- Case sensitivie searching
     splitbelow = true, -- Splitting a new window below the current one
     splitright = true, -- Splitting a new window at the right of the current one
-    swapfile = false, -- Disable use of swapfile for the buffer
     tabstop = 2, -- Number of space in a tab
     termguicolors = true, -- Enable 24-bit RGB color in the TUI
     timeoutlen = 300, -- Length of time to wait for a mapped sequence
