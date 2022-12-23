c45f0112dd1492b193f531e71d40bc28461d3ca6
Micah Halter <micah@mehalter.com>
2022-12-23 10:36:03 -0500

    refactor!: remove AstroReload because it's buggy and not supported by lazy

--- lua/user_example/init.lua
+++ lua/user_example/init.lua
@@ -16,7 +16,6 @@ local config = {
     pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
     skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_reload = false, -- automatically reload and sync packer after a successful update
     auto_quit = false, -- automatically quit the current session after a successful update
     -- remotes = { -- easily add new remotes to track
     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
