b27be720005ad08adee3340d3d2c3eb771754003
Micah Halter <micah@mehalter.com>
2022-12-24 08:45:24 -0500

    refactor(luasnip)!: deprecate _snippet_paths luasnip configuration key

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -4,9 +4,6 @@ if user_settings.config then luasnip.config.setup(user_settings.config) end
 for _, load_type in ipairs { "vscode", "snipmate", "lua" } do
   local loader = require("luasnip.loaders.from_" .. load_type)
   loader.lazy_load()
-  -- TODO: DEPRECATE _snippet_paths option in next major version release
-  local paths = user_settings[load_type .. "_snippet_paths"]
-  if paths then loader.lazy_load { paths = paths } end
   local loader_settings = user_settings[load_type]
   if loader_settings then loader.lazy_load(loader_settings) end
 end
