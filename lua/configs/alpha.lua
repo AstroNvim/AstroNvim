3ee25fc288cff70320f07f936b52aa1382a05605
Micah Halter <micah@mehalter.com>
2022-12-25 09:35:31 -0500

    refactor(alpha): simplify alpha configuration

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -1,36 +1,33 @@
-require("alpha").setup(astronvim.user_plugin_opts("plugins.alpha", {
-  layout = {
-    { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
-    {
-      type = "text",
-      val = astronvim.user_plugin_opts("header", {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }, false),
-      opts = { position = "center", hl = "DashboardHeader" },
-    },
-    { type = "padding", val = 5 },
-    {
-      type = "group",
-      val = {
-        astronvim.alpha_button("LDR f f", "  Find File  "),
-        astronvim.alpha_button("LDR f o", "  Recents  "),
-        astronvim.alpha_button("LDR f w", "  Find Word  "),
-        astronvim.alpha_button("LDR f n", "  New File  "),
-        astronvim.alpha_button("LDR f m", "  Bookmarks  "),
-        astronvim.alpha_button("LDR S l", "  Last Session  "),
-      },
-      opts = { spacing = 1 },
-    },
-  },
-  opts = { noautocmd = true },
-}))
+local dashboard = require "alpha.themes.dashboard"
+dashboard.section.header.val = astronvim.user_plugin_opts("header", {
+  " █████  ███████ ████████ ██████   ██████",
+  "██   ██ ██         ██    ██   ██ ██    ██",
+  "███████ ███████    ██    ██████  ██    ██",
+  "██   ██      ██    ██    ██   ██ ██    ██",
+  "██   ██ ███████    ██    ██   ██  ██████",
+  " ",
+  "    ███    ██ ██    ██ ██ ███    ███",
+  "    ████   ██ ██    ██ ██ ████  ████",
+  "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+  "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+  "    ██   ████   ████   ██ ██      ██",
+}, false)
+dashboard.section.header.opts.hl = "DashboardHeader"
+
+dashboard.section.buttons.val = {
+  astronvim.alpha_button("LDR f f", "  Find File  "),
+  astronvim.alpha_button("LDR f o", "  Recents  "),
+  astronvim.alpha_button("LDR f w", "  Find Word  "),
+  astronvim.alpha_button("LDR f n", "  New File  "),
+  astronvim.alpha_button("LDR f m", "  Bookmarks  "),
+  astronvim.alpha_button("LDR S l", "  Last Session  "),
+}
+
+dashboard.section.footer.val = { " ", " ", " ", "AstroNvim loaded " .. require("lazy").stats().count .. " plugins " }
+dashboard.section.footer.opts.hl = "DashboardFooter"
+
+dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
+dashboard.config.layout[3].val = 5
+dashboard.config.opts.noautocmd = true
+
+require("alpha").setup(astronvim.user_plugin_opts("plugins.alpha", dashboard.config))
