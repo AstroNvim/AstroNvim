0400426ecf6959ffe709b59e413c974d569c2206
Micah Halter <micah@mehalter.com>
2022-12-27 11:55:34 -0500

    feat: add vim.g.lsp_handlers_enabled option to disable default vim.lsp.handlers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,7 @@
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+if vim.g.lsp_handlers_enabled then
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+end
 local setup_servers = function()
   vim.tbl_map(astronvim.lsp.setup, astronvim.user_plugin_opts "lsp.servers")
   vim.cmd "silent! do FileType"
