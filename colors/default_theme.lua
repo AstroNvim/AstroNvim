2a59266feaede4a93bcdb6289e058fc773aba73e
Micah Halter <micah@mehalter.com>
2022-12-27 10:20:01 -0500

    refactor!: rename default_theme to astronvim and default_theme options to astronvim_theme

--- colors/default_theme.lua
+++
@@ -1,9 +0,0 @@
-vim.g.colors_name = "default_theme"
-
-package.loaded["default_theme"] = nil
-package.loaded["default_theme.base"] = nil
-package.loaded["default_theme.treesitter"] = nil
-package.loaded["default_theme.lsp"] = nil
-package.loaded["default_theme.others"] = nil
-
-require "default_theme"
