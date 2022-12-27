c89f90e4457c9d3801ff1ca352b3942f22a7f5af
Micah Halter <micah@mehalter.com>
2022-12-27 08:21:55 -0500

    refactor(configs)!: remove plugin specific user config abstractions

--- lua/configs/autopairs.lua
+++ lua/configs/autopairs.lua
@@ -16,12 +16,6 @@ npairs.setup(astronvim.user_plugin_opts("plugins.nvim-autopairs", {
 }))
 
 if not vim.g.autopairs_enabled then npairs.disable() end
-
-local rules = astronvim.user_plugin_opts("nvim-autopairs").add_rules
-if vim.tbl_contains({ "function", "table" }, type(rules)) then
-  npairs.add_rules(type(rules) == "function" and rules(npairs) or rules)
-end
-
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if cmp_status_ok then
   cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done { tex = false })
