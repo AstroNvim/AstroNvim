76a81a1f9151810b4d0791767df3b18ebd04b4b7
Micah Halter <micah@mehalter.com>
2022-12-28 00:37:58 -0500

    chore(which-key): simplify registration

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -1,7 +1,9 @@
 local user_plugin_opts = astronvim.user_plugin_opts
 local is_available = astronvim.is_available
+local wk = require "which-key"
+local register = wk.register
 
-require("which-key").setup(user_plugin_opts("plugins.which-key", {
+wk.setup(user_plugin_opts("plugins.which-key", {
   plugins = {
     spelling = { enabled = true },
     presets = { operators = false },
@@ -13,48 +15,30 @@ require("which-key").setup(user_plugin_opts("plugins.which-key", {
   disable = { filetypes = { "TelescopePrompt" } },
 }))
 
-local mappings = {
-  n = {
-    ["<leader>"] = {
-      f = { name = "File" },
-      p = { name = "Packages" },
-      l = { name = "LSP" },
-      u = { name = "UI" },
-    },
-  },
-}
-local extra_sections = {
-  b = "Buffers",
-  D = "Debugger",
-  g = "Git",
-  s = "Search",
-  S = "Session",
-  t = "Terminal",
-}
+register({
+  f = { name = "File" },
+  p = { name = "Packages" },
+  l = { name = "LSP" },
+  u = { name = "UI" },
+}, { prefix = "<leader>" })
 
-local function init_table(mode, prefix, idx)
-  if not mappings[mode][prefix][idx] then mappings[mode][prefix][idx] = { name = extra_sections[idx] } end
-end
+local sections = {
+  b = { name = "Buffers" },
+  D = { name = "Debugger" },
+  g = { name = "Git" },
+  s = { name = "Search" },
+  S = { name = "Session" },
+  t = { name = "Terminal" },
+}
 
 if is_available "Comment.nvim" then
-  for _, mode in ipairs { "n", "v" } do
-    if not mappings[mode] then mappings[mode] = {} end
-    if not mappings[mode].g then mappings[mode].g = {} end
-    mappings[mode].g.c = "Comment toggle linewise"
-    mappings[mode].g.b = "Comment toggle blockwise"
-  end
+  local comments = { c = "Comment toggle linewise", b = "Comment toggle blockwise" }
+  register(comments, { mode = "n", prefix = "g" })
+  register(comments, { mode = "v", prefix = "g" })
 end
-if is_available "gitsigns.nvim" then init_table("n", "<leader>", "g") end
-if is_available "heirline.nvim" then init_table("n", "<leader>", "b") end
-if is_available "neovim-session-manager" then init_table("n", "<leader>", "S") end
-if is_available "nvim-dap" then init_table("n", "<leader>", "D") end
-if is_available "telescope.nvim" then
-  init_table("n", "<leader>", "s")
-  init_table("n", "<leader>", "g")
-end
-if is_available "toggleterm.nvim" then
-  init_table("n", "<leader>", "g")
-  init_table("n", "<leader>", "t")
-end
-
-astronvim.which_key_register(mappings)
+if is_available "gitsigns.nvim" then register({ g = sections["g"] }, { prefix = "<leader>" }) end
+if is_available "heirline.nvim" then register({ b = sections["b"] }, { prefix = "<leader>" }) end
+if is_available "neovim-session-manager" then register({ S = sections["S"] }, { prefix = "<leader>" }) end
+if is_available "nvim-dap" then register({ D = sections["D"] }, { prefix = "<leader>" }) end
+if is_available "telescope.nvim" then register({ s = sections["s"], g = sections["g"] }, { prefix = "<leader>" }) end
+if is_available "toggleterm.nvim" then register({ g = sections["g"], t = sections["t"] }, { prefix = "<leader>" }) end
