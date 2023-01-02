29e640b23aa04cd21cc2737261a0cb46b7e1240b
Micah Halter <micah@mehalter.com>
2023-01-02 10:45:32 -0500

    chore(options): set new diffopt linematch and splitkeep opts

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,7 @@
 vim.opt.shortmess:append { s = true, I = true } -- disable startup message
+if vim.fn.has "nvim-0.9" == 1 then -- TODO v3 REMOVE THIS CONDITIONAL
+  vim.opt.diffopt:append "linematch:60" -- enable linematch diff algorithm
+end
 astronvim.vim_opts(astronvim.user_plugin_opts("options", {
   opt = {
     backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
@@ -26,6 +29,8 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     signcolumn = "yes", -- Always show the sign column
     smartcase = true, -- Case sensitivie searching
     splitbelow = true, -- Splitting a new window below the current one
+    -- TODO v3 REMOVE THIS CONDITIONAL
+    splitkeep = vim.fn.has "nvim-0.9" == 1 and "screen" or nil, -- Maintain code view when splitting
     splitright = true, -- Splitting a new window at the right of the current one
     tabstop = 2, -- Number of space in a tab
     termguicolors = true, -- Enable 24-bit RGB color in the TUI
