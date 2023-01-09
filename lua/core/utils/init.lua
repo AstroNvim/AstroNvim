e7afb4da1578e10683869a8e1749ac9875035490
Micah Halter <micah@mehalter.com>
2023-01-09 10:15:18 -0500

    fix(utils): rebuild toggle terminals on exit to update cwd

--- lua/core/utils/init.lua
+++ lua/core/utils/init.lua
@@ -275,6 +275,7 @@ function astronvim.toggle_term_cmd(opts)
   if not terms[opts.cmd] then terms[opts.cmd] = {} end
   if not terms[opts.cmd][num] then
     if not opts.count then opts.count = vim.tbl_count(terms) * 100 + num end
+    if not opts.on_exit then opts.on_exit = function() terms[opts.cmd][num] = nil end end
     terms[opts.cmd][num] = require("toggleterm.terminal").Terminal:new(opts)
   end
   -- toggle the terminal
