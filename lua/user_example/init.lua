a7c54b3a8ebcae1dd5beeb916d85d7c24d4b3ed6
Micah Halter <micah@mehalter.com>
2023-01-09 15:08:05 -0500

    chore(user_example): add which-key menu name example

--- lua/user_example/init.lua
+++ lua/user_example/init.lua
@@ -214,6 +214,9 @@ local config = {
       ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
       ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
       ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+      -- tables with the `name` key will be registered with which-key if it's installed
+      -- this is useful for naming menus
+      ["<leader>b"] = { name = "Buffers" },
       -- quick save
       -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     },
