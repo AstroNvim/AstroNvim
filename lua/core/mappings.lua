e9431ea994c4be8d79bb3f5a9c018c892afc5ee5
Micah Halter <micah@mehalter.com>
2023-01-12 10:24:30 -0500

    feat(mappings): add `<leader>ft` for picking themes

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -219,6 +219,8 @@ if is_available "telescope.nvim" then
   end
   maps.n["<leader>fo"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" }
   maps.n["<leader>fr"] = { function() require("telescope.builtin").registers() end, desc = "Find registers" }
+  maps.n["<leader>ft"] =
+    { function() require("telescope.builtin").colorscheme { enable_preview = true } end, desc = "Find themes" }
   maps.n["<leader>fw"] = { function() require("telescope.builtin").live_grep() end, desc = "Find words" }
   maps.n["<leader>fW"] = {
     function()
