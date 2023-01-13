fb9dde6c75ca62fd80da17039f796b67b7a70652
Micah Halter <micah@mehalter.com>
2023-01-13 06:47:10 -0500

    chore(lazy): improve bootstrap of lazy

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,7 +1,7 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath }
-  vim.fn.system { "git", "-C", lazypath, "checkout", "tags/stable" }
+  -- stylua: ignore
+  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath }
   local oldcmdheight = vim.opt.cmdheight:get()
   vim.opt.cmdheight = 1
   vim.notify "Please wait while plugins are installed..."
