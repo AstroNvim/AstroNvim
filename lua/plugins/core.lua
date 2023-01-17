97d61056c4426d6f21b2239f5b7de82c076fd5de
Micah Halter <micah@mehalter.com>
2023-01-17 14:15:46 -0500

    chore(plugins): add more default mappings to nvim-ufo preview

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -69,6 +69,14 @@ return {
     dependencies = { "kevinhwang91/promise-async" },
     event = "BufEnter",
     opts = {
+      preview = {
+        mappings = {
+          scrollB = "<C-b>",
+          scrollF = "<C-f>",
+          scrollU = "<C-u>",
+          scrollD = "<C-d>",
+        },
+      },
       provider_selector = function(_, filetype, buftype)
         return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
           or { "treesitter", "indent" } -- if file opened, try to use treesitter if available
