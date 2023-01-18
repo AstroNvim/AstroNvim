9946ac2aafba155120fbd0d77365cd1339eba6c8
Micah Halter <micah@mehalter.com>
2023-01-18 10:31:29 -0500

    feat(theme): add highlight groups for ts-rainbow2

---
+++ lua/astronvim_theme/plugins/rainbow2.lua
@@ -0,0 +1,9 @@
+return {
+  TSRainbowRed = { fg = C.red_1 },
+  TSRainbowYellow = { fg = C.yellow_1 },
+  TSRainbowBlue = { fg = C.blue_1 },
+  TSRainbowOrange = { fg = C.orange_1 },
+  TSRainbowGreen = { fg = C.green },
+  TSRainbowViolet = { fg = C.purple },
+  TSRainbowCyan = { fg = C.cyan },
+}
