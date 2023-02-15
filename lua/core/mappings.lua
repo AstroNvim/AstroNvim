eaa7b088bbfbfd0a0805c0238e7e759feb752c4d
Micah Halter <micah@mehalter.com>
2023-02-15 11:34:14 -0500

    feat(mappings): make `<leader>o` toggle focus to explorer

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -139,7 +139,16 @@ end
 -- NeoTree
 if is_available "neo-tree.nvim" then
   maps.n["<leader>e"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" }
-  maps.n["<leader>o"] = { "<cmd>Neotree focus<cr>", desc = "Focus Explorer" }
+  maps.n["<leader>o"] = {
+    function()
+      if vim.bo.filetype == "neo-tree" then
+        vim.cmd.wincmd "p"
+      else
+        vim.cmd.Neotree "focus"
+      end
+    end,
+    desc = "Toggle Explorer Focus",
+  }
 end
 
 -- Session Manager
