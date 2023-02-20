86672f4eeb1c678c9884b541805a7bd55952e13b
Micah Halter <micah@mehalter.com>
2023-02-20 08:09:35 -0500

    feat(nvim-ufo): use lsp for folding if available before

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -66,8 +66,21 @@ return {
         },
       },
       provider_selector = function(_, filetype, buftype)
+        local function handleFallbackException(bufnr, err, providerName)
+          if type(err) == "string" and err:match "UfoFallbackException" then
+            return require("ufo").getFolds(bufnr, providerName)
+          else
+            return require("promise").reject(err)
+          end
+        end
+
         return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or { "treesitter", "indent" } -- if file opened, try to use treesitter if available
+          or function(bufnr)
+            return require("ufo")
+              .getFolds(bufnr, "lsp")
+              :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+              :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+          end
       end,
     },
   },
