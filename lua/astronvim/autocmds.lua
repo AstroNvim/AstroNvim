28b41d8e99fa03703aaf6c6c675de2b9f1b39a04
Micah Halter <micah@mehalter.com>
2023-02-27 11:17:18 -0500

    feat(autocmds): add autocommand to save cursor position and fold status for real files

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -57,6 +57,28 @@ autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
   callback = function() utils.set_url_match() end,
 })
 
+local view_group = vim.api.nvim_create_augroup("auto_view", { clear = true })
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  desc = "Save view with mkview for real files",
+  group = view_group,
+  callback = function(event)
+    if vim.b[event.buf].view_activated then vim.cmd.mkview() end
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  desc = "Try to load file view if available and enable view saving for real files",
+  group = view_group,
+  callback = function(event)
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = event.buf })
+    local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit" }
+    if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+      vim.b[event.buf].view_activated = true
+      vim.cmd.loadview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+
 autocmd("TextYankPost", {
   desc = "Highlight yanked text",
   group = augroup("highlightyank", { clear = true }),
