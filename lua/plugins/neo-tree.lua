817f7c8af5a76796fba5ea70380eab47486fb005
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-02-27 19:20:06 -0800

    remote trash mapping

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -62,38 +62,6 @@ return {
           vim.fn.setreg("+", result.val)
         end
       end,
-      trash = function(state)
-        local inputs = require("neo-tree.ui.inputs")
-        local path = state.tree:get_node().path
-        local msg = "Are you sure you want to trash " .. path
-        inputs.confirm(msg, function(confirmed)
-          if not confirmed then return end
-
-          vim.fn.system { "trash", vim.fn.fnameescape(path) }
-          require("neo-tree.sources.manager").refresh(state.name)
-        end)
-      end,
-      trash_visual = function(state, selected_nodes)
-        local inputs = require("neo-tree.ui.inputs")
-
-        function GetTableLen(tbl)
-          local len = 0
-          for _ in pairs(tbl) do
-            len = len + 1
-          end
-          return len
-        end
-
-        local count = GetTableLen(selected_nodes)
-        local msg = "Are you sure you want to trash " .. count .. " files ?"
-        inputs.confirm(msg, function(confirmed)
-          if not confirmed then return end
-          for _, node in ipairs(selected_nodes) do
-            vim.fn.system { "trash", vim.fn.fnameescape(node.path) }
-          end
-          require("neo-tree.sources.manager").refresh(state.name)
-        end)
-      end,
     }
     local get_icon = require("astronvim.utils").get_icon
     return {
@@ -142,7 +110,6 @@ return {
           h = "parent_or_close",
           l = "child_or_open",
           Y = "copy_selector",
-          T = "trash",
         },
       },
       filesystem = {
