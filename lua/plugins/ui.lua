838c9abd7086091219b8b04bbf88a9ecd0e6d312
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-03 22:38:17 -0800

    change notify clearing animation to reduce ui clutter

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,7 +47,11 @@ return {
   {
     "rcarriga/nvim-notify",
     init = function() require("astronvim.utils").load_plugin_with_func("nvim-notify", vim, "notify") end,
-    opts = { stages = "fade" },
+    opts = {
+      timeout = 3000,
+      max_height = function() return math.floor(vim.o.lines * 0.75) end,
+      max_width = function() return math.floor(vim.o.columns * 0.75) end,
+    },
     config = require "plugins.configs.notify",
   },
   {
