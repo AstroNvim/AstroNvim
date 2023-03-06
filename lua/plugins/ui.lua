630a33d660c3799f25745e8791ac7d5ea51f24f7
Micah Halter <micah@mehalter.com>
2023-03-06 13:20:17 -0500

    feat(ui): make notifications appear on top of floating windows

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,6 +47,7 @@ return {
   {
     "rcarriga/nvim-notify",
     init = function() require("astronvim.utils").load_plugin_with_func("nvim-notify", vim, "notify") end,
+    opts = { on_open = function(win) vim.api.nvim_win_set_config(win, { zindex = 1000 }) end },
     config = require "plugins.configs.notify",
   },
   {
