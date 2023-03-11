20e563978a2a19c2c5f506c01c5e347e8c9c9165
Przemyslaw Pajak <office@fearlessspider.com>
2023-03-11 19:35:28 +0100

    fix(mapping): fix mapping
    fix mapping
    
    Signed-off-by: Przemyslaw Pajak

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -6,12 +6,6 @@
 return {
   -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
-    ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
-    ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
