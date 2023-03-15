3abd728bda581fab0f67f65172adfc6bc2cfec43
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-03-15 13:30:26 +0100

    feat(alpha): added startup time to alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -28,9 +28,16 @@ return {
       button("LDR S l", "  Last Session  "),
     }
 
-    dashboard.section.footer.val =
-      { " ", " ", " ", "AstroNvim loaded " .. require("lazy").stats().count .. " plugins " }
-    dashboard.section.footer.opts.hl = "DashboardFooter"
+    vim.api.nvim_create_autocmd("UIEnter", {
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+
+        dashboard.section.footer.val =
+          { " ", " ", " ", "AstroNvim loaded " .. stats.count .. " plugins   in " .. ms .. "ms" }
+        dashboard.section.footer.opts.hl = "DashboardFooter"
+      end,
+    })
 
     dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
     dashboard.config.layout[3].val = 5
