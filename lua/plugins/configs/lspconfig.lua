77f08e0d24ff1a06dd52334b726ecd31ed5b1adc
Micah Halter <micah@mehalter.com>
2023-03-23 10:30:42 -0400

    fix(lsp): make lsp handlers silent when no information

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -19,9 +19,9 @@ return function(_, _)
   lsp.setup_diagnostics(signs)
 
   if vim.g.lsp_handlers_enabled then
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", silent = true })
     vim.lsp.handlers["textDocument/signatureHelp"] =
-      vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+      vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", silent = true })
   end
   local setup_servers = function()
     vim.tbl_map(require("astronvim.utils.lsp").setup, astronvim.user_opts "lsp.servers")
