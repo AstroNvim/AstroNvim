3b77deb5ad6c23ceaa06765286feb18593d49ec4
Snooky <7481925+klxdev@users.noreply.github.com>
2023-03-26 17:47:35 -0700

    Update cmp.lua
    removing tab references as they are jumping all over.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -70,26 +70,26 @@ return {
           ["<C-y>"] = cmp.config.disable,
           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
           ["<CR>"] = cmp.mapping.confirm { select = false },
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          --["<Tab>"] = cmp.mapping(function(fallback)
+          --  if cmp.visible() then
+          --    cmp.select_next_item()
+          --  elseif luasnip.expand_or_jumpable() then
+          --    luasnip.expand_or_jump()
+          --  elseif has_words_before() then
+          --    cmp.complete()
+          --  else
+          --    fallback()
+          --  end
+          --end, { "i", "s" }),
+          --["<S-Tab>"] = cmp.mapping(function(fallback)
+          --  if cmp.visible() then
+          --   cmp.select_prev_item()
+          --  elseif luasnip.jumpable(-1) then
+          --    luasnip.jump(-1)
+          --  else
+          --    fallback()
+          --  end
+          --end, { "i", "s" }),
         },
         sources = cmp.config.sources {
           { name = "nvim_lsp", priority = 1000 },
