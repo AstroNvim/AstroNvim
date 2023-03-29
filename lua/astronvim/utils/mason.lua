40e3ed240327519bdce15d95ac0963922d92f4c4
Micah Halter <micah@mehalter.com>
2023-03-29 10:34:46 -0400

    fix(mason): fix MasonUpdate command to work with new mason update

--- lua/astronvim/utils/mason.lua
+++ lua/astronvim/utils/mason.lua
@@ -15,10 +15,12 @@ local utils = require "astronvim.utils"
 local notify = utils.notify
 local astroevent = utils.event
 
---- Update a mason package
--- @param pkg_name string of the name of the package as defined in Mason (Not mason-lspconfig or mason-null-ls)
+--- Update specified mason packages, or just update the registries if no packages are listed
+-- @param pkg_names nil|string|list of strings of the name of the packages as defined in Mason (Not mason-lspconfig or mason-null-ls) if the value is nil then it will just update the registries
 -- @param auto_install boolean of whether or not to install a package that is not currently installed (default: True)
-function M.update(pkg_name, auto_install)
+function M.update(pkg_names, auto_install)
+  pkg_names = pkg_names or {}
+  if type(pkg_names) == "string" then pkg_names = { pkg_names } end
   if auto_install == nil then auto_install = true end
   local registry_avail, registry = pcall(require, "mason-registry")
   if not registry_avail then
@@ -26,30 +28,40 @@ function M.update(pkg_name, auto_install)
     return
   end
 
-  registry.refresh(function()
-    local pkg_avail, pkg = pcall(registry.get_package, pkg_name)
-    if not pkg_avail then
-      notify(("Mason: %s is not available"):format(pkg_name), "error")
-    else
-      if not pkg:is_installed() then
-        if auto_install then
-          notify(("Mason: Installing %s"):format(pkg.name))
-          pkg:install()
-        else
-          notify(("Mason: %s not installed"):format(pkg.name), "warn")
-        end
-      else
-        pkg:check_new_version(function(update_available, version)
-          if update_available then
-            notify(("Mason: Updating %s to %s"):format(pkg.name, version.latest_version))
-            pkg:install():on("closed", function() notify(("Mason: Updated %s"):format(pkg.name)) end)
-          else
-            notify(("Mason: No updates available for %s"):format(pkg.name))
-          end
-        end)
+  registry.update(vim.schedule_wrap(function(success, updated_registries)
+    if success then
+      local count = #updated_registries
+      if vim.tbl_count(pkg_names) == 0 then
+        notify(("Successfully updated %d %s."):format(count, count == 1 and "registry" or "registries"))
       end
+      for _, pkg_name in ipairs(pkg_names) do
+        local pkg_avail, pkg = pcall(registry.get_package, pkg_name)
+        if not pkg_avail then
+          notify(("Mason: %s is not available"):format(pkg_name), "error")
+        else
+          if not pkg:is_installed() then
+            if auto_install then
+              notify(("Mason: Installing %s"):format(pkg.name))
+              pkg:install()
+            else
+              notify(("Mason: %s not installed"):format(pkg.name), "warn")
+            end
+          else
+            pkg:check_new_version(function(update_available, version)
+              if update_available then
+                notify(("Mason: Updating %s to %s"):format(pkg.name, version.latest_version))
+                pkg:install():on("closed", function() notify(("Mason: Updated %s"):format(pkg.name)) end)
+              else
+                notify(("Mason: No updates available for %s"):format(pkg.name))
+              end
+            end)
+          end
+        end
+      end
+    else
+      notify(("Failed to update registries: %s"):format(updated_registries), vim.log.levels.ERROR)
     end
-  end)
+  end))
 end
 
 --- Update all packages in Mason
@@ -61,43 +73,47 @@ function M.update_all()
   end
 
   notify "Mason: Checking for package updates..."
-  registry.refresh(function()
-    local installed_pkgs = registry.get_installed_packages()
-    local running = #installed_pkgs
-    local no_pkgs = running == 0
+  registry.update(vim.schedule_wrap(function(success, updated_registries)
+    if success then
+      local installed_pkgs = registry.get_installed_packages()
+      local running = #installed_pkgs
+      local no_pkgs = running == 0
 
-    if no_pkgs then
-      notify "Mason: No updates available"
-      astroevent "MasonUpdateCompleted"
-    else
-      local updated = false
-      for _, pkg in ipairs(installed_pkgs) do
-        pkg:check_new_version(function(update_available, version)
-          if update_available then
-            updated = true
-            notify(("Mason: Updating %s to %s"):format(pkg.name, version.latest_version))
-            pkg:install():on("closed", function()
+      if no_pkgs then
+        notify "Mason: No updates available"
+        astroevent "MasonUpdateCompleted"
+      else
+        local updated = false
+        for _, pkg in ipairs(installed_pkgs) do
+          pkg:check_new_version(function(update_available, version)
+            if update_available then
+              updated = true
+              notify(("Mason: Updating %s to %s"):format(pkg.name, version.latest_version))
+              pkg:install():on("closed", function()
+                running = running - 1
+                if running == 0 then
+                  notify "Mason: Update Complete"
+                  astroevent "MasonUpdateCompleted"
+                end
+              end)
+            else
               running = running - 1
               if running == 0 then
-                notify "Mason: Update Complete"
+                if updated then
+                  notify "Mason: Update Complete"
+                else
+                  notify "Mason: No updates available"
+                end
                 astroevent "MasonUpdateCompleted"
               end
-            end)
-          else
-            running = running - 1
-            if running == 0 then
-              if updated then
-                notify "Mason: Update Complete"
-              else
-                notify "Mason: No updates available"
-              end
-              astroevent "MasonUpdateCompleted"
             end
-          end
-        end)
+          end)
+        end
       end
+    else
+      notify(("Failed to update registries: %s"):format(updated_registries), vim.log.levels.ERROR)
     end
-  end)
+  end))
 end
 
 return M
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,20 +7,9 @@ return {
       "MasonUninstall",
       "MasonUninstallAll",
       "MasonLog",
+      "MasonUpdate", -- AstroNvim extension here as well
+      "MasonUpdateAll", -- AstroNvim specific
     },
-    init = function()
-      local cmd = vim.api.nvim_create_user_command
-      cmd(
-        "MasonUpdateAll",
-        function() require("astronvim.utils.mason").update_all() end,
-        { desc = "Update Mason Packages" }
-      )
-      cmd(
-        "MasonUpdate",
-        function(options) require("astronvim.utils.mason").update(options.args) end,
-        { nargs = 1, desc = "Update Mason Package" }
-      )
-    end,
     opts = {
       ui = {
         icons = {
