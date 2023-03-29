7a11c628f3a6f3726f84ac15c2bfee3d0614f2ab
Micah Halter <micah@mehalter.com>
2023-03-29 14:19:19 -0400

    feat(mason): add tab completion to MasonUpdate

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -3,11 +3,11 @@ return function(_, opts)
 
   -- TODO AstroNvim v4: change these auto command names to not conflict with core Mason commands
   local cmd = vim.api.nvim_create_user_command
-  cmd(
-    "MasonUpdate",
-    function(options) require("astronvim.utils.mason").update(options.fargs) end,
-    { nargs = "?", desc = "Update Mason Package" }
-  )
+  cmd("MasonUpdate", function(options) require("astronvim.utils.mason").update(options.fargs) end, {
+    nargs = "*",
+    desc = "Update Mason Package",
+    complete = "custom,v:lua.mason_completion.available_package_completion",
+  })
   cmd(
     "MasonUpdateAll",
     function() require("astronvim.utils.mason").update_all() end,
