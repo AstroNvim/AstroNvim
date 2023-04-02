458830f2acb633b5516dc3764e4f4fe669238bff
Micah Halter <micah@mehalter.com>
2023-04-02 10:07:24 -0400

    refactor(autocmds): generalize approach to using q to quit non-file buffers

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -85,9 +85,12 @@ autocmd("BufWinEnter", {
 autocmd("FileType", {
   desc = "Make q close help, man, quickfix, dap floats",
   group = augroup("q_close_windows", { clear = true }),
-  pattern = { "qf", "help", "man", "dap-float", "aerial-nav" },
   callback = function(event)
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true, nowait = true })
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = event.buf })
+    if buftype == "nofile" or filetype == "help" then
+      vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true, nowait = true })
+    end
   end,
 })
 
