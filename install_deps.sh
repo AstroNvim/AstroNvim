f9990e8cccae4932f360c59a047764acb925ebd3
Przemysław Pająk <office@fearlessspider.com>
2023-04-03 18:22:39 -0400

    feat(deps): fedora

--- install_deps.sh
+++ install_deps.sh
@@ -3,52 +3,57 @@
 platform='unknown'
 unamestr=$(uname)
 if [ "$unamestr" = 'Linux' ]; then
-   platform='linux'
+	platform='linux'
 elif [ "$unamestr" = 'Darwin' ]; then
-   platform='darwin'
+	platform='darwin'
 fi
 
 if [ "$platform" = "linux" ]; then
-  distro=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
-  echo "Determined platform: $distro"
-  if [ "$distro" = "Garuda Linux" ]; then
-    sudo pacman -Syy
-    sudo pacman -Su
-    sudo pacman -S git curl nodejs python erlang elixir ruby rust lua go \
-      typescript ghc perl shellcheck ripgrep fd lazygit ncdu nvm \
-      checkmake postgresql github-cli sqlite openssl readline xz zlib gum \
-      rust-analyzer iniparser fftw ncurses base-devel espeak-ng prettier \
-      luarocks hledger libtool automake portaudio astyle shfmt cppcheck \
-      lua-language-server bash-language-server haskell-language-server gopls \
-      gradle tmux
-    yay -S rebar3 hadolint rbenv cava tetris-terminal-git elixir-ls
-    gem install tmuxinator
-  elif [ "$distro" = "Ubuntu Linux" ]; then
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install git curl gum rust-analyzer libfftw3-dev \
-      libasound2-dev libncursesw5-dev libpulse-dev libtool automake \
-      cava espeak-ng gradle
-  fi
+	distro=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
+	echo "Determined platform: $distro"
+	if [ "$distro" = "Garuda Linux" ]; then
+		sudo pacman -Syy
+		sudo pacman -Su
+		sudo pacman -S git curl nodejs python erlang elixir ruby rust lua go \
+			typescript ghc perl shellcheck ripgrep fd lazygit ncdu nvm \
+			checkmake postgresql github-cli sqlite openssl readline xz zlib gum \
+			rust-analyzer iniparser fftw ncurses base-devel espeak-ng prettier \
+			luarocks hledger libtool automake portaudio astyle shfmt cppcheck \
+			lua-language-server bash-language-server haskell-language-server gopls \
+			gradle tmux
+		yay -S rebar3 hadolint rbenv cava tetris-terminal-git elixir-ls
+	elif [ "$distro" = "Ubuntu Linux" ]; then
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install git curl gum rust-analyzer libfftw3-dev \
+			libasound2-dev libncursesw5-dev libpulse-dev libtool automake \
+			cava espeak-ng gradle
+	elif [ "$distro" = "Fedora Linux" ]; then
+		sudo yum update
+		sudo yum install git curl gcc-c++ erlang elixir java python-pip go cargo nodejs luarocks rust rust-analyzer \
+			libtool automake cava espeak-ng gh rebar ruby-devel rubygems qemu-kvm bridge-utils libvirt virt-install \
+			zlib.i686 ncurses-libs.i686 bzip2-libs.i686 postgresql
+	fi
 elif [ "$platform" = "darwin" ]; then
-  echo "Determined platform: $platform" 
-  brew update && brew upgrade
-  brew install git curl node python erlang elixir ruby rust lua go \
-    typescript ghc perl rebar3 shellcheck ripgrep fd lazygit ncdu \
-    nvm hadolint checkmake postgresql gh openssl readline sqlite3 \
-    xz zlib rbenv gum rust-analyzer fftw ncurses libtool automake \
-    portaudio cava astyle shfmt cppcheck gitlint golangci-lint \
-    lua-language-server elixir-ls samtay/tui/tetris espeak autoconf-archive \
-    Code-Hex/tap/neo-cowsay youtube-dl achannarasappa/tap/ticker \
-    circumflex hledger clang-format bash-language-server haskell-language-server \
-    efm-langserver gopls gradle 
-  sudo gem update
-  sudo gem install rubocop neovim tmuxinator
-  export LIBTOOL='which glibtool'
-  export LIBTOOLIZE='which glibtoolize'
-  ln -s 'which glibtoolize' /usr/local/bin/libtoolize
-  ln -s /usr/lib/libncurses.dylib /usr/local/lib/libncursesw.dylib
+	echo "Determined platform: $platform"
+	brew update && brew upgrade
+	brew install git curl node python erlang elixir ruby rust lua go \
+		typescript ghc perl rebar3 shellcheck ripgrep fd lazygit ncdu \
+		nvm hadolint checkmake postgresql gh openssl readline sqlite3 \
+		xz zlib rbenv gum rust-analyzer fftw ncurses libtool automake \
+		portaudio cava astyle shfmt cppcheck gitlint golangci-lint \
+		lua-language-server elixir-ls samtay/tui/tetris espeak autoconf-archive \
+		Code-Hex/tap/neo-cowsay youtube-dl achannarasappa/tap/ticker \
+		circumflex hledger clang-format bash-language-server haskell-language-server \
+		efm-langserver gopls gradle
+	export LIBTOOL='which glibtool'
+	export LIBTOOLIZE='which glibtoolize'
+	ln -s 'which glibtoolize' /usr/local/bin/libtoolize
+	ln -s /usr/lib/libncurses.dylib /usr/local/lib/libncursesw.dylib
 fi
+
+sudo gem update
+sudo gem install solargraph rubocop neovim tmuxinator
 
 curl https://pyenv.run | bash
 
@@ -70,19 +75,22 @@ gh extension install dlvhdr/gh-dash
 pip install flake8 black isort cmake-language-server djlint pynvim
 
 npm i -g eslint vscode-langservers-extracted markdownlint-cli write-good \
-  fixjson @fsouza/prettierd stylelint shopify-cli cross-env webpack \
-  sass serverless npm-run-all nativescript dockerfile-language-server-nodejs \
-  neovim gulp
+	fixjson @fsouza/prettierd stylelint shopify-cli cross-env webpack \
+	sass serverless npm-run-all nativescript dockerfile-language-server-nodejs \
+	neovim gulp
 
-  curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io" | bash
+
+sdk install gradle
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 if [ -n "$ZSH_VERSION" ]; then
-  zsh_shell=true
+	zsh_shell=true
 elif [ -n "$FISH_VERSION" ]; then
-  fish_shell=true
-  curl -sL https://git.io/fisher | . && fisher install jorgebucaran/fisher
-  fisher install reitzig/sdkman-for-fish@v1.4.0
+	fish_shell=true
+	curl -sL https://git.io/fisher | . && fisher install jorgebucaran/fisher
+	fisher install reitzig/sdkman-for-fish@v1.4.0
 elif [ -n "$BASH_VERSION" ]; then
-  bash_shell=true
+	bash_shell=true
 fi
-
