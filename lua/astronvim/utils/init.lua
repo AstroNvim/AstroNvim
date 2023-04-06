9130334ebc38b3f425991a2dbbc5e8f2a4c5ae5a
logan271 <logan.luo.x@gmail.com>
2023-04-06 20:07:12 +0800

    fix(utils): update utils.cmd
    1. fix typing warning
    2. fix possible string concat error

--- lua/astronvim/utils/init.lua
+++ lua/astronvim/utils/init.lua
@@ -243,8 +243,9 @@ end
 --- @param show_error boolean Wwhether or not to show an unsuccessful command as an error to the user
 --- @return string|nil # The result of a successfully executed command or nil
 function M.cmd(cmd, show_error)
-  if vim.fn.has "win32" == 1 then cmd = { "cmd.exe", "/C", cmd } end
-  local result = vim.fn.system(cmd)
+  local wind32_cmd
+  if vim.fn.has "win32" == 1 then wind32_cmd = { "cmd.exe", "/C", cmd } end
+  local result = vim.fn.system(wind32_cmd or cmd)
   local success = vim.api.nvim_get_vvar "shell_error" == 0
   if not success and (show_error == nil or show_error) then
     vim.api.nvim_err_writeln("Error running command: " .. cmd .. "\nError message:\n" .. result)
