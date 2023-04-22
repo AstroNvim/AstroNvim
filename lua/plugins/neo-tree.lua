090ac2cf32176f10f5f6fdfeded230ed22a7dc4b
Akio <akio@pop-os.localdomain>
2023-04-22 12:23:04 +0200

    fix(neotree): replaces the deprecated tab_labels with sources as recommended by Neotree migrations

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,7 +20,7 @@ return {
         if node.type == "directory" or node:has_children() then
           if not node:is_expanded() then -- if unexpanded, expand
             state.commands.toggle_node(state)
-          else -- if expanded and has children, seleect the next child
+          else                           -- if expanded and has children, seleect the next child
             require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
           end
         else -- if not a directory just open it
@@ -48,9 +48,9 @@ return {
         for i, result in pairs(results) do
           if result.val and result.val ~= "" then
             vim.list_extend(messages, {
-              { ("%s."):format(i), "Identifier" },
+              { ("%s."):format(i),           "Identifier" },
               { (" %s: "):format(result.msg) },
-              { result.val, "String" },
+              { result.val,                  "String" },
               { "\n" },
             })
           end
@@ -70,11 +70,23 @@ return {
       source_selector = {
         winbar = true,
         content_layout = "center",
-        tab_labels = {
-          filesystem = get_icon "FolderClosed" .. " File",
-          buffers = get_icon "DefaultFile" .. " Bufs",
-          git_status = get_icon "Git" .. " Git",
-          diagnostics = get_icon "Diagnostic" .. " Diagnostic",
+        sources = {
+          {
+            source = "filesystem",
+            display_name = get_icon "FolderClosed" .. " File"
+          },
+          {
+            source = "buffers",
+            display_name = get_icon "DefaultFile" .. " Bufs"
+          },
+          {
+            source = "git_status",
+            display_name = get_icon "Git" .. " Git",
+          },
+          {
+            source = "diagnostics",
+            display_name = get_icon "Diagnostic" .. " Diagnostic"
+          }
         },
       },
       default_component_configs = {
