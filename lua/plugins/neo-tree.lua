074092acb91a70b2b3996467cfbe2cb1174b99bb
Micah Halter <micah@mehalter.com>
2023-05-08 13:53:57 -0400

    chore(plugins): provide a default list of neo-tree sources

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,7 @@ return {
   opts = {
     auto_clean_after_session_restore = true,
     close_if_last_window = true,
+    sources = { "filesystem", "buffers", "git_status" },
     source_selector = {
       winbar = true,
       content_layout = "center",
