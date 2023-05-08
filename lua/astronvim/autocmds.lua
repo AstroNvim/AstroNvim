c5f9910cc9d397ed49f3e6dc39db2c809f13ab3c
Micah Halter <micah@mehalter.com>
2023-05-08 08:38:20 -0400

    refactor(autocmds): simplify neo-tree autocmd

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -203,9 +203,8 @@ if is_available "neo-tree.nvim" then
       else
         local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
         if stats and stats.type == "directory" then
-          require "neo-tree"
           vim.api.nvim_del_augroup_by_name "neotree_start"
-          vim.api.nvim_exec_autocmds("BufEnter", {})
+          require "neo-tree"
         end
       end
     end,
