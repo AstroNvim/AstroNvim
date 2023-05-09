63f782f3457b553798996761b15f7440b4388257
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-05-09 00:03:20 -0700

    fix(plugins): add which-key description when lazy-loading Comment.nvim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -87,7 +87,10 @@ return {
   },
   {
     "numToStr/Comment.nvim",
-    keys = { { "gc", mode = { "n", "v" } }, { "gb", mode = { "n", "v" } } },
+    keys = {
+      { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+      { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+    },
     opts = function()
       local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
       return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
