03cb1f91630c4eabe81722041b522586982c1dc2
Micah Halter <micah@mehalter.com>
2023-05-25 14:17:54 -0400

    chore(LuaSnip): add jsregexp installation to LuaSnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,9 @@
 return {
   {
     "L3MON4D3/LuaSnip",
+    build = vim.fn.has "win32" ~= 0
+        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+      or nil,
     dependencies = { "rafamadriz/friendly-snippets" },
     config = require "plugins.configs.luasnip",
   },
