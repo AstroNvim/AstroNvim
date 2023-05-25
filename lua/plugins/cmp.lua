c409cf5149f550b850041e09c6501ee9023e4cdd
Micah Halter <micah@mehalter.com>
2023-05-25 08:02:25 -0400

    chore(cmp): don't pin cmp anymore

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,7 +6,6 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
-    commit = "a9c701fa7e12e9257b3162000e5288a75d280c28", -- https://github.com/hrsh7th/nvim-cmp/issues/1382
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-buffer",
