e698d95411e9cb8a63b01249a4dfeed923a5e501
Micah Halter <micah@mehalter.com>
2023-05-28 10:53:06 -0400

    revert: mode changing seems fixed upstream in neovim
    Refs: 84630bae8dc769311fab09884cb517cb1f36b681

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,23 +7,6 @@ return {
   opts = function()
     local actions = require "telescope.actions"
     local get_icon = require("astronvim.utils").get_icon
-    local mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-      n = { ["q"] = actions.close },
-    }
-    -- HACK: remove after Telescope mode issue is resolved: https://github.com/nvim-telescope/telescope.nvim/issues/2501
-    if vim.fn.has "nvim-0.10" == 1 then
-      for _, key in ipairs { "<CR>", "<C-x>", "<C-v>", "<C-t>", "<C-q>", "<M-q>" } do
-        mappings.i[key] = function()
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>" .. key, true, false, true), "i", false)
-        end
-      end
-    end
     return {
       defaults = {
         prompt_prefix = get_icon("Selected", 1),
@@ -31,18 +14,21 @@ return {
         path_display = { "truncate" },
         sorting_strategy = "ascending",
         layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-          },
-          vertical = {
-            mirror = false,
-          },
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
           width = 0.87,
           height = 0.80,
           preview_cutoff = 120,
         },
-        mappings = mappings,
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = { q = actions.close },
+        },
       },
     }
   end,
