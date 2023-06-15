ab2af38d7155bf2d659db122f4e967b20e7ce8d2
Micah Halter <micah@mehalter.com>
2023-06-15 09:03:52 -0400

    feat(utils): add helper function to calculate a spinning icon

--- lua/astronvim/utils/init.lua
+++ lua/astronvim/utils/init.lua
@@ -75,8 +75,8 @@ function M.conditional_func(func, condition, ...)
   if condition and type(func) == "function" then return func(...) end
 end
 
---- Get an icon from `lspkind` if it is available and return it
----@param kind string The kind of icon in `lspkind` to retrieve
+--- Get an icon from the AstroNvim internal icons if it is available and return it
+---@param kind string The kind of icon in astronvim.icons to retrieve
 ---@param padding? integer Padding to add to the end of the icon
 ---@param no_fallback? boolean Whether or not to disable fallback to text icon
 ---@return string icon
@@ -89,6 +89,18 @@ function M.get_icon(kind, padding, no_fallback)
   end
   local icon = M[icon_pack] and M[icon_pack][kind]
   return icon and icon .. string.rep(" ", padding or 0) or ""
+end
+
+--- Get a icon spinner table if it is available in the AstroNvim icons. Icons in format `kind1`,`kind2`, `kind3`, ...
+---@param kind string The kind of icon to check for sequential entries of
+---@return string[]|nil spinners # A collected table of spinning icons in sequential order or nil if none exist
+function M.get_spinner(kind, ...)
+  local spinner = {}
+  repeat
+    local icon = M.get_icon(("%s%d"):format(kind, #spinner + 1), ...)
+    if icon ~= "" then table.insert(spinner, icon) end
+  until not icon or icon == ""
+  if #spinner > 0 then return spinner end
 end
 
 --- Get highlight properties for a given highlight name
