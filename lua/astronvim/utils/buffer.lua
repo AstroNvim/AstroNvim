16312878f406c93434633e4c7340421c473373ea
Micah Halter <micah@mehalter.com>
2023-06-26 12:02:51 -0400

    fix(buffer): add confirmation when trying to close a modified buffer

--- lua/astronvim/utils/buffer.lua
+++ lua/astronvim/utils/buffer.lua
@@ -134,6 +134,20 @@ end
 ---@param force? boolean Whether or not to foce close the buffers or confirm changes (default: false)
 function M.close(bufnr, force)
   if utils.is_available "mini.bufremove" then
+    if not force and vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
+      local bufname = vim.fn.expand "%"
+      local empty = bufname == ""
+      if empty then bufname = "Untitled" end
+      local confirm = vim.fn.confirm(('Save changes to "%s"?'):format(bufname), "&Yes\n&No\n&Cancel", 1, "Question")
+      if confirm == 1 then
+        if empty then return end
+        vim.cmd.write()
+      elseif confirm == 2 then
+        force = true
+      else
+        return
+      end
+    end
     require("mini.bufremove").delete(bufnr, force)
   else
     vim.cmd((force and "bd!" or "confirm bd") .. (bufnr == nil and "" or bufnr))
