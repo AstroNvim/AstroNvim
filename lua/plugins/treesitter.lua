c5519d8f0a1017bfc6bce968efa7aa90753a693d
Micah Halter <micah@mehalter.com>
2023-06-26 18:26:58 -0400

    fix(treesitter): fix other conflicting treesitter mappings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -55,25 +55,21 @@ return {
         set_jumps = true,
         goto_next_start = {
           ["]k"] = { query = "@block.outer", desc = "Next block start" },
-          ["]c"] = { query = "@class.outer", desc = "Next class start" },
           ["]f"] = { query = "@function.outer", desc = "Next function start" },
           ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
         },
         goto_next_end = {
           ["]k"] = { query = "@block.outer", desc = "Next block end" },
-          ["]c"] = { query = "@class.outer", desc = "Next class end" },
           ["]f"] = { query = "@function.outer", desc = "Next function end" },
           ["]a"] = { query = "@parameter.outer", desc = "Next parameter end" },
         },
         goto_previous_start = {
           ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-          ["[c"] = { query = "@class.outer", desc = "Previous class start" },
           ["[f"] = { query = "@function.outer", desc = "Previous function start" },
           ["[a"] = { query = "@parameter.outer", desc = "Previous parameter start" },
         },
         goto_previous_end = {
           ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-          ["[C"] = { query = "@class.outer", desc = "Previous class end" },
           ["[F"] = { query = "@function.outer", desc = "Previous function end" },
           ["[A"] = { query = "@parameter.outer", desc = "Previous parameter end" },
         },
