c9718a4ab724060926f7e4b616e5720a6a9dd4cf
Micah Halter <micah@mehalter.com>
2023-06-26 14:34:09 -0400

    fix(autocmds): improve astrofile and astrogitfile detection

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -250,13 +250,16 @@ autocmd({ "VimEnter", "ColorScheme" }, {
   end,
 })
 
-autocmd({ "BufReadPost", "BufNewFile" }, {
+autocmd({ "BufReadPost", "BufNewFile", "BufWritePost" }, {
   desc = "AstroNvim user events for file detection (AstroFile and AstroGitFile)",
   group = augroup("file_user_events", { clear = true }),
   callback = function(args)
     if not (vim.fn.expand "%" == "" or vim.api.nvim_get_option_value("buftype", { buf = args.buf }) == "nofile") then
       utils.event "File"
-      if utils.cmd({ "git", "-C", vim.fn.expand "%:p:h", "rev-parse" }, false) then utils.event "GitFile" end
+      if utils.cmd({ "git", "-C", vim.fn.expand "%:p:h", "rev-parse" }, false) then
+        utils.event "GitFile"
+        vim.api.nvim_del_augroup_by_name "file_user_events"
+      end
     end
   end,
 })
