d05228a3488bf05118604c281b0c50a02307f54f
Micah Halter <micah@mehalter.com>
2023-06-27 14:35:15 -0400

    fix(treesitter): set up treesitter text object selection if mini.ai disabled

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -22,54 +22,74 @@ return {
     "TSUpdateSync",
   },
   build = ":TSUpdate",
-  opts = {
-    autotag = { enable = true },
-    context_commentstring = { enable = true, enable_autocmd = false },
-    highlight = {
-      enable = true,
-      disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
-    },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
-    textobjects = {
-      move = {
+  opts = function()
+    return {
+      autotag = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      highlight = {
         enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]k"] = { query = "@block.outer", desc = "Next block start" },
-          ["]f"] = { query = "@function.outer", desc = "Next function start" },
-          ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
+        disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
+      },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = not require("astronvim.utils").is_available "mini.ai",
+          lookahead = true,
+          keymaps = {
+            ["ak"] = "@block.outer",
+            ["ik"] = "@block.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["a?"] = "@conditional.outer",
+            ["i?"] = "@conditional.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+          },
         },
-        goto_next_end = {
-          ["]K"] = { query = "@block.outer", desc = "Next block end" },
-          ["]F"] = { query = "@function.outer", desc = "Next function end" },
-          ["]A"] = { query = "@parameter.outer", desc = "Next parameter end" },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]k"] = { query = "@block.outer", desc = "Next block start" },
+            ["]f"] = { query = "@function.outer", desc = "Next function start" },
+            ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
+          },
+          goto_next_end = {
+            ["]K"] = { query = "@block.outer", desc = "Next block end" },
+            ["]F"] = { query = "@function.outer", desc = "Next function end" },
+            ["]A"] = { query = "@parameter.outer", desc = "Next parameter end" },
+          },
+          goto_previous_start = {
+            ["[k"] = { query = "@block.outer", desc = "Previous block start" },
+            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+            ["[a"] = { query = "@parameter.outer", desc = "Previous parameter start" },
+          },
+          goto_previous_end = {
+            ["[K"] = { query = "@block.outer", desc = "Previous block end" },
+            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+            ["[A"] = { query = "@parameter.outer", desc = "Previous parameter end" },
+          },
         },
-        goto_previous_start = {
-          ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-          ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-          ["[a"] = { query = "@parameter.outer", desc = "Previous parameter start" },
-        },
-        goto_previous_end = {
-          ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-          ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-          ["[A"] = { query = "@parameter.outer", desc = "Previous parameter end" },
+        swap = {
+          enable = true,
+          swap_next = {
+            [">K"] = { query = "@block.outer", desc = "Swap next block" },
+            [">F"] = { query = "@function.outer", desc = "Swap next function" },
+            [">A"] = { query = "@parameter.inner", desc = "Swap next parameter" },
+          },
+          swap_previous = {
+            ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+            ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+            ["<A"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
+          },
         },
       },
-      swap = {
-        enable = true,
-        swap_next = {
-          [">K"] = { query = "@block.outer", desc = "Swap next block" },
-          [">F"] = { query = "@function.outer", desc = "Swap next function" },
-          [">A"] = { query = "@parameter.inner", desc = "Swap next parameter" },
-        },
-        swap_previous = {
-          ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-          ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-          ["<A"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
-        },
-      },
-    },
-  },
+    }
+  end,
   config = require "plugins.configs.nvim-treesitter",
 }
