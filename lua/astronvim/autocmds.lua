6f8db8dd7ec934c6c07e9c5e2d4537f52cfc13b9
Micah Halter <micah@mehalter.com>
2023-06-28 07:04:14 -0400

    fix(autocmds): make sure `vim.t.bufs` gets created

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -20,12 +20,12 @@ autocmd({ "BufAdd", "BufEnter", "TabNewEntered" }, {
   group = bufferline_group,
   callback = function(args)
     local buf_utils = require "astronvim.utils.buffer"
+    if not vim.t.bufs then vim.t.bufs = {} end
     if not buf_utils.is_valid(args.buf) then return end
     if args.buf ~= buf_utils.current_buf then
       buf_utils.last_buf = buf_utils.is_valid(buf_utils.current_buf) and buf_utils.current_buf or nil
       buf_utils.current_buf = args.buf
     end
-    if not vim.t.bufs then vim.t.bufs = {} end
     local bufs = vim.t.bufs
     if not vim.tbl_contains(bufs, args.buf) then
       table.insert(bufs, args.buf)
