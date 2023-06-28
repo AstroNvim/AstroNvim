dda91ab270d4d793f3bc4eab5924e1e008b57267
Micah Halter <micah@mehalter.com>
2023-06-27 22:58:52 -0400

    fix(treesitter): fix parameter query

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -57,22 +57,22 @@ return {
           goto_next_start = {
             ["]k"] = { query = "@block.outer", desc = "Next block start" },
             ["]f"] = { query = "@function.outer", desc = "Next function start" },
-            ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
+            ["]a"] = { query = "@parameter.inner", desc = "Next parameter start" },
           },
           goto_next_end = {
             ["]K"] = { query = "@block.outer", desc = "Next block end" },
             ["]F"] = { query = "@function.outer", desc = "Next function end" },
-            ["]A"] = { query = "@parameter.outer", desc = "Next parameter end" },
+            ["]A"] = { query = "@parameter.inner", desc = "Next parameter end" },
           },
           goto_previous_start = {
             ["[k"] = { query = "@block.outer", desc = "Previous block start" },
             ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-            ["[a"] = { query = "@parameter.outer", desc = "Previous parameter start" },
+            ["[a"] = { query = "@parameter.inner", desc = "Previous parameter start" },
           },
           goto_previous_end = {
             ["[K"] = { query = "@block.outer", desc = "Previous block end" },
             ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-            ["[A"] = { query = "@parameter.outer", desc = "Previous parameter end" },
+            ["[A"] = { query = "@parameter.inner", desc = "Previous parameter end" },
           },
         },
         swap = {
