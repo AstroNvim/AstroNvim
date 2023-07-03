cd81a45c1dd7c9bc4757dce5e5cd8d058d2e5f72
Micah Halter <micah@mehalter.com>
2023-07-03 11:34:55 -0400

    fix(utils): improve AstroReload command

--- lua/astronvim/utils/init.lua
+++ lua/astronvim/utils/init.lua
@@ -23,6 +23,8 @@ end
 ---@param quiet? boolean Whether or not to notify on completion of reloading
 ---@return boolean # True if the reload was successful, False otherwise
 function M.reload(quiet)
+  local was_modifiable = vim.opt.modifiable:get()
+  if not was_modifiable then vim.opt.modifiable = true end
   local core_modules = { "astronvim.bootstrap", "astronvim.options", "astronvim.mappings" }
   local modules = vim.tbl_filter(function(module) return module:find "^user%." end, vim.tbl_keys(package.loaded))
 
@@ -36,6 +38,7 @@ function M.reload(quiet)
       success = false
     end
   end
+  if not was_modifiable then vim.opt.modifiable = false end
   if not quiet then -- if not quiet, then notify of result
     if success then
       M.notify("AstroNvim successfully reloaded", vim.log.levels.INFO)
