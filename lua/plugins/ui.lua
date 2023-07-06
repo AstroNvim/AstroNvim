d56ca2207f8e02691ae13a0e550af54099e74d14
Micah Halter <micah@mehalter.com>
2023-07-06 12:45:22 -0400

    refactor: simplify notification formatting implementation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -53,8 +53,13 @@ return {
     opts = {
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 175 })
-        -- close notification immediately if notifications disabled
         if not vim.g.ui_notifications_enabled then vim.api.nvim_win_close(win, true) end
+        if require("astronvim.utils").is_available "nvim-treesitter" then
+          if not package.loaded["nvim-treesitter"] then require "nvim-treesitter" end
+          vim.wo[win].conceallevel = 3
+          vim.bo[vim.api.nvim_win_get_buf(win)].filetype = "markdown"
+          vim.wo[win].spell = false
+        end
       end,
     },
     config = require "plugins.configs.notify",
