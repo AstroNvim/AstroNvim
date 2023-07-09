efef0bf423e2aee331ef72a303f1f10a63e7278d
zhaown <51357674+chaozwn@users.noreply.github.com>
2023-07-09 21:40:21 +0800

    feat: add nvim-dap-virtual-text for nvim dap
    nvim-dap-virtual-text can show variables in line end when you debug.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,23 +1,35 @@
 return {
-  "mfussenegger/nvim-dap",
-  enabled = vim.fn.has "win32" == 0,
-  dependencies = {
     {
-      "jay-babu/mason-nvim-dap.nvim",
-      dependencies = { "nvim-dap" },
-      cmd = { "DapInstall", "DapUninstall" },
-      opts = { handlers = {} },
-    },
-    {
-      "rcarriga/nvim-dap-ui",
-      opts = { floating = { border = "rounded" } },
-      config = require "plugins.configs.nvim-dap-ui",
-    },
-    {
-      "rcarriga/cmp-dap",
-      dependencies = { "nvim-cmp" },
-      config = require "plugins.configs.cmp-dap",
-    },
+      "mfussenegger/nvim-dap",
+      enabled = vim.fn.has "win32" == 0,
+      dependencies = {
+        {
+          "jay-babu/mason-nvim-dap.nvim",
+          dependencies = { "nvim-dap" },
+          cmd = { "DapInstall", "DapUninstall" },
+          opts = { handlers = {} },
+        },
+        {
+          "rcarriga/nvim-dap-ui",
+          opts = { floating = { border = "rounded" } },
+          config = require "plugins.configs.nvim-dap-ui",
+        },
+        {
+          "rcarriga/cmp-dap",
+          dependencies = { "nvim-cmp" },
+          config = require "plugins.configs.cmp-dap",
+        },
+      },
+      event = "User AstroFile",
   },
-  event = "User AstroFile",
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    event = "User AstroFile",
+    opts = {
+      commented = true,
+      enabled = true,
+      enabled_commands = true,
+  },
 }
+
