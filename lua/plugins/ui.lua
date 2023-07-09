844d7ad87092b3195771aa8abae752c1b3ea764a
Micah Halter <micah@mehalter.com>
2023-07-09 15:10:34 -0400

    fix(ui): fix treesitter starting for notification highlighting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -54,12 +54,11 @@ return {
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 175 })
         if not vim.g.ui_notifications_enabled then vim.api.nvim_win_close(win, true) end
-        if require("astronvim.utils").is_available "nvim-treesitter" then
-          if not package.loaded["nvim-treesitter"] then require "nvim-treesitter" end
-          vim.wo[win].conceallevel = 3
-          vim.bo[vim.api.nvim_win_get_buf(win)].filetype = "markdown"
-          vim.wo[win].spell = false
-        end
+        if not package.loaded["nvim-treesitter"] then pcall(require, "nvim-treesitter") end
+        vim.wo[win].conceallevel = 3
+        local buf = vim.api.nvim_win_get_buf(win)
+        if not pcall(vim.treesitter.start, buf, "markdown") then vim.bo[buf].syntax = "markdown" end
+        vim.wo[win].spell = false
       end,
     },
     config = require "plugins.configs.notify",
