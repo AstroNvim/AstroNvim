621b79e487083766e4a2c47bbb2b71a6343b7d58
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-08 21:58:08 -0700

    fix(utils): allow terminals opened by `toggle_term_cmd` to dynamically resize

--- lua/astronvim/utils/init.lua
+++ lua/astronvim/utils/init.lua
@@ -166,7 +166,7 @@ end
 function M.toggle_term_cmd(opts)
   local terms = astronvim.user_terminals
   -- if a command string is provided, create a basic table for Terminal:new() options
-  if type(opts) == "string" then opts = { cmd = opts, hidden = true } end
+  if type(opts) == "string" then opts = { cmd = opts, hidden = false } end
   local num = vim.v.count > 0 and vim.v.count or 1
   -- if terminal doesn't exist yet, create it
   if not terms[opts.cmd] then terms[opts.cmd] = {} end
