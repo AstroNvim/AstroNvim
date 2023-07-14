528736866f9bde10cb15e2415f95d0edb1102a3a
Micah Halter <micah@mehalter.com>
2023-07-14 10:22:46 -0400

    feat: improve performance for large files

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -15,6 +15,18 @@ vim.on_key(function(char)
   end
 end, namespace "auto_hlsearch")
 
+autocmd("BufReadPre", {
+  desc = "Disable certain functionality on very large files",
+  group = augroup("large_buf", { clear = true }),
+  callback = function(args)
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+    vim.b[args.buf].large_buf = (ok and stats and stats.size > vim.g.max_file.size)
+      or vim.api.nvim_buf_line_count(args.buf) > vim.g.max_file.lines
+
+    if vim.b[args.buf].large_buf then pcall(vim.cmd.IndentBlanklineDisable) end
+  end,
+})
+
 local bufferline_group = augroup("bufferline", { clear = true })
 autocmd({ "BufAdd", "BufEnter", "TabNewEntered" }, {
   desc = "Update buffers when adding new buffers",
