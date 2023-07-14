250be6c1701c6aeec920cb36109a09212b2c41c2
Micah Halter <micah@mehalter.com>
2023-07-14 10:22:46 -0400

    feat: improve performance for large files

--- lua/astronvim/options.lua
+++ lua/astronvim/options.lua
@@ -52,6 +52,7 @@ local options = astronvim.user_opts("options", {
     mapleader = " ", -- set leader key
     maplocalleader = ",", -- set default local leader key
     -- AstroNvim specific global options
+    max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     autopairs_enabled = true, -- enable autopairs at start
     cmp_enabled = true, -- enable completion at start
