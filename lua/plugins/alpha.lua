eddd678f8782392dd06e30a9a2e91c000555cdb3
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-14 21:59:28 -0700

    fix(alpha): FileType event not emitted by alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -31,7 +31,6 @@ return {
 
     dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
     dashboard.config.layout[3].val = 5
-    dashboard.config.opts.noautocmd = true
     return dashboard
   end,
   config = require "plugins.configs.alpha",
