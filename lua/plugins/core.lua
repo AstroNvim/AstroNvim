039072e4410a871440bce144f278dbdfc0d7b4f3
Micah Halter <micah@mehalter.com>
2023-07-17 11:52:29 -0400

    fix(toggleterm): unify ToggleTerm highlights with UI

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -104,6 +104,16 @@ return {
     "akinsho/toggleterm.nvim",
     cmd = { "ToggleTerm", "TermExec" },
     opts = {
+      highlights = {
+        Normal = { link = "Normal" },
+        NormalNC = { link = "NormalNC" },
+        NormalFloat = { link = "NormalFloat" },
+        FloatBorder = { link = "FloatBorder" },
+        StatusLine = { link = "StatusLine" },
+        StatusLineNC = { link = "StatusLineNC" },
+        WinBar = { link = "WinBar" },
+        WinBarNC = { link = "WinBarNC" },
+      },
       size = 10,
       on_create = function()
         vim.opt.foldcolumn = "0"
