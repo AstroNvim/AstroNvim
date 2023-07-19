2cf670e099980bb11562ed2b1a9f6b30bf516788
Micah Halter <micah@mehalter.com>
2023-07-19 15:21:31 -0400

    refactor!: modularize config with AstroCore, AstroUI, and AstroLSP
    feat: move to modular plugin for configuring LSP options
    
    feat: move to modular plugin for configuring mappings and autocmds
    
    refactor!: move configuration defaults to `opts` tables
    
    refactor!: move updater, git, and mason utilities to `astrocore`
    
    refactor!: move buffer to `astrocore` and icons to `astroui`
    
    refactor!: move resession extension to AstroCore
    
    feat!: make resession the default session manager
    
    refactor!: move status API to AstroUI
    
    refactor!: move colorscheme to AstroUI and polish to AstroCore
    
    refactor!: move astronvim.user_terminals to AstroCore
    
    chore: reorganize plugin files
    
    fix(astrolsp): update lsp mapping conditions
    
    fix(astrolsp): which-key integration fixed
    
    chore!: remove deprecated plugin configs and unnecessary LSPLoaded icon
    
    refactor: localize LSP mappings to their respective plugins
    
    refactor!: move UI/UX utils to `astrocore`
    
    refactor(astroui): reorganize astroui status setup
    
    refactor: move autocmd and user commands to AstroCore configuration
    
    refactor: move polish function back to outside of AstroCore
    
    refactor: move `vim.on_key` to AstroCore configuration
    
    refactor!: move astronvim specific options to AstroCore options
    
    refactor!: move Heirline `setup_colors` function to AstroUI
    
    refactor!: move plugins and lazy_snapshot into `astronvim` module
    
    refactor!: move to a model of just providing plugins
    
    refactor!: remove updater mappings and commands
    
    fix: fix initial startup ordering
    
    refactor: `file_worktree` moved to astrocore.utils
    
    fix(neo-tree): fix autocmds
    
    feat: add `AstroUpdate` to update Lazy and Mason
    
    chore(plugins): add `lazy = true` where necessary
    
    fix: `astrocore.utils` moved to `astrocore`
    
    fix(plugins): don't use the shorthand notation for plugins
    
    chore(plugins): use shorthand for just AstroNvim
    
    fix(astrolsp)!: `setup_handlers` renamed to `handlers`
    
    chore(mason): use lua for mason bindings
    
    refactor: update all mappings to match casing in vim docs (`:h`)
    
    perf(options): set options directly
    
    perf(mappings): remove unnecessary check
    
    fix(status): use user provided `status.colors` table for overriding
    
    refactor(heirline): improve heirline structure
    
    fix(status): allow for function in colors definition
    
    fix(init): improve initialization sequence
    
    fix: improve first installation path
    
    feat: add `init.lua` to warn user if they try to use AstroNvim as a direct Neovim configuration
    
    chore(plugins): add AstroNvim type checking
    
    refactor!: drop support for Neovim v0.8
    
    feat: add `vim.g.astronvim_options` as an optional function for setting up options
    
    feat(plugins)!: use `on_load` and remove some unnecessary `config` functions
    
    chore(plugins): use `is_available` rather than `pcall` where applicable
    
    fix(astrolsp): update autoformat_enabled to autoformat
    
    fix(astrocore): `syntax` toggle renamed to `buffer_syntax`

---
+++ lua/astronvim/plugins/_astroui_status.lua
@@ -0,0 +1,250 @@
+return {
+  "AstroNvim/astroui",
+  ---@param opts AstroUIOpts
+  opts = function(_, opts)
+    local function pattern_match(str, pattern_list)
+      for _, pattern in ipairs(pattern_list) do
+        if str:find(pattern) then return true end
+      end
+      return false
+    end
+
+    local sign_handlers = {}
+    -- gitsigns handlers
+    local gitsigns = function(_)
+      local gitsigns_avail, gitsigns = pcall(require, "gitsigns")
+      if gitsigns_avail then vim.schedule(gitsigns.preview_hunk) end
+    end
+    for _, sign in ipairs { "Topdelete", "Untracked", "Add", "Changedelete", "Delete" } do
+      local name = "GitSigns" .. sign
+      if not sign_handlers[name] then sign_handlers[name] = gitsigns end
+    end
+    -- diagnostic handlers
+    local diagnostics = function(args)
+      if args.mods:find "c" then
+        vim.schedule(vim.lsp.buf.code_action)
+      else
+        vim.schedule(vim.diagnostic.open_float)
+      end
+    end
+    for _, sign in ipairs { "Error", "Hint", "Info", "Warn" } do
+      local name = "DiagnosticSign" .. sign
+      if not sign_handlers[name] then sign_handlers[name] = diagnostics end
+    end
+    -- DAP handlers
+    local dap_breakpoint = function(_)
+      local dap_avail, dap = pcall(require, "dap")
+      if dap_avail then vim.schedule(dap.toggle_breakpoint) end
+    end
+    for _, sign in ipairs { "", "Rejected", "Condition" } do
+      local name = "DapBreakpoint" .. sign
+      if not sign_handlers[name] then sign_handlers[name] = dap_breakpoint end
+    end
+
+    opts.status = {
+      fallback_colors = {
+        none = "NONE",
+        fg = "#abb2bf",
+        bg = "#1e222a",
+        dark_bg = "#2c323c",
+        blue = "#61afef",
+        green = "#98c379",
+        grey = "#5c6370",
+        bright_grey = "#777d86",
+        dark_grey = "#5c5c5c",
+        orange = "#ff9640",
+        purple = "#c678dd",
+        bright_purple = "#a9a1e1",
+        red = "#e06c75",
+        bright_red = "#ec5f67",
+        white = "#c9c9c9",
+        yellow = "#e5c07b",
+        bright_yellow = "#ebae34",
+      },
+      modes = {
+        ["n"] = { "NORMAL", "normal" },
+        ["no"] = { "OP", "normal" },
+        ["nov"] = { "OP", "normal" },
+        ["noV"] = { "OP", "normal" },
+        ["no"] = { "OP", "normal" },
+        ["niI"] = { "NORMAL", "normal" },
+        ["niR"] = { "NORMAL", "normal" },
+        ["niV"] = { "NORMAL", "normal" },
+        ["i"] = { "INSERT", "insert" },
+        ["ic"] = { "INSERT", "insert" },
+        ["ix"] = { "INSERT", "insert" },
+        ["t"] = { "TERM", "terminal" },
+        ["nt"] = { "TERM", "terminal" },
+        ["v"] = { "VISUAL", "visual" },
+        ["vs"] = { "VISUAL", "visual" },
+        ["V"] = { "LINES", "visual" },
+        ["Vs"] = { "LINES", "visual" },
+        [""] = { "BLOCK", "visual" },
+        ["s"] = { "BLOCK", "visual" },
+        ["R"] = { "REPLACE", "replace" },
+        ["Rc"] = { "REPLACE", "replace" },
+        ["Rx"] = { "REPLACE", "replace" },
+        ["Rv"] = { "V-REPLACE", "replace" },
+        ["s"] = { "SELECT", "visual" },
+        ["S"] = { "SELECT", "visual" },
+        [""] = { "BLOCK", "visual" },
+        ["c"] = { "COMMAND", "command" },
+        ["cv"] = { "COMMAND", "command" },
+        ["ce"] = { "COMMAND", "command" },
+        ["r"] = { "PROMPT", "inactive" },
+        ["rm"] = { "MORE", "inactive" },
+        ["r?"] = { "CONFIRM", "inactive" },
+        ["!"] = { "SHELL", "inactive" },
+        ["null"] = { "null", "inactive" },
+      },
+      separators = {
+        none = { "", "" },
+        left = { "", "  " },
+        right = { "  ", "" },
+        center = { "  ", "  " },
+        tab = { "", " " },
+        breadcrumbs = "  ",
+        path = "  ",
+      },
+      attributes = {
+        buffer_active = { bold = true, italic = true },
+        buffer_picker = { bold = true },
+        macro_recording = { bold = true },
+        git_branch = { bold = true },
+        git_diff = { bold = true },
+      },
+      icon_highlights = {
+        file_icon = {
+          tabline = function(self) return self.is_active or self.is_visible end,
+          statusline = true,
+        },
+      },
+      buf_matchers = {
+        filetype = function(pattern_list, bufnr) return pattern_match(vim.bo[bufnr or 0].filetype, pattern_list) end,
+        buftype = function(pattern_list, bufnr) return pattern_match(vim.bo[bufnr or 0].buftype, pattern_list) end,
+        bufname = function(pattern_list, bufnr)
+          return pattern_match(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr or 0), ":t"), pattern_list)
+        end,
+      },
+      sign_handlers = sign_handlers,
+      setup_colors = function()
+        local astro = require "astrocore"
+        ---@type AstroUIStatusOpts
+        local status_opts = require("astroui").config.status
+        local color = assert(status_opts.fallback_colors)
+        local get_hlgroup = astro.get_hlgroup
+        local lualine_mode = require("astroui.status.hl").lualine_mode
+        local function resolve_lualine(orig, ...) return (not orig or orig == "NONE") and lualine_mode(...) or orig end
+
+        local Normal = get_hlgroup("Normal", { fg = color.fg, bg = color.bg })
+        local Comment = get_hlgroup("Comment", { fg = color.bright_grey, bg = color.bg })
+        local Error = get_hlgroup("Error", { fg = color.red, bg = color.bg })
+        local StatusLine = get_hlgroup("StatusLine", { fg = color.fg, bg = color.dark_bg })
+        local TabLine = get_hlgroup("TabLine", { fg = color.grey, bg = color.none })
+        local TabLineFill = get_hlgroup("TabLineFill", { fg = color.fg, bg = color.dark_bg })
+        local TabLineSel = get_hlgroup("TabLineSel", { fg = color.fg, bg = color.none })
+        local WinBar = get_hlgroup("WinBar", { fg = color.bright_grey, bg = color.bg })
+        local WinBarNC = get_hlgroup("WinBarNC", { fg = color.grey, bg = color.bg })
+        local Conditional = get_hlgroup("Conditional", { fg = color.bright_purple, bg = color.dark_bg })
+        local String = get_hlgroup("String", { fg = color.green, bg = color.dark_bg })
+        local TypeDef = get_hlgroup("TypeDef", { fg = color.yellow, bg = color.dark_bg })
+        local GitSignsAdd = get_hlgroup("GitSignsAdd", { fg = color.green, bg = color.dark_bg })
+        local GitSignsChange = get_hlgroup("GitSignsChange", { fg = color.orange, bg = color.dark_bg })
+        local GitSignsDelete = get_hlgroup("GitSignsDelete", { fg = color.bright_red, bg = color.dark_bg })
+        local DiagnosticError = get_hlgroup("DiagnosticError", { fg = color.bright_red, bg = color.dark_bg })
+        local DiagnosticWarn = get_hlgroup("DiagnosticWarn", { fg = color.orange, bg = color.dark_bg })
+        local DiagnosticInfo = get_hlgroup("DiagnosticInfo", { fg = color.white, bg = color.dark_bg })
+        local DiagnosticHint = get_hlgroup("DiagnosticHint", { fg = color.bright_yellow, bg = color.dark_bg })
+        local HeirlineInactive =
+          resolve_lualine(get_hlgroup("HeirlineInactive", { bg = nil }).bg, "inactive", color.dark_grey)
+        local HeirlineNormal = resolve_lualine(get_hlgroup("HeirlineNormal", { bg = nil }).bg, "normal", color.blue)
+        local HeirlineInsert = resolve_lualine(get_hlgroup("HeirlineInsert", { bg = nil }).bg, "insert", color.green)
+        local HeirlineVisual = resolve_lualine(get_hlgroup("HeirlineVisual", { bg = nil }).bg, "visual", color.purple)
+        local HeirlineReplace =
+          resolve_lualine(get_hlgroup("HeirlineReplace", { bg = nil }).bg, "replace", color.bright_red)
+        local HeirlineCommand =
+          resolve_lualine(get_hlgroup("HeirlineCommand", { bg = nil }).bg, "command", color.bright_yellow)
+        local HeirlineTerminal =
+          resolve_lualine(get_hlgroup("HeirlineTerminal", { bg = nil }).bg, "insert", HeirlineInsert)
+
+        local colors = {
+          close_fg = Error.fg,
+          fg = StatusLine.fg,
+          bg = StatusLine.bg,
+          section_fg = StatusLine.fg,
+          section_bg = StatusLine.bg,
+          git_branch_fg = Conditional.fg,
+          mode_fg = StatusLine.bg,
+          treesitter_fg = String.fg,
+          scrollbar = TypeDef.fg,
+          git_added = GitSignsAdd.fg,
+          git_changed = GitSignsChange.fg,
+          git_removed = GitSignsDelete.fg,
+          diag_ERROR = DiagnosticError.fg,
+          diag_WARN = DiagnosticWarn.fg,
+          diag_INFO = DiagnosticInfo.fg,
+          diag_HINT = DiagnosticHint.fg,
+          winbar_fg = WinBar.fg,
+          winbar_bg = WinBar.bg,
+          winbarnc_fg = WinBarNC.fg,
+          winbarnc_bg = WinBarNC.bg,
+          tabline_bg = TabLineFill.bg,
+          tabline_fg = TabLineFill.bg,
+          buffer_fg = Comment.fg,
+          buffer_path_fg = WinBarNC.fg,
+          buffer_close_fg = Comment.fg,
+          buffer_bg = TabLineFill.bg,
+          buffer_active_fg = Normal.fg,
+          buffer_active_path_fg = WinBarNC.fg,
+          buffer_active_close_fg = Error.fg,
+          buffer_active_bg = Normal.bg,
+          buffer_visible_fg = Normal.fg,
+          buffer_visible_path_fg = WinBarNC.fg,
+          buffer_visible_close_fg = Error.fg,
+          buffer_visible_bg = Normal.bg,
+          buffer_overflow_fg = Comment.fg,
+          buffer_overflow_bg = TabLineFill.bg,
+          buffer_picker_fg = Error.fg,
+          tab_close_fg = Error.fg,
+          tab_close_bg = TabLineFill.bg,
+          tab_fg = TabLine.fg,
+          tab_bg = TabLine.bg,
+          tab_active_fg = TabLineSel.fg,
+          tab_active_bg = TabLineSel.bg,
+          inactive = HeirlineInactive,
+          normal = HeirlineNormal,
+          insert = HeirlineInsert,
+          visual = HeirlineVisual,
+          replace = HeirlineReplace,
+          command = HeirlineCommand,
+          terminal = HeirlineTerminal,
+        }
+
+        local user_colors = status_opts.colors
+        if type(user_colors) == "table" then
+          colors = astro.extend_tbl(colors, user_colors)
+        elseif type(user_colors) == "function" then
+          colors = user_colors(colors)
+        end
+
+        for _, section in ipairs {
+          "git_branch",
+          "file_info",
+          "git_diff",
+          "diagnostics",
+          "lsp",
+          "macro_recording",
+          "mode",
+          "cmd_info",
+          "treesitter",
+          "nav",
+        } do
+          if not colors[section .. "_bg"] then colors[section .. "_bg"] = colors["section_bg"] end
+          if not colors[section .. "_fg"] then colors[section .. "_fg"] = colors["section_fg"] end
+        end
+
+        return colors
+      end,
+    }
+  end,
+}
---
+++ lua/astronvim/plugins/heirline.lua
@@ -0,0 +1,139 @@
+return {
+  "rebelot/heirline.nvim",
+  event = "BufEnter",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        if require("astrocore").is_available "heirline.nvim" then
+          maps.n["<Leader>bb"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(function(bufnr) vim.api.nvim_win_set_buf(0, bufnr) end)
+            end,
+            desc = "Select buffer from tabline",
+          }
+          maps.n["<Leader>bd"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(
+                function(bufnr) require("astrocore.buffer").close(bufnr) end
+              )
+            end,
+            desc = "Close buffer from tabline",
+          }
+          maps.n["<Leader>b\\"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(function(bufnr)
+                vim.cmd.split()
+                vim.api.nvim_win_set_buf(0, bufnr)
+              end)
+            end,
+            desc = "Horizontal split buffer from tabline",
+          }
+          maps.n["<Leader>b|"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(function(bufnr)
+                vim.cmd.vsplit()
+                vim.api.nvim_win_set_buf(0, bufnr)
+              end)
+            end,
+            desc = "Vertical split buffer from tabline",
+          }
+        end
+      end,
+    },
+  },
+  opts = function()
+    local status = require "astroui.status"
+    return {
+      opts = {
+        colors = require("astroui").config.status.setup_colors(),
+        disable_winbar_cb = function(args)
+          return not require("astrocore.buffer").is_valid(args.buf)
+            or status.condition.buffer_matches({
+              buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+              filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+            }, args.buf)
+        end,
+      },
+      statusline = { -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode(),
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+        status.component.mode { surround = { separator = "right" } },
+      },
+      winbar = { -- winbar
+        init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+        fallthrough = false,
+        {
+          condition = function() return not status.condition.is_active() end,
+          status.component.separated_path(),
+          status.component.file_info {
+            file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+            file_modified = false,
+            file_read_only = false,
+            hl = status.hl.get_attributes("winbarnc", true),
+            surround = false,
+            update = "BufEnter",
+          },
+        },
+        status.component.breadcrumbs { hl = status.hl.get_attributes("winbar", true) },
+      },
+      tabline = { -- bufferline
+        { -- file tree padding
+          condition = function(self)
+            self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+            return status.condition.buffer_matches({
+              filetype = {
+                "NvimTree",
+                "OverseerList",
+                "aerial",
+                "dap%-repl",
+                "dapui_.",
+                "edgy",
+                "neo%-tree",
+                "undotree",
+              },
+            }, vim.api.nvim_win_get_buf(self.winid))
+          end,
+          provider = function(self) return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1) end,
+          hl = { bg = "tabline_bg" },
+        },
+        status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
+        status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
+        { -- tab list
+          condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
+          status.heirline.make_tablist { -- component for each tab
+            provider = status.provider.tabnr(),
+            hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
+          },
+          { -- close button for current tab
+            provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+            hl = status.hl.get_attributes("tab_close", true),
+            on_click = {
+              callback = function() require("astrocore.buffer").close_tab() end,
+              name = "heirline_tabline_close_tab_callback",
+            },
+          },
+        },
+      },
+      statuscolumn = {
+        init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+        status.component.foldcolumn(),
+        status.component.fill(),
+        status.component.numbercolumn(),
+        status.component.signcolumn(),
+      },
+    }
+  end,
+  config = function(...) require "astronvim.plugins.configs.heirline"(...) end,
+}
