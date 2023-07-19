3c5b883aade3107960d75a04ab5873fdb2f6662b
Micah Halter <micah@mehalter.com>
2023-07-19 15:21:31 -0400

    refactor!: modularize config with AstroCore, AstroUI, and AstroLSP
    feat: move to modular plugin for configuring LSP options
    
    feat: move to modular plugin for configuring mappings and autocmds
    
    refactor!: move configuration defaults to `opts` tables
    
    refactor!: move updater, git, and mason utilities to `astrocore`
    
    refactor!: move buffer to `astrocore` and icons to `astroui`
    
    refactor!: move resession extension to AstroCore
    
    feat!: make resession the default session manager
    
    refactor!: move status API to AstroUI
    
    refactor!: move colorscheme to AstroUI and polish to AstroCore
    
    refactor!: move astronvim.user_terminals to AstroCore
    
    chore: reorganize plugin files
    
    fix(astrolsp): update lsp mapping conditions
    
    fix(astrolsp): which-key integration fixed
    
    chore!: remove deprecated plugin configs and unnecessary LSPLoaded icon
    
    refactor: localize LSP mappings to their respective plugins
    
    refactor!: move UI/UX utils to `astrocore`
    
    refactor(astroui): reorganize astroui status setup
    
    refactor: move autocmd and user commands to AstroCore configuration
    
    refactor: move polish function back to outside of AstroCore
    
    refactor: move `vim.on_key` to AstroCore configuration
    
    refactor!: move astronvim specific options to AstroCore options
    
    refactor!: move Heirline `setup_colors` function to AstroUI
    
    refactor!: move plugins and lazy_snapshot into `astronvim` module
    
    refactor!: move to a model of just providing plugins
    
    refactor!: remove updater mappings and commands
    
    fix: fix initial startup ordering
    
    refactor: `file_worktree` moved to astrocore.utils
    
    fix(neo-tree): fix autocmds
    
    feat: add `AstroUpdate` to update Lazy and Mason
    
    chore(plugins): add `lazy = true` where necessary
    
    fix: `astrocore.utils` moved to `astrocore`
    
    fix(plugins): don't use the shorthand notation for plugins
    
    chore(plugins): use shorthand for just AstroNvim
    
    fix(astrolsp)!: `setup_handlers` renamed to `handlers`
    
    chore(mason): use lua for mason bindings
    
    refactor: update all mappings to match casing in vim docs (`:h`)
    
    perf(options): set options directly
    
    perf(mappings): remove unnecessary check
    
    fix(status): use user provided `status.colors` table for overriding
    
    refactor(heirline): improve heirline structure
    
    fix(status): allow for function in colors definition
    
    fix(init): improve initialization sequence
    
    fix: improve first installation path
    
    feat: add `init.lua` to warn user if they try to use AstroNvim as a direct Neovim configuration
    
    chore(plugins): add AstroNvim type checking
    
    refactor!: drop support for Neovim v0.8
    
    feat: add `vim.g.astronvim_options` as an optional function for setting up options
    
    feat(plugins)!: use `on_load` and remove some unnecessary `config` functions
    
    chore(plugins): use `is_available` rather than `pcall` where applicable
    
    fix(astrolsp): update autoformat_enabled to autoformat
    
    fix(astrocore): `syntax` toggle renamed to `buffer_syntax`

---
+++ lua/astronvim/plugins/_astrocore.lua
@@ -0,0 +1,29 @@
+require("astronvim").init()
+
+return {
+  { "AstroNvim", priority = 10000, lazy = false },
+  {
+    "AstroNvim/astrocore",
+    dependencies = { "AstroNvim/astroui" },
+    lazy = false,
+    priority = 10000,
+    ---@type AstroCoreOpts
+    opts = {
+      features = {
+        max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files
+        autopairs = true, -- enable autopairs at start
+        cmp = true, -- enable completion at start
+        highlighturl = true, -- highlight URLs by default
+        notifications = true, -- disable notifications
+      },
+      sessions = {
+        autosave = { last = true, cwd = true },
+        ignore = {
+          dirs = {},
+          filetypes = { "gitcommit", "gitrebase" },
+          buftypes = {},
+        },
+      },
+    },
+  },
+}
