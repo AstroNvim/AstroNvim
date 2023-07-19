0e0d8bdef5e0a147f1f1a0c434539016d39940b5
Micah Halter <micah@mehalter.com>
2023-07-19 15:21:31 -0400

    refactor!: modularize config with AstroCore, AstroUI, and AstroLSP
    feat: move to modular plugin for configuring LSP options
    
    feat: move to modular plugin for configuring mappings and autocmds
    
    refactor!: move configuration defaults to `opts` tables
    
    refactor!: move updater, git, and mason utilities to `astrocore`
    
    refactor!: move buffer to `astrocore` and icons to `astroui`
    
    refactor!: move resession extension to AstroCore
    
    feat!: make resession the default session manager
    
    refactor!: move status API to AstroUI
    
    refactor!: move colorscheme to AstroUI and polish to AstroCore
    
    refactor!: move astronvim.user_terminals to AstroCore
    
    chore: reorganize plugin files
    
    fix(astrolsp): update lsp mapping conditions
    
    fix(astrolsp): which-key integration fixed
    
    chore!: remove deprecated plugin configs and unnecessary LSPLoaded icon
    
    refactor: localize LSP mappings to their respective plugins
    
    refactor!: move UI/UX utils to `astrocore`
    
    refactor(astroui): reorganize astroui status setup
    
    refactor: move autocmd and user commands to AstroCore configuration
    
    refactor: move polish function back to outside of AstroCore
    
    refactor: move `vim.on_key` to AstroCore configuration
    
    refactor!: move astronvim specific options to AstroCore options
    
    refactor!: move Heirline `setup_colors` function to AstroUI
    
    refactor!: move plugins and lazy_snapshot into `astronvim` module
    
    refactor!: move to a model of just providing plugins
    
    refactor!: remove updater mappings and commands
    
    fix: fix initial startup ordering
    
    refactor: `file_worktree` moved to astrocore.utils
    
    fix(neo-tree): fix autocmds
    
    feat: add `AstroUpdate` to update Lazy and Mason
    
    chore(plugins): add `lazy = true` where necessary
    
    fix: `astrocore.utils` moved to `astrocore`
    
    fix(plugins): don't use the shorthand notation for plugins
    
    chore(plugins): use shorthand for just AstroNvim
    
    fix(astrolsp)!: `setup_handlers` renamed to `handlers`
    
    chore(mason): use lua for mason bindings
    
    refactor: update all mappings to match casing in vim docs (`:h`)
    
    perf(options): set options directly
    
    perf(mappings): remove unnecessary check
    
    fix(status): use user provided `status.colors` table for overriding
    
    refactor(heirline): improve heirline structure
    
    fix(status): allow for function in colors definition
    
    fix(init): improve initialization sequence
    
    fix: improve first installation path
    
    feat: add `init.lua` to warn user if they try to use AstroNvim as a direct Neovim configuration
    
    chore(plugins): add AstroNvim type checking
    
    refactor!: drop support for Neovim v0.8
    
    feat: add `vim.g.astronvim_options` as an optional function for setting up options
    
    feat(plugins)!: use `on_load` and remove some unnecessary `config` functions
    
    chore(plugins): use `is_available` rather than `pcall` where applicable
    
    fix(astrolsp): update autoformat_enabled to autoformat
    
    fix(astrocore): `syntax` toggle renamed to `buffer_syntax`

--- init.lua
+++ init.lua
@@ -1,23 +1,8 @@
-if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+vim.api.nvim_echo({
+  { "This repository is not meant to be used as a direct Neovim configuration\n", "ErrorMsg" },
+  { "Please check the AstroNvim documentation for installation details\n", "WarningMsg" },
+  { "Press any key to exit...", "MoreMsg" },
+}, true, {})
 
-for _, source in ipairs {
-  "astronvim.bootstrap",
-  "astronvim.options",
-  "astronvim.lazy",
-  "astronvim.autocmds",
-  "astronvim.mappings",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-end
-
-if astronvim.default_colorscheme then
-  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
-    require("astronvim.utils").notify(
-      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
-      vim.log.levels.ERROR
-    )
-  end
-end
-
-require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+vim.fn.getchar()
+vim.cmd.quit()
