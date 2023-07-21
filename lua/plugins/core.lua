053eef964b71bd75cc020531541f705f3dc32197
Micah Halter <micah@mehalter.com>
2023-07-21 08:22:27 -0400

    refactor!: move resession extension to AstroCore

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -54,7 +54,7 @@ return {
     opts = {
       buf_filter = function(bufnr) return require("astrocore.buffer").is_restorable(bufnr) end,
       tab_buf_filter = function(tabpage, bufnr) return vim.tbl_contains(vim.t[tabpage].bufs, bufnr) end,
-      extensions = { astronvim = {} },
+      extensions = { astrocore = {} },
     },
   },
   {
