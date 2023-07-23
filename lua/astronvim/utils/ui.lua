9aa2960b38874c3ee47c6d7d64254d02a05eedf8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-23 08:40:34 -0700

    fix(ui): don't check every lsp client when toggling semantic tokens

--- lua/astronvim/utils/ui.lua
+++ lua/astronvim/utils/ui.lua
@@ -93,7 +93,7 @@ end
 function M.toggle_buffer_semantic_tokens(bufnr, silent)
   bufnr = bufnr or 0
   vim.b[bufnr].semantic_tokens_enabled = not vim.b[bufnr].semantic_tokens_enabled
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
+  for _, client in ipairs(vim.lsp.get_active_clients { bufnr = bufnr }) do
     if client.server_capabilities.semanticTokensProvider then
       vim.lsp.semantic_tokens[vim.b[bufnr].semantic_tokens_enabled and "start" or "stop"](bufnr, client.id)
       ui_notify(
