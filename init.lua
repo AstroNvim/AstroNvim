69300bf79447a8b2e9dc28e1b721b1c24c4cf24a
Micah Halter <micah@mehalter.com>
2023-07-24 11:06:01 -0400

    refactor: move polish function back to outside of AstroCore

--- init.lua
+++ init.lua
@@ -4,3 +4,6 @@ for _, source in ipairs { "astronvim.bootstrap", "astronvim.options", "astronvim
   local status_ok, fault = pcall(require, source)
   if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
+
+local polish = astronvim.user_opts("polish", nil, false)
+if type(polish) == "function" then polish() end
