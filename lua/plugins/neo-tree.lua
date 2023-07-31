5958d6e9f8235f8eabf4e957f98749f0bf3bd7db
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-30 17:14:09 -0700

    fix(neo-tree): add missing mappings to navigate neo-tree filter

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -126,6 +126,10 @@ return {
           l = "child_or_open",
           o = "open",
         },
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          ["<C-j>"] = "move_cursor_down",
+          ["<C-k>"] = "move_cursor_up",
+        },
       },
       filesystem = {
         follow_current_file = { enabled = true },
