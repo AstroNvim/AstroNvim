0db6605db4f04a65293c1173b782ded9c0a5598b
Micah Halter <micah@mehalter.com>
2023-07-31 11:17:42 -0400

    fix(utils): fix `alpha_button` string substitution

--- lua/astronvim/utils/init.lua
+++ lua/astronvim/utils/init.lua
@@ -191,7 +191,7 @@ end
 ---@return table # A button entity table for an alpha configuration
 function M.alpha_button(sc, txt)
   -- replace <leader> in shortcut text with LDR for nicer printing
-  local sc_ = sc:gsub("%s", ""):gsub("LDR", "<leader>")
+  local sc_ = sc:gsub("%s", ""):gsub("<[l|L]eader>", "LDR")
   -- if the leader is set, replace the text with the actual leader key for nicer printing
   if vim.g.mapleader then sc = sc:gsub("LDR", vim.g.mapleader == " " and "SPC" or vim.g.mapleader) end
   -- return the button entity to display the correct text and send the correct keybinding on press
