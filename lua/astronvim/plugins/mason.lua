f8d90c22b0422e7630bd01da73122e9f842a7871
Micah Halter <micah@mehalter.com>
2023-08-08 07:49:54 -0400

    fix(mason)!: rename `MasonUpdate` and `MasonUpdateAll` to `AstroMasonUpdate` and `AstroMasonUpdateAll`

--- lua/astronvim/plugins/mason.lua
+++ lua/astronvim/plugins/mason.lua
@@ -7,17 +7,30 @@ return {
       "MasonUninstall",
       "MasonUninstallAll",
       "MasonLog",
-      "MasonUpdate", -- AstroNvim extension here as well
-      "MasonUpdateAll", -- AstroNvim specific
     },
     dependencies = {
       {
         "AstroNvim/astrocore",
         opts = function(_, opts)
           local maps = opts.mappings
+          local cmd = opts.commands
           if require("astrocore").is_available "mason.nvim" then
             maps.n["<Leader>pm"] = { function() require("mason.ui").open() end, desc = "Mason Installer" }
             maps.n["<Leader>pM"] = { function() require("astrocore.mason").update_all() end, desc = "Mason Update" }
+            cmd.AstroMasonUpdate = {
+              function(options) require("astrocore.mason").update(options.fargs) end,
+              nargs = "*",
+              desc = "Update Mason Package",
+              complete = function(arg_lead)
+                local _ = require "mason-core.functional"
+                return _.sort_by(
+                  _.identity,
+                  _.filter(_.starts_with(arg_lead), require("mason-registry").get_installed_package_names())
+                )
+              end,
+            }
+            cmd.AstroMasonUpdateAll =
+              { function() require("astrocore.mason").update_all() end, desc = "Update Mason Packages" }
           end
         end,
       },
