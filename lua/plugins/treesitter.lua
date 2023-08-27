e8ae856c38491339d1171ea224b2df0e124d7b27
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-08-27 16:46:31 -0400

    fix(nvim-ts-autotag): TS closing tag disabled until issue is closed (#2244)
    * fix: TS closing tag disabled until issue is closed
    
    * Update treesitter.lua
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    asdf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,8 @@ return {
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
     "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
+    -- HACK: remove when https://github.com/windwp/nvim-ts-autotag/issues/125 closed.
+    { "windwp/nvim-ts-autotag", opts = { autotag = { enable_close_on_slash = false } } },
   },
   event = "User AstroFile",
   cmd = {
