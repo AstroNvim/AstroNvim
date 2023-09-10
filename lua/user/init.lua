f94368258e9119fe234402987f78bd173ce2305e
Kris <klxdev@gmail.com>
2023-09-09 17:00:27 -0700

    adding more colors and switching default to ayu

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,5 +1,9 @@
 return {
-  colorscheme = "sonokai",
+  --background = dark,
+  --colorscheme = "sonokai",
+  --colorscheme = "PaperColor",
+  --colorscheme = "gruvbox",
+  colorscheme = "ayu",
   plugins = {
     {
       "sainnhe/sonokai",
@@ -7,5 +11,22 @@ return {
         vim.g.sonokai_style = "atlantis"
       end,
     },
+    {
+      "NLKNguyen/papercolor-theme",
+      init = function() -- init function runs before the plugin is loaded
+        vim.g.airline_theme = "papercolor"
+      end,
+    },
+    {
+      "morhetz/gruvbox",
+      init = function()
+      end
+    },
+    {
+      "ayu-theme/ayu-vim",
+      init = function()
+        ayucolor = "mirage"
+      end
+    }
   },
 }
