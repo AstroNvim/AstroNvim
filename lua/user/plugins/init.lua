04cd2d9263e31a09ce3682f3b83aaa54cbbd49d2
Kris <klxdev@gmail.com>
2023-09-23 10:13:44 -0700

    adding ayu mirage back as the default theme and removing unused themes

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -7,7 +7,7 @@ return {
           list = true,
         },
         show_current_context = true,
-        show_current_context_start = true,
+        --show_current_context_start = true,
         show_end_of_line = true,
         space_char_blankline = " ",
         char_highlight_list = {
@@ -22,26 +22,12 @@ return {
     end
   },
   {
-      "sainnhe/sonokai",
-      init = function() -- init function runs before the plugin is loaded
-        vim.g.sonokai_style = "atlantis"
-      end,
-    },
-    {
-      "NLKNguyen/papercolor-theme",
-      init = function() -- init function runs before the plugin is loaded
-        vim.g.airline_theme = "papercolor"
-      end,
-    },
-    {
-      "morhetz/gruvbox",
-      init = function()
-      end
-    },
-    {
-      "ayu-theme/ayu-vim",
-      init = function()
-        ayucolor = "mirage"
-      end
-    }
+    "Shatur/neovim-ayu",
+    init = function()
+      require('ayu').setup {
+          mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+          overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+      }
+    end
+  }
 }
