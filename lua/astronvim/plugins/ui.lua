5614192947643e7548637a70b68eb81a32386f76
Micah Halter <micah@mehalter.com>
2023-09-29 07:44:39 -0400

    perf(plugins): lazy now ignores plugin fragments if a plugin is disabled
    We don't need to add our own checks for it plugins are available now because Lazy will handle ignoring the parts of the configuration automatically if a plugin is disable.

--- lua/astronvim/plugins/ui.lua
+++ lua/astronvim/plugins/ui.lua
@@ -79,6 +79,15 @@ return {
   },
   {
     "NvChad/nvim-colorizer.lua",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["<Leader>uC"] = { "<Cmd>ColorizerToggle<CR>", desc = "Toggle color highlight" }
+        end,
+      },
+    },
     event = "User AstroFile",
     cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerDetachFromBuffer", "ColorizerReloadAllBuffers" },
     opts = { user_default_options = { names = false } },
