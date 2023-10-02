1986bdfe6440fd718a085b99e8cf3b1c69665d79
Kris <klxdev@gmail.com>
2023-10-01 18:09:40 -0700

    updating syntax for indent_blank_line for v3

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-      require("indent_blankline").setup {
+      require("ibl").setup {
         opt = {
           list = true,
         },
