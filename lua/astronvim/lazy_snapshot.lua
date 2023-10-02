68d01674c64fd0489555eb5a6d8f03f00cdfa910
Micah Halter <micah@mehalter.com>
2023-10-02 14:06:09 -0400

    feat(plugins)!: move from `null-ls` to maintained fork `none-ls`

--- lua/astronvim/lazy_snapshot.lua
+++ lua/astronvim/lazy_snapshot.lua
@@ -20,7 +20,7 @@ return {
   { "hrsh7th/nvim-cmp", commit = "538e37ba87284942c1d76ed38dd497e54e65b891", optional = true },
   { "jay-babu/mason-null-ls.nvim", version = "^2", optional = true },
   { "jay-babu/mason-nvim-dap.nvim", version = "^2", optional = true },
-  { "jose-elias-alvarez/null-ls.nvim", commit = "0010ea927ab7c09ef0ce9bf28c2b573fc302f5a7", optional = true },
+  { "nvimtools/none-ls.nvim", commit = "0010ea927ab7c09ef0ce9bf28c2b573fc302f5a7", optional = true },
   { "kevinhwang91/nvim-ufo", version = "^1", optional = true },
   { "kevinhwang91/promise-async", version = "^1", optional = true },
   { "lewis6991/gitsigns.nvim", version = "^0.7", optional = true },
