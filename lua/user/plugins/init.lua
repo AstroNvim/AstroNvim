92aaf51e4b75f63b8d0034bd177860b32d44368d
Kris Lankford <krislank@amazon.com>
2023-10-06 06:54:37 -0700

    removing indent_line becauase it breaks and I don't need it anymore

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,27 +1,5 @@
 return {
   {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup {
-        opt = {
-          list = true,
-        },
-        show_current_context = true,
-        --show_current_context_start = true,
-        show_end_of_line = true,
-        space_char_blankline = " ",
-        --char_highlight_list = {
-        --  "IndentBlanklineIndent1",
-        --  "IndentBlanklineIndent2",
-        --  "IndentBlanklineIndent3",
-        --  "IndentBlanklineIndent4",
-        --  "IndentBlanklineIndent5",
-        --  "IndentBlanklineIndent6",
-        --},
-    }
-    end
-  },
-  {
     "Shatur/neovim-ayu",
     init = function()
       require('ayu').setup {
