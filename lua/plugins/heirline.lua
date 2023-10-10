b54a6c874bb4ffbd40721f7bd38242e04c863972
Micah Halter <micah@mehalter.com>
2023-10-10 15:11:38 -0400

    fix(status): fix `signcolumn=number`

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -84,6 +84,7 @@ return {
         },
       },
       statuscolumn = vim.fn.has "nvim-0.9" == 1 and {
+        init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
         status.component.foldcolumn(),
         status.component.fill(),
         status.component.numbercolumn(),
