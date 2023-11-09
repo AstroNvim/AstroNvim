4f8e87f069324541f1149b574dcdf39518ef37d5
Micah Halter <micah@mehalter.com>
2023-11-09 05:51:33 -0500

    fix(treesitter): force installation of shipped neovim parsers

--- lua/plugins/configs/nvim-treesitter.lua
+++ lua/plugins/configs/nvim-treesitter.lua
@@ -1,1 +1,11 @@
-return function(_, opts) require("nvim-treesitter.configs").setup(opts) end
+return function(_, opts)
+  if type(opts.ensure_installed) == "table" then
+    local added = {}
+    opts.ensure_installed = vim.tbl_filter(function(parser)
+      if added[parser] then return false end
+      added[parser] = true
+      return true
+    end, opts.ensure_installed)
+  end
+  require("nvim-treesitter.configs").setup(opts)
+end
