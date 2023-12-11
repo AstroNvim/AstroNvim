b39d8f9b20b39bf5f520046d02d19216c16ba808
Micah Halter <micah@mehalter.com>
2023-12-11 05:43:40 -0500

    chore(notify): set default max height/width for notifications

--- lua/astronvim/plugins/ui.lua
+++ lua/astronvim/plugins/ui.lua
@@ -68,6 +68,8 @@ return {
     },
     init = function() require("astrocore").load_plugin_with_func("nvim-notify", vim, "notify") end,
     opts = {
+      max_height = function() return math.floor(vim.o.lines * 0.75) end,
+      max_width = function() return math.floor(vim.o.columns * 0.75) end,
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 175 })
         if not require("astrocore").config.features.notifications then vim.api.nvim_win_close(win, true) end
