47d7cf68738efffa80b89f6e4a0d92ebecee3aab
Micah Halter <micah@mehalter.com>
2023-12-18 08:09:05 -0500

    feat: add configuration of plugin pinning

---
+++ lua/astronvim/config.lua
@@ -0,0 +1,5 @@
+---@class AstroNvimOpts
+---@field pin_plugins boolean? whether to pin plugins or not, if `nil` then will pin if version is set.
+
+---@type AstroNvimOpts
+return {}
--- lua/astronvim/init.lua
+++ lua/astronvim/init.lua
@@ -2,6 +2,8 @@ local M = {}
 
 M.did_init = false
 
+M.config = require "astronvim.config"
+
 function M.init()
   if not M.did_init then
     M.did_init = true
@@ -21,7 +23,20 @@ function M.init()
     -- set options
     if vim.bo.filetype == "lazy" then vim.cmd.bw() end
     require "astronvim.options"
+
+    -- force setup during initialization
+    local plugin = require("lazy.core.config").spec.plugins.AstroNvim
+
+    local opts = require("lazy.core.plugin").values(plugin, "opts")
+    if opts.pin_plugins == nil then opts.pin_plugins = plugin.version ~= nil end
+
+    ---@diagnostic disable-next-line: cast-local-type
+    opts = vim.tbl_deep_extend("force", M.config, opts)
+    ---@cast opts -nil
+    M.config = opts
   end
 end
+
+function M.setup() end
 
 return M
