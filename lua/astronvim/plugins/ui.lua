fd3dfef66188e4bc2f9e32cd25431c830b78116e
Micah Halter <micah@mehalter.com>
2023-12-19 07:17:20 -0500

    perf: improve performance of triggered plugin loading

--- lua/astronvim/plugins/ui.lua
+++ lua/astronvim/plugins/ui.lua
@@ -71,9 +71,10 @@ return {
       max_height = function() return math.floor(vim.o.lines * 0.75) end,
       max_width = function() return math.floor(vim.o.columns * 0.75) end,
       on_open = function(win)
+        local astrocore = require "astrocore"
         vim.api.nvim_win_set_config(win, { zindex = 175 })
-        if not require("astrocore").config.features.notifications then vim.api.nvim_win_close(win, true) end
-        if not package.loaded["nvim-treesitter"] then pcall(require, "nvim-treesitter") end
+        if not astrocore.config.features.notifications then vim.api.nvim_win_close(win, true) end
+        if astrocore.is_available "nvim-treesitter" then require("lazy").load { plugins = { "nvim-treesitter" } } end
         vim.wo[win].conceallevel = 3
         local buf = vim.api.nvim_win_get_buf(win)
         if not pcall(vim.treesitter.start, buf, "markdown") then vim.bo[buf].syntax = "markdown" end
