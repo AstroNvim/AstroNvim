a7c26c6a0d8322ad656270be19be3ee6003a2f8d
Micah Halter <micah@mehalter.com>
2024-01-12 15:21:21 -0500

    fix(mappings): gdu executable for mac is `gdu-go`

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -337,8 +337,9 @@ if is_available "toggleterm.nvim" then
   if vim.fn.executable "node" == 1 then
     maps.n["<leader>tn"] = { function() utils.toggle_term_cmd "node" end, desc = "ToggleTerm node" }
   end
-  if vim.fn.executable "gdu" == 1 then
-    maps.n["<leader>tu"] = { function() utils.toggle_term_cmd "gdu" end, desc = "ToggleTerm gdu" }
+  local gdu = vim.fn.has "mac" == 1 and "gdu-go" or "gdu"
+  if vim.fn.executable(gdu) == 1 then
+    maps.n["<leader>tu"] = { function() utils.toggle_term_cmd(gdu) end, desc = "ToggleTerm gdu" }
   end
   if vim.fn.executable "btm" == 1 then
     maps.n["<leader>tt"] = { function() utils.toggle_term_cmd "btm" end, desc = "ToggleTerm btm" }
