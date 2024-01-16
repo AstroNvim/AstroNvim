7235fb5e7c40b09035966ffdb38fba5683d26ad3
Micah Halter <micah@mehalter.com>
2024-01-16 07:04:54 -0500

    feat(plugins): move to `vim-illuminate` to reference highlighting

--- lua/astronvim/plugins/ui.lua
+++ lua/astronvim/plugins/ui.lua
@@ -84,6 +84,35 @@ return {
     config = function(...) require "astronvim.plugins.configs.notify"(...) end,
   },
   {
+    "RRethy/vim-illuminate",
+    event = "User AstroFile",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["]r"] = { function() require("illuminate")["goto_next_reference"](false) end, desc = "Next reference" }
+          maps.n["[r"] =
+            { function() require("illuminate")["goto_prev_reference"](false) end, desc = "Previous reference" }
+          maps.n["<Leader>ur"] =
+            { function() require("illuminate").toggle() end, desc = "Toggle reference highlighting" }
+          maps.n["<Leader>uR"] =
+            { function() require("illuminate").toggle_buf() end, desc = "Toggle reference highlighting (buffer)" }
+        end,
+      },
+    },
+    opts = function()
+      return {
+        delay = 200,
+        min_count_to_highlight = 2,
+        large_file_cutoff = require("astrocore").config.features.max_file.lines,
+        large_file_overrides = { providers = { "lsp" } },
+        should_enable = function(bufnr) return require("astrocore.buffer").is_valid(bufnr) end,
+      }
+    end,
+    config = function(...) require "astronvim.plugins.configs.vim-illuminate"(...) end,
+  },
+  {
     "stevearc/dressing.nvim",
     lazy = true,
     init = function() require("astrocore").load_plugin_with_func("dressing.nvim", vim.ui, { "input", "select" }) end,
