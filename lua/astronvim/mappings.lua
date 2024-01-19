eab1383281aaf27b84d52f764be6beb67f5d8277
Micah Halter <micah@mehalter.com>
2024-01-19 09:08:06 -0500

    fix(mappings): fix bug in `<Leader>h` when `neo-tree` is open

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -127,7 +127,7 @@ if is_available "alpha-nvim" then
   maps.n["<leader>h"] = {
     function()
       local wins = vim.api.nvim_tabpage_list_wins(0)
-      if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
+      if #wins > 1 and vim.bo[vim.api.nvim_win_get_buf(wins[1])].filetype == "neo-tree" then
         vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
       end
       require("alpha").start(false)
