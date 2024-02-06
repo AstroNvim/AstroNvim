551b4d13fdc9727dd320a1e043724f50c329f928
Micah Halter <micah@mehalter.com>
2024-02-06 13:46:33 -0500

    feat: add `AstroLargeBuf` autocmd user event and update `max_file` usage to `large_buf`

--- lua/astronvim/plugins/configs/indent-blankline.lua
+++
@@ -1,5 +0,0 @@
-return function(plugin, opts)
-  require(plugin.main).setup(opts)
-  local hooks = require "ibl.hooks"
-  hooks.register(hooks.type.ACTIVE, function(bufnr) return not vim.b[bufnr].large_buf end)
-end
