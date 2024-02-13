27adb26c3c65ce82489370f930de93237a671b48
Micah Halter <micah@mehalter.com>
2024-02-13 13:38:28 -0500

    feat(config): add ability to configure `mapleader` and `icons_enabled` in AstroNvim `opts`

--- lua/astronvim/config.lua
+++ lua/astronvim/config.lua
@@ -1,5 +1,10 @@
 ---@class AstroNvimOpts
+---@field mapleader string? the leader key to map before setting up Lazy
+---@field icons_enabled boolean? whether to enable icons, default to `true`
 ---@field pin_plugins boolean? whether to pin plugins or not, if `nil` then will pin if version is set.
 
 ---@type AstroNvimOpts
-return {}
+return {
+  mapleader = " ",
+  icons_enabled = true,
+}
