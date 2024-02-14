e4c586cba5ca73286b838c87e13deedf61ed2f39
Micah Halter <micah@mehalter.com>
2024-02-14 07:17:59 -0500

    feat(resession): add `<Leader>SD` for deleting a directory session

--- lua/astronvim/plugins/core.lua
+++ lua/astronvim/plugins/core.lua
@@ -22,6 +22,8 @@ return {
           maps.n["<Leader>Ss"] = { function() require("resession").save() end, desc = "Save this session" }
           maps.n["<Leader>St"] = { function() require("resession").save_tab() end, desc = "Save this tab's session" }
           maps.n["<Leader>Sd"] = { function() require("resession").delete() end, desc = "Delete a session" }
+          maps.n["<Leader>SD"] =
+            { function() require("resession").delete(nil, { dir = "dirsession" }) end, desc = "Delete a dirsession" }
           maps.n["<Leader>Sf"] = { function() require("resession").load() end, desc = "Load a session" }
           maps.n["<Leader>SF"] =
             { function() require("resession").load(nil, { dir = "dirsession" }) end, desc = "Load a dirsession" }
