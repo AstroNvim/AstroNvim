b4202af442e4bb5be99ba9c922de908e42818e78
Micah Halter <micah@mehalter.com>
2024-02-26 05:15:19 -0500

    chore: move to selene for linting

--- lua/astronvim/plugins/_astroui_status.lua
+++ lua/astronvim/plugins/_astroui_status.lua
@@ -4,17 +4,17 @@ return {
   opts = function(_, opts)
     local sign_handlers = {}
     -- gitsigns handlers
-    local gitsigns = function(_)
+    local gitsigns_handler = function(_)
       local gitsigns_avail, gitsigns = pcall(require, "gitsigns")
       if gitsigns_avail then vim.schedule(gitsigns.preview_hunk) end
     end
     for _, sign in ipairs { "Topdelete", "Untracked", "Add", "Change", "Changedelete", "Delete" } do
       local name = "GitSigns" .. sign
-      if not sign_handlers[name] then sign_handlers[name] = gitsigns end
+      if not sign_handlers[name] then sign_handlers[name] = gitsigns_handler end
     end
-    sign_handlers["gitsigns_extmark_signs_"] = gitsigns
+    sign_handlers["gitsigns_extmark_signs_"] = gitsigns_handler
     -- diagnostic handlers
-    local diagnostics = function(args)
+    local diagnostics_handler = function(args)
       if args.mods:find "c" then
         vim.schedule(vim.lsp.buf.code_action)
       else
@@ -23,16 +23,16 @@ return {
     end
     for _, sign in ipairs { "Error", "Hint", "Info", "Warn" } do
       local name = "DiagnosticSign" .. sign
-      if not sign_handlers[name] then sign_handlers[name] = diagnostics end
+      if not sign_handlers[name] then sign_handlers[name] = diagnostics_handler end
     end
     -- DAP handlers
-    local dap_breakpoint = function(_)
+    local dap_breakpoint_handler = function(_)
       local dap_avail, dap = pcall(require, "dap")
       if dap_avail then vim.schedule(dap.toggle_breakpoint) end
     end
     for _, sign in ipairs { "", "Rejected", "Condition" } do
       local name = "DapBreakpoint" .. sign
-      if not sign_handlers[name] then sign_handlers[name] = dap_breakpoint end
+      if not sign_handlers[name] then sign_handlers[name] = dap_breakpoint_handler end
     end
 
     opts.status = {
