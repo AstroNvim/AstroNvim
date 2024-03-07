378dd78911439998427d01cd4f117a159b768d6c
William Roy <wroy@proton.me>
2024-03-07 11:44:28 -0500

    fix(which-key): handle cmdheight properly

--- lua/astronvim/plugins/which-key.lua
+++ lua/astronvim/plugins/which-key.lua
@@ -5,4 +5,16 @@ return {
     icons = { group = vim.g.icons_enabled ~= false and "" or "+", separator = "" },
     disable = { filetypes = { "TelescopePrompt" } },
   },
+  config = function(_, opts)
+    local wk = require "which-key"
+    local show = wk.show
+    wk.show = function(keys, opts)
+      if vim.o.cmdheight == 1 then return show(keys, opts) end
+      vim.o.cmdheight = 1
+      vim.cmd.redraw()
+      show(keys, opts)
+      vim.schedule(function() vim.o.cmdheight = 0 end)
+    end
+    wk.setup { opts }
+  end,
 }
