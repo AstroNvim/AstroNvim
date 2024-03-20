8161967aa871286e695e0267acd766ce16dc3a1c
Micah Halter <micah.halter@gtri.gatech.edu>
2024-03-20 13:44:53 -0400

    revert(toggleterm): continue separating custom terminals as hidden

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -118,9 +118,16 @@ return {
         WinBarNC = { link = "WinBarNC" },
       },
       size = 10,
-      on_create = function()
+      ---@param t Terminal
+      on_create = function(t)
         vim.opt_local.foldcolumn = "0"
         vim.opt_local.signcolumn = "no"
+        if t.hidden then
+          local toggle = function() t:toggle() end
+          for _, key in ipairs { "<C-'>", "<F7>" } do
+            vim.keymap.set({ "n", "t", "i" }, key, toggle, { desc = "Toggle terminal", buffer = t.bufnr })
+          end
+        end
       end,
       shading_factor = 2,
       direction = "float",
