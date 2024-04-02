0c9f87bff8232d3efbebdb3d8725ac65887dc28a
Micah Halter <micah@mehalter.com>
2024-04-02 13:08:43 -0400

    fix(resession): enable AstroCore resession extension for single tab restore

--- lua/astronvim/plugins/resession.lua
+++ lua/astronvim/plugins/resession.lua
@@ -45,6 +45,6 @@ return {
   opts = {
     buf_filter = function(bufnr) return require("astrocore.buffer").is_restorable(bufnr) end,
     tab_buf_filter = function(tabpage, bufnr) return vim.tbl_contains(vim.t[tabpage].bufs, bufnr) end,
-    extensions = { astrocore = {} },
+    extensions = { astrocore = { enable_in_tab = true } },
   },
 }
