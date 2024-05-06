c1e8e74312284223a6faa816e6b75dbfcd4d1c2e
Micah Halter <micah@mehalter.com>
2024-05-06 07:41:24 -0400

    perf(guess-indent): improve lazy loading (#2561)
    this also moves to a maintained fork until PR is merged upstream

--- lua/astronvim/plugins/guess-indent.lua
+++ lua/astronvim/plugins/guess-indent.lua
@@ -1,5 +1,33 @@
 return {
+  -- TODO: Move back to upstream after modernization PR is merged
+  -- https://github.com/NMAC427/guess-indent.nvim/pull/20
   "NMAC427/guess-indent.nvim",
-  event = "User AstroFile",
-  config = function(...) require "astronvim.plugins.configs.guess-indent"(...) end,
+  url = "https://github.com/mehalter/guess-indent.nvim",
+  cmd = "GuessIndent",
+  dependencies = {
+    "AstroNvim/astrocore",
+    opts = {
+      autocmds = {
+        GuessIndent = {
+          {
+            event = "BufReadPost",
+            desc = "Guess indentation when loading a file",
+            callback = function(args) require("guess-indent").set_from_buffer(args.buf, true) end,
+          },
+          {
+            event = "BufNewFile",
+            desc = "Guess indentation when saving a new file",
+            callback = function(args)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                buffer = args.buf,
+                once = true,
+                callback = function(wargs) require("guess-indent").set_from_buffer(wargs.buf, true) end,
+              })
+            end,
+          },
+        },
+      },
+    },
+  },
+  opts = { auto_cmd = false },
 }
