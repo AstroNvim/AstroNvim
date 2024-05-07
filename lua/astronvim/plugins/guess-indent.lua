3d094ea7a6dafa90ac7bf6f48ffb399b28c22b12
Micah Halter <micah@mehalter.com>
2024-05-07 08:43:48 -0400

    revert: use main upstream for `guess-indent` as lazy gets very confused
    refs 4952573b, c1e8e743

--- lua/astronvim/plugins/guess-indent.lua
+++ lua/astronvim/plugins/guess-indent.lua
@@ -1,33 +1,5 @@
 return {
-  -- TODO: Move back to upstream after modernization PR is merged
-  -- https://github.com/NMAC427/guess-indent.nvim/pull/20
   "NMAC427/guess-indent.nvim",
-  url = "https://github.com/mehalter/guess-indent.nvim",
-  cmd = "GuessIndent",
-  dependencies = {
-    "AstroNvim/astrocore",
-    opts = {
-      autocmds = {
-        GuessIndent = {
-          {
-            event = "BufReadPost",
-            desc = "Guess indentation when loading a file",
-            callback = function(args) require("guess-indent").set_from_buffer(args.buf, true, true) end,
-          },
-          {
-            event = "BufNewFile",
-            desc = "Guess indentation when saving a new file",
-            callback = function(args)
-              vim.api.nvim_create_autocmd("BufWritePost", {
-                buffer = args.buf,
-                once = true,
-                callback = function(wargs) require("guess-indent").set_from_buffer(wargs.buf, true, true) end,
-              })
-            end,
-          },
-        },
-      },
-    },
-  },
-  opts = { auto_cmd = false },
+  event = "User AstroFile",
+  config = function(...) require "astronvim.plugins.configs.guess-indent"(...) end,
 }
