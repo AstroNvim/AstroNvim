b5ee5a1a4a1c056eb266858f2f5a58dd35095910
Micah Halter <micah@mehalter.com>
2024-05-12 11:02:12 -0400

    chore(mason): add todo about plans for v5
    ref #2587

--- lua/astronvim/plugins/mason.lua
+++ lua/astronvim/plugins/mason.lua
@@ -1,3 +1,8 @@
+-- TODO: AstroNvim v5:
+-- - Add `mason-tool-installer.nvim` to manage mason installation
+-- - default disable all integrations to improve lazy loading
+-- - remove `astrocore.mason` and mappings, replace with `:MasonToolsUpdate`
+-- - remove `init` functions from other Mason plugins
 return {
   "williamboman/mason.nvim",
   cmd = {
