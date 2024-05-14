d7c525fc65095f1ea7a22a72e5fc54c6804eaba1
Micah Halter <micah@mehalter.com>
2024-05-14 08:14:19 -0400

    feat(plugins)!: move from `nvim-colorizer` to `nvim-highlight-colors`

--- lua/astronvim/plugins/configs/colorizer.lua
+++
@@ -1,7 +0,0 @@
-return function(_, opts)
-  local colorizer = require "colorizer"
-  colorizer.setup(opts)
-  for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-    if vim.t[tab].bufs then vim.tbl_map(function(buf) colorizer.attach_to_buffer(buf) end, vim.t[tab].bufs) end
-  end
-end
