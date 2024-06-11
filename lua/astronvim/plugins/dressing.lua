2195fc54c3ed767ffb835f67c030bd4de853c654
Micah Halter <micah@mehalter.com>
2024-06-11 11:02:50 -0400

    fix(dressing): use AstroUI for default prompt

--- lua/astronvim/plugins/dressing.lua
+++ lua/astronvim/plugins/dressing.lua
@@ -2,8 +2,10 @@ return {
   "stevearc/dressing.nvim",
   lazy = true,
   init = function() require("astrocore").load_plugin_with_func("dressing.nvim", vim.ui, { "input", "select" }) end,
-  opts = {
-    input = { default_prompt = "➤ " },
-    select = { backend = { "telescope", "builtin" } },
-  },
+  opts = function()
+    local get_icon = require("astroui").get_icon
+    return {
+      input = { default_prompt = get_icon("Selected", 1) },
+    }
+  end,
 }
