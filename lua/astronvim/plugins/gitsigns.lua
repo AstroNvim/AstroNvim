e0d228527bca539b079dc5a1397ebd5aec2d90ad
Micah Halter <micah@mehalter.com>
2024-06-28 08:25:18 -0400

    feat(gitsigns): set mappings with `on_attach` in `gitsigns` `opts` to make mappings context aware

--- lua/astronvim/plugins/gitsigns.lua
+++ lua/astronvim/plugins/gitsigns.lua
@@ -1,40 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
   enabled = vim.fn.executable "git" == 1,
-  dependencies = {
-    {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = opts.mappings
-        maps.n["<Leader>g"] = vim.tbl_get(opts, "_map_sections", "g")
-        maps.v["<Leader>g"] = vim.tbl_get(opts, "_map_sections", "g")
-        maps.n["]g"] = { function() require("gitsigns").next_hunk() end, desc = "Next Git hunk" }
-        maps.n["[g"] = { function() require("gitsigns").prev_hunk() end, desc = "Previous Git hunk" }
-        for _, mode in ipairs { "o", "x" } do
-          maps[mode]["ig"] = { ":<C-U>Gitsigns select_hunk<CR>", desc = "inside Git hunk" }
-        end
-
-        maps.n["<Leader>gl"] = { function() require("gitsigns").blame_line() end, desc = "View Git blame" }
-        maps.n["<Leader>gL"] =
-          { function() require("gitsigns").blame_line { full = true } end, desc = "View full Git blame" }
-        maps.n["<Leader>gp"] = { function() require("gitsigns").preview_hunk_inline() end, desc = "Preview Git hunk" }
-        maps.n["<Leader>gr"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" }
-        maps.v["<Leader>gr"] = {
-          function() require("gitsigns").reset_hunk { vim.fn.line ".", vim.fn.line "v" } end,
-          desc = "Reset Git hunk",
-        }
-        maps.n["<Leader>gR"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" }
-        maps.n["<Leader>gs"] = { function() require("gitsigns").stage_hunk() end, desc = "Stage Git hunk" }
-        maps.v["<Leader>gs"] = {
-          function() require("gitsigns").stage_hunk { vim.fn.line ".", vim.fn.line "v" } end,
-          desc = "Stage Git hunk",
-        }
-        maps.n["<Leader>gS"] = { function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" }
-        maps.n["<Leader>gu"] = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" }
-        maps.n["<Leader>gd"] = { function() require("gitsigns").diffthis() end, desc = "View Git diff" }
-      end,
-    },
-  },
   event = "User AstroGitFile",
   opts = function()
     local get_icon = require("astroui").get_icon
@@ -47,6 +13,40 @@ return {
         changedelete = { text = get_icon "GitSign" },
         untracked = { text = get_icon "GitSign" },
       },
+      on_attach = function(bufnr)
+        local astrocore = require "astrocore"
+        local prefix, maps = "<Leader>g", astrocore.empty_map_table()
+        for _, mode in ipairs { "n", "v" } do
+          maps[mode][prefix] = { desc = get_icon("Git", 1, true) .. "Git" }
+        end
+
+        maps.n[prefix .. "l"] = { function() require("gitsigns").blame_line() end, desc = "View Git blame" }
+        maps.n[prefix .. "L"] =
+          { function() require("gitsigns").blame_line { full = true } end, desc = "View full Git blame" }
+        maps.n[prefix .. "p"] = { function() require("gitsigns").preview_hunk_inline() end, desc = "Preview Git hunk" }
+        maps.n[prefix .. "r"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" }
+        maps.v[prefix .. "r"] = {
+          function() require("gitsigns").reset_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+          desc = "Reset Git hunk",
+        }
+        maps.n[prefix .. "R"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" }
+        maps.n[prefix .. "s"] = { function() require("gitsigns").stage_hunk() end, desc = "Stage Git hunk" }
+        maps.v[prefix .. "s"] = {
+          function() require("gitsigns").stage_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+          desc = "Stage Git hunk",
+        }
+        maps.n[prefix .. "S"] = { function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" }
+        maps.n[prefix .. "u"] = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" }
+        maps.n[prefix .. "d"] = { function() require("gitsigns").diffthis() end, desc = "View Git diff" }
+
+        maps.n["]g"] = { function() require("gitsigns").next_hunk() end, desc = "Next Git hunk" }
+        maps.n["[g"] = { function() require("gitsigns").prev_hunk() end, desc = "Previous Git hunk" }
+        for _, mode in ipairs { "o", "x" } do
+          maps[mode]["ig"] = { ":<C-U>Gitsigns select_hunk<CR>", desc = "inside Git hunk" }
+        end
+
+        astrocore.set_mappings(maps, { buffer = bufnr })
+      end,
       worktrees = require("astrocore").config.git_worktrees,
     }
   end,
