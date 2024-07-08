eb8a4153d7cf6bd2555c806658f6110f15d51026
Micah Halter <micah@mehalter.com>
2024-07-08 17:22:47 -0400

    chore(lspconfig): simplify `lspconfig` configuration function

--- lua/astronvim/plugins/configs/lspconfig.lua
+++ lua/astronvim/plugins/configs/lspconfig.lua
@@ -1,4 +1,5 @@
 return function(_, _)
+  local astrocore = require "astrocore"
   local setup_servers = function()
     local was_setup, astrolsp = {}, require "astrolsp"
     for _, server in ipairs(astrolsp.config.servers) do
@@ -7,11 +8,9 @@ return function(_, _)
         was_setup[server] = true
       end
     end
-    require("astrocore").exec_buffer_autocmds("FileType", { group = "lspconfig" })
-
-    require("astrocore").event "LspSetup"
+    astrocore.exec_buffer_autocmds("FileType", { group = "lspconfig" })
+    astrocore.event "LspSetup"
   end
-  local astrocore = require "astrocore"
   if astrocore.is_available "mason-lspconfig.nvim" then
     astrocore.on_load("mason-lspconfig.nvim", setup_servers)
   else
