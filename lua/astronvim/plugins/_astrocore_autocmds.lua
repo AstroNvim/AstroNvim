b4716f5aa7b2a1fdfa6ba34a74b39a7f8c711070
Micah Halter <micah@mehalter.com>
2024-07-09 21:30:24 -0400

    fix(autocmds): only emit each `augroup` event once

--- lua/astronvim/plugins/_astrocore_autocmds.lua
+++ lua/astronvim/plugins/_astrocore_autocmds.lua
@@ -157,8 +157,9 @@ return {
               if vim.g.vscode or not (current_file == "" or vim.bo[args.buf].buftype == "nofile") then
                 local skip_augroups = {}
                 for _, autocmd in ipairs(vim.api.nvim_get_autocmds { event = args.event }) do
-                  if autocmd.group_name ~= "filetypedetect" then skip_augroups[autocmd.group_name] = true end
+                  if autocmd.group_name then skip_augroups[autocmd.group_name] = true end
                 end
+                skip_augroups["filetypedetect"] = false -- don't skip filetypedetect events
                 astro.event "File"
                 local folder = vim.fn.fnamemodify(current_file, ":p:h")
                 if vim.fn.has "win32" == 1 then folder = ('"%s"'):format(folder) end
@@ -173,11 +174,12 @@ return {
                 vim.schedule(function()
                   if require("astrocore.buffer").is_valid(args.buf) then
                     for _, autocmd in ipairs(vim.api.nvim_get_autocmds { event = args.event }) do
-                      if not skip_augroups[autocmd.group_name] then
+                      if autocmd.group_name and not skip_augroups[autocmd.group_name] then
                         vim.api.nvim_exec_autocmds(
                           args.event,
                           { group = autocmd.group_name, buffer = args.buf, data = args.data }
                         )
+                        skip_augroups[autocmd.group_name] = true
                       end
                     end
                   end
