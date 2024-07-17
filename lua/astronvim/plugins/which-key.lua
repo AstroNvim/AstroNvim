b0df6aebc12bd54d4dea6e8dd34287a22fd3303d
Micah Halter <micah@mehalter.com>
2024-07-17 10:09:08 -0400

    fix(which-key): extend `disable` lists by default as well

--- lua/astronvim/plugins/which-key.lua
+++ lua/astronvim/plugins/which-key.lua
@@ -1,7 +1,7 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts_extend = { "spec" },
+  opts_extend = { "spec", "disable.ft", "disable.bt" },
   opts = {
     icons = {
       group = vim.g.icons_enabled ~= false and "" or "+",
