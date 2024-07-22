08399f6ead600d1ec45038e6b96a8240f06e8f91
Micah Halter <micah@mehalter.com>
2024-07-22 09:49:03 -0400

    fix(better-escape): only use `jj` and `jk` for escape in insert mode

--- lua/astronvim/plugins/better-escape.lua
+++ lua/astronvim/plugins/better-escape.lua
@@ -1,1 +1,11 @@
-return { "max397574/better-escape.nvim", event = "VeryLazy", opts = { timeout = 300 } }
+return {
+  "max397574/better-escape.nvim",
+  event = "VeryLazy",
+  opts = {
+    timeout = 300,
+    default_mappings = false,
+    mappings = {
+      i = { j = { k = "<Esc>", j = "<Esc>" } },
+    },
+  },
+}
