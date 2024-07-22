7c6cb6fe4d4888937881413a5377bd3a11ab02ec
Micah Halter <micah@mehalter.com>
2024-07-22 10:21:22 -0400

    fix(telescope): automatically rebuild `telescope-fzf` if necessary

--- lua/astronvim/plugins/configs/telescope.lua
+++ lua/astronvim/plugins/configs/telescope.lua
@@ -4,5 +4,4 @@ return function(_, opts)
   local is_available = require("astrocore").is_available
   if is_available "nvim-notify" then telescope.load_extension "notify" end
   if is_available "aerial.nvim" then telescope.load_extension "aerial" end
-  if is_available "telescope-fzf-native.nvim" then telescope.load_extension "fzf" end
 end
