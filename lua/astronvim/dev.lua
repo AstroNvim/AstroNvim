c059f4c05b07a6944ae716630af721b431fb9b67
Micah Halter <micah@mehalter.com>
2024-07-25 11:37:30 -0400

    chore(snapshot): update `lazy_snapshot`

--- lua/astronvim/dev.lua
+++ lua/astronvim/dev.lua
@@ -24,8 +24,12 @@ function M.generate_snapshot(write)
     file:write "return {\n"
   end
   local snapshot = {}
+  local untracked_plugins = {
+    "AstroNvim/AstroNvim", -- Managed by user
+    "Bilal2453/luvit-meta", -- Not a real plugin, used for type stubs only
+  }
   for _, plugin in ipairs(plugins) do
-    if plugin[1] ~= "AstroNvim/AstroNvim" then
+    if not vim.tbl_contains(untracked_plugins, plugin[1]) then
       plugin = { plugin[1], commit = git_commit(plugin.dir), version = plugin.version }
       if prev_snapshot[plugin[1]] and prev_snapshot[plugin[1]].version then
         plugin.version = prev_snapshot[plugin[1]].version
