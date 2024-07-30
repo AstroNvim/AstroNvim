4f95228e559f938b80cb45223bef4bb54fd8ff95
Micah Halter <micah@mehalter.com>
2024-07-30 05:59:35 -0400

    fix(nvim-dap-ui): fix incorrect loading order

--- lua/astronvim/plugins/dap.lua
+++ lua/astronvim/plugins/dap.lua
@@ -3,32 +3,6 @@ return {
   lazy = true,
   specs = {
     {
-      "rcarriga/nvim-dap-ui",
-      lazy = true,
-      specs = {
-        "AstroNvim/astrocore",
-        opts = function(_, opts)
-          local maps = opts.mappings
-          maps.n["<Leader>d"] = vim.tbl_get(opts, "_map_sections", "d")
-          maps.v["<Leader>d"] = vim.tbl_get(opts, "_map_sections", "d")
-          maps.n["<Leader>dE"] = {
-            function()
-              vim.ui.input({ prompt = "Expression: " }, function(expr)
-                if expr then require("dapui").eval(expr, { enter = true }) end
-              end)
-            end,
-            desc = "Evaluate Input",
-          }
-          maps.n["<Leader>du"] = { function() require("dapui").toggle() end, desc = "Toggle Debugger UI" }
-          maps.n["<Leader>dh"] = { function() require("dap.ui.widgets").hover() end, desc = "Debugger Hover" }
-          maps.v["<Leader>dE"] = { function() require("dapui").eval() end, desc = "Evaluate Input" }
-        end,
-      },
-      dependencies = { { "nvim-neotest/nvim-nio", lazy = true } },
-      opts = { floating = { border = "rounded" } },
-      config = function(...) require "astronvim.plugins.configs.nvim-dap-ui"(...) end,
-    },
-    {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local get_icon = require("astroui").get_icon
@@ -83,6 +57,34 @@ return {
     },
   },
   dependencies = {
+    {
+      "rcarriga/nvim-dap-ui",
+      lazy = true,
+      specs = {
+        {
+          "AstroNvim/astrocore",
+          opts = function(_, opts)
+            local maps = opts.mappings
+            maps.n["<Leader>d"] = vim.tbl_get(opts, "_map_sections", "d")
+            maps.v["<Leader>d"] = vim.tbl_get(opts, "_map_sections", "d")
+            maps.n["<Leader>dE"] = {
+              function()
+                vim.ui.input({ prompt = "Expression: " }, function(expr)
+                  if expr then require("dapui").eval(expr, { enter = true }) end
+                end)
+              end,
+              desc = "Evaluate Input",
+            }
+            maps.n["<Leader>du"] = { function() require("dapui").toggle() end, desc = "Toggle Debugger UI" }
+            maps.n["<Leader>dh"] = { function() require("dap.ui.widgets").hover() end, desc = "Debugger Hover" }
+            maps.v["<Leader>dE"] = { function() require("dapui").eval() end, desc = "Evaluate Input" }
+          end,
+        },
+      },
+      dependencies = { { "nvim-neotest/nvim-nio", lazy = true } },
+      opts = { floating = { border = "rounded" } },
+      config = function(...) require "astronvim.plugins.configs.nvim-dap-ui"(...) end,
+    },
     {
       "jay-babu/mason-nvim-dap.nvim",
       dependencies = { "nvim-dap", "williamboman/mason.nvim" },
