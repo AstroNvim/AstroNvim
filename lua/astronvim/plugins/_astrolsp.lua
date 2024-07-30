101543f2fe26ed53b742e4085ccbea8ed2940bf9
Micah Halter <micah@mehalter.com>
2024-07-30 13:46:07 -0400

    fix(astrolsp): LSP signature help shouldn't be focusable

--- lua/astronvim/plugins/_astrolsp.lua
+++ lua/astronvim/plugins/_astrolsp.lua
@@ -19,7 +19,7 @@ return {
         ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", silent = true }),
         ["textDocument/signatureHelp"] = vim.lsp.with(
           vim.lsp.handlers.signature_help,
-          { border = "rounded", silent = true }
+          { border = "rounded", silent = true, focusable = false }
         ),
       },
       servers = {},
