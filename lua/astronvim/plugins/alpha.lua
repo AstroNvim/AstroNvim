6d439577678581b25eb86cc36e34c6e33369324d
Micah Halter <micah@mehalter.com>
2024-08-25 18:00:36 -0400

    fix(alpha): improve window detection for dashboard
    Fixes #2691

--- lua/astronvim/plugins/alpha.lua
+++ lua/astronvim/plugins/alpha.lua
@@ -8,9 +8,19 @@ return {
         local maps = opts.mappings
         maps.n["<Leader>h"] = {
           function()
-            local wins = vim.api.nvim_tabpage_list_wins(0)
-            if #wins > 1 and vim.bo[vim.api.nvim_win_get_buf(wins[1])].filetype == "neo-tree" then
-              vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+            local is_valid_win = function(win)
+              local bufnr = vim.api.nvim_win_get_buf(win)
+              return require("astrocore.buffer").is_valid(bufnr) or vim.bo[bufnr].filetype == "alpha"
+            end
+            local cur_win = vim.api.nvim_get_current_win()
+            -- try to find the most suitable window for Alpha
+            if not is_valid_win(cur_win) then
+              for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+                if win ~= cur_win and is_valid_win(win) then
+                  vim.fn.win_gotoid(win)
+                  break
+                end
+              end
             end
             require("alpha").start(false)
           end,
