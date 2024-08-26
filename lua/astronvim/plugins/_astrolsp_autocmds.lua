fe6e5961206b46d2c1b883ae538b971d5a7d6ac0
Micah Halter <micah@mehalter.com>
2024-08-25 22:07:18 -0400

    feat(astrolsp): factor in `retriggerCharacters` in `signatureHelpProvider`

--- lua/astronvim/plugins/_astrolsp_autocmds.lua
+++ lua/astronvim/plugins/_astrolsp_autocmds.lua
@@ -33,20 +33,34 @@ return {
           event = "TextChangedI",
           desc = "Automatically show signature help if enabled",
           callback = function(args)
-            local signature_help, trigger = vim.b[args.buf].signature_help, vim.b[args.buf].signature_help_trigger
+            local signature_help = vim.b[args.buf].signature_help
             if signature_help == nil then signature_help = require("astrolsp").config.features.signature_help end
-            if signature_help and trigger then
-              local cur_line = vim.api.nvim_get_current_line():gsub("%s+$", "") -- rm trailing spaces
+            if signature_help then
+              local trigger = vim.b[args.buf].signature_help_trigger
+              local retrigger = vim.b[args.buf].signature_help_retrigger
               local pos = vim.api.nvim_win_get_cursor(0)[2]
-              local cur_char = cur_line:sub(pos, pos)
+              local cur_char = vim.api.nvim_get_current_line():sub(pos, pos)
 
-              for _, char in ipairs(trigger) do
-                if cur_char == char then
-                  vim.lsp.buf.signature_help()
-                  return
+              if trigger then
+                for _, char in ipairs(trigger) do
+                  if cur_char == char then
+                    vim.g.signature_help_triggered = true
+                    vim.lsp.buf.signature_help()
+                    return
+                  end
+                end
+              end
+
+              if vim.g.signature_help_triggered and retrigger then
+                for _, char in ipairs(retrigger) do
+                  if cur_char == char then
+                    vim.lsp.buf.signature_help()
+                    return
+                  end
                 end
               end
             end
+            vim.g.signature_help_triggered = false
           end,
         },
       },
