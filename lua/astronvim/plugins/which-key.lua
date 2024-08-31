5a7786079a3ffd747c13a3b4160f22caaa57737b
Micah Halter <micah@mehalter.com>
2024-08-31 06:34:45 -0400

    fix(which-key): terminal mode trigger disabled upstream

--- lua/astronvim/plugins/which-key.lua
+++ lua/astronvim/plugins/which-key.lua
@@ -3,7 +3,6 @@ return {
   event = "VeryLazy",
   opts_extend = { "spec", "disable.ft", "disable.bt" },
   opts = {
-    triggers = { { "<auto>", mode = "nxso" } },
     icons = {
       group = vim.g.icons_enabled ~= false and "" or "+",
       rules = false,
