66faaa5697692cc7c3bca3b39744214565c096e8
Micah Halter <micah@mehalter.com>
2024-09-21 12:04:04 -0400

    fix(astrolsp): update codelens on text changing in normal mode

--- lua/astronvim/plugins/_astrolsp_autocmds.lua
+++ lua/astronvim/plugins/_astrolsp_autocmds.lua
@@ -20,7 +20,7 @@ return {
       lsp_codelens_refresh = {
         cond = "textDocument/codeLens",
         {
-          event = { "InsertLeave", "BufEnter" },
+          event = { "TextChanged", "InsertLeave", "BufEnter" },
           desc = "Refresh codelens (buffer)",
           callback = function(args)
             if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
