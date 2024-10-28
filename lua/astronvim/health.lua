c4fa819eabedd8b4d5ee6c69b72d0bfd2d855f06
Micah Halter <micah.halter@gtri.gatech.edu>
2024-10-28 12:32:51 -0400

    revert: conflicting mapping health checks moved to individual plugins
    Refs: 33a90b5

--- lua/astronvim/health.lua
+++ lua/astronvim/health.lua
@@ -10,7 +10,7 @@ local M = {}
 local health = vim.health
 
 function M.check()
-  health.start "AstroNvim"
+  health.start "Checking requirements"
 
   health.info("AstroNvim Version: " .. require("astronvim").version())
   health.info("Neovim Version: v" .. vim.fn.matchstr(vim.fn.execute "version", "NVIM v\\zs[^\n]*"))
@@ -66,65 +66,6 @@ function M.check()
     else
       health[program.type](("`%s` is not installed: %s"):format(name, program.msg))
     end
-  end
-
-  local function check_duplicate_mappings(all_maps)
-    local set_mappings, any_duplicates = {}, false
-    for mode, mappings in pairs(all_maps) do
-      for lhs, rhs in pairs(mappings) do
-        if rhs then
-          if not set_mappings[mode] then set_mappings[mode] = {} end
-          local normalized_lhs = vim.api.nvim_replace_termcodes(lhs, true, true, true)
-          if set_mappings[mode][normalized_lhs] then
-            set_mappings[mode][normalized_lhs][lhs] = rhs
-            set_mappings[mode][normalized_lhs][1] = true
-            any_duplicates = true
-          else
-            set_mappings[mode][normalized_lhs] = { [1] = false, [lhs] = rhs }
-          end
-        end
-      end
-    end
-
-    if any_duplicates then
-      local msg = ""
-      for mode, mappings in pairs(set_mappings) do
-        local mode_msg
-        for _, duplicate_mappings in pairs(mappings) do
-          if duplicate_mappings[1] then
-            if not mode_msg then
-              mode_msg = ("Duplicate mappings detected in mode `%s`:\n"):format(mode)
-            else
-              mode_msg = mode_msg .. "\n"
-            end
-            for lhs, rhs in pairs(duplicate_mappings) do
-              if type(lhs) == "string" then
-                mode_msg = mode_msg .. ("- %s: %s\n"):format(lhs, type(rhs) == "table" and (rhs.desc or rhs[1]) or rhs)
-              end
-            end
-          end
-        end
-        if mode_msg then msg = msg .. mode_msg end
-      end
-      health.warn(
-        msg,
-        "Make sure to normalize the left hand side of mappings to what is used in :h keycodes. This includes making sure to capitalize <Leader> and <LocalLeader>."
-      )
-    else
-      health.ok "No duplicate mappings detected"
-    end
-  end
-
-  local astrocore_avail, astrocore = pcall(require, "astrocore")
-  if astrocore_avail then
-    health.start "Checking for duplicate mappings in AstroCore"
-    check_duplicate_mappings(astrocore.config.mappings)
-  end
-
-  local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-  if astrolsp_avail then
-    health.start "Checking for duplicate mappings in AstroLSP"
-    check_duplicate_mappings(astrolsp.config.mappings)
   end
 end
 
