50f962ef05ad17010afa6f55df4e91c713d30e07
Micah Halter <micah.halter@gtri.gatech.edu>
2024-11-08 15:45:59 -0500

    feat(astrolsp): use the new `defaults` table for configuring `hover` and `signature_help`

--- lua/astronvim/plugins/_astrolsp.lua
+++ lua/astronvim/plugins/_astrolsp.lua
@@ -1,33 +1,31 @@
 return {
   "AstroNvim/astrolsp",
   lazy = true,
-  opts = function(_, opts)
-    local lsp_handlers
-    if vim.fn.has "nvim-0.11" == 0 then
-      lsp_handlers = {
-        ["textDocument/signatureHelp"] = vim.lsp.with(
-          vim.lsp.handlers.signature_help,
-          { border = "rounded", silent = true, focusable = false }
-        ),
-        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", silent = true }),
-      }
-    end
-
-    return require("astrocore").extend_tbl(opts, {
-      features = {
-        codelens = true,
-        inlay_hints = false,
-        semantic_tokens = true,
+  ---@type AstroLSPOpts
+  opts = {
+    features = {
+      codelens = true,
+      inlay_hints = false,
+      semantic_tokens = true,
+    },
+    capabilities = vim.lsp.protocol.make_client_capabilities(),
+    ---@diagnostic disable-next-line: missing-fields
+    config = {},
+    defaults = {
+      hover = {
+        border = "rounded",
+        silent = true,
       },
-      capabilities = vim.lsp.protocol.make_client_capabilities(),
-      ---@diagnostic disable-next-line: missing-fields
-      config = {},
-      flags = {},
-      formatting = { format_on_save = { enabled = true }, disabled = {} },
-      handlers = { function(server, server_opts) require("lspconfig")[server].setup(server_opts) end },
-      lsp_handlers = lsp_handlers,
-      servers = {},
-      on_attach = nil,
-    } --[[@as AstroLSPOpts]])
-  end,
+      signature_help = {
+        border = "rounded",
+        silent = true,
+        focusable = false,
+      },
+    },
+    flags = {},
+    formatting = { format_on_save = { enabled = true }, disabled = {} },
+    handlers = { function(server, server_opts) require("lspconfig")[server].setup(server_opts) end },
+    servers = {},
+    on_attach = nil,
+  },
 }
