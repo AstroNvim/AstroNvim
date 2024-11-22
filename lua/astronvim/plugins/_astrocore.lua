f26c0de51b80d92c2199e205f76d91eb3768b1b4
Glenn Waters <glenn@watrs.ca>
2024-11-22 13:54:34 -0500

    fix(astrocore)!: use default `update_in_insert` value for diagnostic configuration

--- lua/astronvim/plugins/_astrocore.lua
+++ lua/astronvim/plugins/_astrocore.lua
@@ -50,7 +50,7 @@ return {
               [vim.diagnostic.severity.INFO] = get_icon "DiagnosticInfo",
             },
           },
-          update_in_insert = true,
+          update_in_insert = false,
           underline = true,
           severity_sort = true,
           float = {
