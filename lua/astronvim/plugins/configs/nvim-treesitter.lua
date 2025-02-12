06c071d28fd7954996a8c1748bb4f1ed1dbc59b9
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-12 11:25:35 -0500

    perf(treesitter): evaluate large buffer detection immediately

--- lua/astronvim/plugins/configs/nvim-treesitter.lua
+++ lua/astronvim/plugins/configs/nvim-treesitter.lua
@@ -4,16 +4,14 @@ return function(plugin, opts)
   if vim.fn.executable "git" == 0 then opts.ensure_installed = nil end
 
   -- disable all treesitter modules on large buffer
-  if vim.tbl_get(require("astrocore").config, "features", "large_buf") then
-    for _, module in ipairs(ts.available_modules()) do
-      if not opts[module] then opts[module] = {} end
-      local module_opts = opts[module]
-      local disable = module_opts.disable
-      module_opts.disable = function(lang, bufnr)
-        return vim.b[bufnr].large_buf
-          or (type(disable) == "table" and vim.tbl_contains(disable, lang))
-          or (type(disable) == "function" and disable(lang, bufnr))
-      end
+  for _, module in ipairs(ts.available_modules()) do
+    if not opts[module] then opts[module] = {} end
+    local module_opts = opts[module]
+    local disable = module_opts.disable
+    module_opts.disable = function(lang, bufnr)
+      return require("astrocore").is_large_buf(bufnr)
+        or (type(disable) == "table" and vim.tbl_contains(disable, lang))
+        or (type(disable) == "function" and disable(lang, bufnr))
     end
   end
 
