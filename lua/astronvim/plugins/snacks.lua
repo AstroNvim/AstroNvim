8710dbbd5921958fc09ca44b316dd0f7c2d380cb
Micah Halter <micah@mehalter.com>
2025-02-14 11:54:19 -0500

    feat(plugins)!: replace `nvim-notify` with `snacks.notifier`

--- lua/astronvim/plugins/snacks.lua
+++ lua/astronvim/plugins/snacks.lua
@@ -3,10 +3,33 @@ return {
   lazy = false,
   priority = 1000,
   opts = function(_, opts)
+    local get_icon = require("astroui").get_icon
+
     -- configure `vim.ui.input`
     opts.input = {}
+
+    -- configure notifier
+    opts.notifier = {}
+    opts.notifier.icons = {
+      DEBUG = get_icon "Debugger",
+      ERROR = get_icon "DiagnosticError",
+      INFO = get_icon "DiagnosticInfo",
+      TRACE = get_icon "DiagnosticHint",
+      WARN = get_icon "DiagnosticWarn",
+    }
 
     -- configure picker and `vim.ui.select`
     opts.picker = { ui_select = true }
   end,
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+
+        -- Snacks.notifier mappings
+        maps.n["<Leader>uD"] = { function() require("snacks.notifier").hide() end, desc = "Dismiss notifications" }
+      end,
+    },
+  },
 }
