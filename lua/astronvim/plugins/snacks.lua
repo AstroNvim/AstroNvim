810236d6bc596095ac1111790baf6036baf96d31
Micah Halter <micah@mehalter.com>
2025-02-14 11:55:23 -0500

    feat(plugins)!: replace `indent-blankline` with `snacks.indent`

--- lua/astronvim/plugins/snacks.lua
+++ lua/astronvim/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
   priority = 1000,
   opts = function(_, opts)
     local get_icon = require("astroui").get_icon
+    local buf_utils = require "astrocore.buffer"
 
     -- configure `vim.ui.input`
     opts.input = {}
@@ -20,12 +21,28 @@ return {
 
     -- configure picker and `vim.ui.select`
     opts.picker = { ui_select = true }
+
+    opts.indent = {
+      indent = { char = "▏" },
+      scope = { char = "▏" },
+      filter = function(bufnr)
+        return buf_utils.is_valid(bufnr)
+          and not buf_utils.is_large(bufnr)
+          and vim.g.snacks_indent ~= false
+          and vim.b[bufnr].snacks_indent ~= false
+      end,
+      animate = { enabled = false },
+    }
   end,
   specs = {
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
+
+        -- Snacks.indent mappings
+        maps.n["<Leader>u|"] =
+          { function() require("snacks").toggle.indent():toggle() end, desc = "Toggle indent guides" }
 
         -- Snacks.notifier mappings
         maps.n["<Leader>uD"] = { function() require("snacks.notifier").hide() end, desc = "Dismiss notifications" }
