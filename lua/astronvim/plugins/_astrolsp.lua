ac1555797980dc2791a391e9e40224ee5349d96b
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-17 12:05:22 -0500

    fix(astrolsp): disable LSP file operations by default until v5

--- lua/astronvim/plugins/_astrolsp.lua
+++ lua/astronvim/plugins/_astrolsp.lua
@@ -15,12 +15,12 @@ return {
       file_operations = {
         timeout = 10000,
         operations = {
-          didCreate = true,
-          didDelete = true,
-          didRename = true,
-          willCreate = true,
-          willDelete = true,
-          willRename = true,
+          didCreate = false,
+          didDelete = false,
+          didRename = false,
+          willCreate = false,
+          willDelete = false,
+          willRename = false,
         },
       },
       flags = {},
