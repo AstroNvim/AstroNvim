533d7166593d42886ca2e05f0997299472ac160a
Micah Halter <micah.halter@gtri.gatech.edu>
2025-03-27 09:21:27 -0400

    fix(mason): use ascii icons if icons are disabled

--- lua/astronvim/plugins/mason.lua
+++ lua/astronvim/plugins/mason.lua
@@ -17,15 +17,21 @@ return {
     },
   },
   opts_extend = { "registries" },
-  opts = {
-    registries = { "github:mason-org/mason-registry" },
-    ui = {
-      icons = {
+  opts = function(_, opts)
+    if not opts.registries then opts.registries = {} end
+    table.insert(opts.registries, "github:mason-org/mason-registry")
+    if not opts.ui then opts.ui = {} end
+    opts.ui.icons = vim.g.icons_enabled == false
+        and {
+          package_installed = "O",
+          package_uninstalled = "X",
+          package_pending = "0",
+        }
+      or {
         package_installed = "✓",
         package_uninstalled = "✗",
         package_pending = "⟳",
-      },
-    },
-  },
+      }
+  end,
   build = ":MasonUpdate",
 }
