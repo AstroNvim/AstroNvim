10fd1c6d07e1ef1142ab33ff262b2c8abbbcc9c2
Micah Halter <micah.halter@gtri.gatech.edu>
2025-03-28 11:01:21 -0400

    fix(which-key): disable all icons when icons are disabled
    Closes #2770

--- lua/astronvim/plugins/which-key.lua
+++ lua/astronvim/plugins/which-key.lua
@@ -2,11 +2,44 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts_extend = { "spec", "disable.ft", "disable.bt" },
-  opts = {
-    icons = {
-      group = vim.g.icons_enabled ~= false and "" or "+",
-      rules = false,
-      separator = "-",
-    },
-  },
+  opts = function(_, opts)
+    if not opts.icons then opts.icons = {} end
+    opts.icons.group = ""
+    opts.icons.rules = false
+    opts.icons.separator = "-"
+    if vim.g.icons_enabled == false then
+      opts.icons.breadcrumb = ">"
+      opts.icons.group = "+"
+      opts.icons.keys = {
+        Up = "Up",
+        Down = "Down",
+        Left = "Left",
+        Right = "Right",
+        C = "Ctrl+",
+        M = "Alt+",
+        D = "Cmd+",
+        S = "Shift+",
+        CR = "Enter",
+        Esc = "Esc",
+        ScrollWheelDown = "ScrollDown",
+        ScrollWheelUp = "ScrollUp",
+        NL = "Enter",
+        BS = "Backspace",
+        Space = "Space",
+        Tab = "Tab",
+        F1 = "F1",
+        F2 = "F2",
+        F3 = "F3",
+        F4 = "F4",
+        F5 = "F5",
+        F6 = "F6",
+        F7 = "F7",
+        F8 = "F8",
+        F9 = "F9",
+        F10 = "F10",
+        F11 = "F11",
+        F12 = "F12",
+      }
+    end
+  end,
 }
