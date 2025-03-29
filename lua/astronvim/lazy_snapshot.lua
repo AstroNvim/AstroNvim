30b0d2fe6bcd057c5f8d57b22d820bc954603eeb
Micah Halter <micah@mehalter.com>
2025-03-29 09:18:04 -0400

    fix(snapshot): temporarily pin todo-comments to commit until next release

--- lua/astronvim/lazy_snapshot.lua
+++ lua/astronvim/lazy_snapshot.lua
@@ -18,7 +18,7 @@ return {
   { "folke/lazydev.nvim", version = "^1", optional = true },
   { "folke/neoconf.nvim", commit = "d4848c791496eab6989951d46586fabf7a22942c", optional = true },
   { "folke/snacks.nvim", version = "^2", optional = true },
-  { "folke/todo-comments.nvim", version = "^1", optional = true },
+  { "folke/todo-comments.nvim", commit = "304a8d204ee787d2544d8bc23cd38d2f929e7cc5", optional = true },
   { "folke/which-key.nvim", version = "^3", optional = true },
   { "jay-babu/mason-null-ls.nvim", version = "^2", optional = true },
   { "jay-babu/mason-nvim-dap.nvim", version = "^2", optional = true },
