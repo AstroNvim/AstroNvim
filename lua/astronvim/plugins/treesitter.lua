ab69f958bd7b2bf7bdaaeabd40ef5c6460d04c8d
Micah Halter <micah.halter@gtri.gatech.edu>
2025-03-31 16:22:17 -0400

    fix(treesitter): load treesitter immediately if opening a file directly

--- lua/astronvim/plugins/treesitter.lua
+++ lua/astronvim/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   main = "nvim-treesitter.configs",
   dependencies = { { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true } },
   event = "VeryLazy",
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter immediately when opening a file from the cmdline
   cmd = {
     "TSBufDisable",
     "TSBufEnable",
