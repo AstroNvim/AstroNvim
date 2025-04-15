35c00400b25cbf3d9eeee9c100532ce25e34211e
Micah Halter <micah.halter@gtri.gatech.edu>
2025-04-15 11:08:17 -0400

    refactor(todo-comments): clean up spec for better maintainability

--- lua/astronvim/plugins/todo-comments.lua
+++ lua/astronvim/plugins/todo-comments.lua
@@ -4,41 +4,51 @@ return {
   cmd = { "TodoTrouble", "TodoLocList", "TodoQuickFix" },
   event = "User AstroFile",
   specs = {
-    {
-      "nvim-telescope/telescope.nvim",
-      optional = true,
-      specs = { "folke/todo-comments.nvim", cmd = { "TodoTelescope" } },
-    },
-    {
-      "ibhagwan/fzf-lua",
-      optional = true,
-      specs = { "folke/todo-comments.nvim", cmd = { "TodoFzfLua" } },
-    },
     { "nvim-lua/plenary.nvim", lazy = true },
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        if require("astrocore").is_available "fzf-lua" then
-          maps.n["<Leader>fT"] = { "<Cmd>TodoFzfLua<CR>", desc = "Find TODOs" }
-        elseif require("astrocore").is_available "telescope.nvim" then
-          maps.n["<Leader>fT"] = { "<Cmd>TodoTelescope<CR>", desc = "Find TODOs" }
-        elseif require("astrocore").is_available "snacks.nvim" then
-          if vim.tbl_get(require("astrocore").plugin_opts "snacks.nvim", "picker", "enabled") ~= false then
-            maps.n["<Leader>fT"] = {
-              function()
-                if not package.loaded["todo-comments"] then -- make sure to load todo-comments
-                  require("lazy").load { plugins = { "todo-comments.nvim" } }
-                end
-                require("snacks").picker.todo_comments()
-              end,
-              desc = "Find TODOs",
-            }
-          end
+        local astrocore = require "astrocore"
+        if
+          astrocore.is_available "snacks.nvim"
+          and vim.tbl_get(astrocore.plugin_opts "snacks.nvim", "picker", "enabled") ~= false
+        then
+          maps.n["<Leader>fT"] = {
+            function()
+              if not package.loaded["todo-comments"] then -- make sure to load todo-comments
+                require("lazy").load { plugins = { "todo-comments.nvim" } }
+              end
+              require("snacks").picker.todo_comments()
+            end,
+            desc = "Find TODOs",
+          }
         end
         maps.n["]T"] = { function() require("todo-comments").jump_next() end, desc = "Next TODO comment" }
         maps.n["[T"] = { function() require("todo-comments").jump_prev() end, desc = "Previous TODO comment" }
       end,
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      optional = true,
+      specs = {
+        { "folke/todo-comments.nvim", cmd = { "TodoTelescope" } },
+        {
+          "AstroNvim/astrocore",
+          opts = { mappings = { n = { ["<Leader>fT"] = { "<Cmd>TodoTelescope<CR>", desc = "Find TODOs" } } } },
+        },
+      },
+    },
+    {
+      "ibhagwan/fzf-lua",
+      optional = true,
+      specs = {
+        { "folke/todo-comments.nvim", cmd = { "TodoFzfLua" } },
+        {
+          "AstroNvim/astrocore",
+          opts = { mappings = { n = { ["<Leader>fT"] = { "<Cmd>TodoFzfLua<CR>", desc = "Find TODOs" } } } },
+        },
+      },
     },
   },
   opts = {},
