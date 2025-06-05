aa646450bc218fdacb395424435ff739626e1b90
Micah Halter <micah.halter@gtri.gatech.edu>
2025-06-05 10:38:05 -0400

    fix: resolve loading dependency order

--- lua/astronvim/plugins/lspconfig.lua
+++ lua/astronvim/plugins/lspconfig.lua
@@ -1,11 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
-  specs = {
-    {
-      "AstroNvim/astrolsp",
-      optional = true,
-      dependencies = { "neovim/nvim-lspconfig" },
-    },
+  dependencies = {
+    { "AstroNvim/astrolsp", optional = true },
   },
   cmd = { "LspInfo", "LspLog", "LspStart" },
   event = "User AstroFile",
