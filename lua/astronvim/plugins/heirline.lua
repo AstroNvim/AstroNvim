cd9b755becb9fdb4a3d25c760769cf4c11c45c4c
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-08-26 22:33:40 +0200

    M lua/astronvim/plugins/heirline.lua

--- lua/astronvim/plugins/heirline.lua
+++ lua/astronvim/plugins/heirline.lua
@@ -55,6 +55,7 @@ return {
   opts = function(_, opts)
     local status = require "astroui.status"
     local ui_config = require("astroui").config
+    local show_close_button = vim.tbl_get(ui_config, "status", "show_close_button") ~= false
     local cached_func = function(func, ...)
       local cached
       local args = { ... }
@@ -128,16 +129,15 @@ return {
             provider = status.provider.tabnr(),
             hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
           },
-          vim.tbl_get(ui_config, "status", "show_close_button") ~= false
-              and { -- close button for current tab
-                provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
-                hl = cached_func(status.hl.get_attributes, "tab_close", true),
-                on_click = {
-                  callback = function() require("astrocore.buffer").close_tab() end,
-                  name = "heirline_tabline_close_tab_callback",
-                },
-              }
-            or nil,
+          show_close_button
+            and {
+              provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+              hl = cached_func(status.hl.get_attributes, "tab_close", true),
+              on_click = {
+                callback = function() require("astrocore.buffer").close_tab() end,
+                name = "heirline_tabline_close_tab_callback",
+              },
+            },
         },
       },
       statuscolumn = {
