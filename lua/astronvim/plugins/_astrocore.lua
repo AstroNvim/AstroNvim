89c880f50134b21f218495c530a79da31fae4e55
Micah Halter <micah.halter@gtri.gatech.edu>
2025-10-10 15:32:06 -0400

    feat(treesitter): add disabling treesitter on large buffers

--- lua/astronvim/plugins/_astrocore.lua
+++ lua/astronvim/plugins/_astrocore.lua
@@ -90,6 +90,7 @@ return {
           },
         },
         treesitter = {
+          disabled = function(_, bufnr) return require("astrocore.buffer").is_large(bufnr) end,
           highlight = true,
           indent = true,
           ensure_installed = { "bash", "c", "lua", "markdown", "markdown_inline", "python", "query", "vim", "vimdoc" },
